12200358c5dbee8a8e86bb2dcc562e2abce81098
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-04-15 13:11:15 +0530

    card  assign to user logic implemented

--- src/pages/admin/cards/scanned.tsx
+++ src/pages/admin/cards/scanned.tsx
@@ -1,17 +1,19 @@
 import MaterialTable from "@material-table/core";
-import { useFetch } from "hooks";
+import { useChange, useFetch } from "hooks";
 import PanelLayout from "layouts/panel";
-import { Card } from "types";
+import { Card, User } from "types";
 import { MuiTblOptions, clock, getDataWithSL } from "utils";
 
 const Cards = () => {
-  const { data, isLoading } = useFetch<Card[]>(`cards`);
+  const { data, isLoading, mutate } = useFetch<Card[]>(`cards`);
+  const { data: users, isLoading: isUsersFetching } = useFetch<User[]>(`users`);
+  const { change, isChanging } = useChange();
   return (
     <PanelLayout title="Scanned Cards - SY HR MS">
-      <section className="container mx-auto">
+      <section className="w-11/12 mx-auto">
         <MaterialTable
           title="All Scanned Cards"
-          isLoading={isLoading}
+          isLoading={isLoading || isUsersFetching || isChanging}
           data={data ? getDataWithSL<Card>(data) : []}
           options={{ ...MuiTblOptions(), selection: true }}
           columns={[
@@ -24,24 +26,39 @@ const Cards = () => {
             {
               title: "Card ID",
               field: "cardId",
+              editable: "never",
             },
             {
               title: "Assigned User",
               field: "userId",
+              lookup: users?.reduce((lookup: any, user) => {
+                lookup[user.id] = user.name;
+                return lookup;
+              }, {}),
             },
             {
               title: "Last Updated",
               field: "updatedAt",
               render: (data) => clock(data.updatedAt).fromNow(),
+              editable: "never",
             },
             {
               title: "Created",
               field: "createdAt",
               render: (data) => new Date(data.createdAt).toDateString(),
+              editable: "never",
             },
           ]}
           editable={{
-            onRowDelete: async (oldData) => {},
+            async onRowUpdate(newData, oldData) {
+              const response = await change(`cards/${newData.cardId}`, {
+                method: "PATCH",
+                body: { userId: newData.userId },
+              });
+              console.log(response);
+              mutate();
+            },
+            async onRowDelete(oldData) {},
           }}
         />
       </section>
