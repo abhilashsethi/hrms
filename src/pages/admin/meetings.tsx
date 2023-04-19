9fc40db135dd84c0cbb8d699b78652a8dc0341bd
KumarGudu <kg150201@gmail.com>
2023-04-19 17:55:02 +0530

    project and meetings

---
+++ src/pages/admin/meetings.tsx
@@ -0,0 +1,11 @@
+import PanelLayout from "layouts/panel";
+const Meetings = () => {
+  return (
+    <PanelLayout title="Meetings - SY HR MS">
+      <h1>meetings</h1>
+      <h1>Work on Progress</h1>
+    </PanelLayout>
+  );
+};
+
+export default Meetings;
---
+++ src/pages/admin/projects.tsx
@@ -0,0 +1,11 @@
+import PanelLayout from "layouts/panel";
+const Projects = () => {
+  return (
+    <PanelLayout title="Projects - SY HR MS">
+      <div>Projects</div>
+      <h1>Work on Progress</h1>
+    </PanelLayout>
+  );
+};
+
+export default Projects;
