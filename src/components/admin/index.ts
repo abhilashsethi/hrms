ad9b28d26c4af3e16e5fe92190aa7390768ff57f
KumarGudu <kg150201@gmail.com>
2023-04-19 18:26:52 +0530

    project dashboard

--- src/components/admin/index.ts
+++ src/components/admin/index.ts
@@ -3,3 +3,4 @@ export { default as WelcomeUser } from "./WelcomeUser";
 export { default as EmployDashboard } from "./EmployDashboard";
 export { default as EmplyeesGrid } from "./EmplyeesGrid";
 export { default as EmployeesColumn } from "./EmployeesColumn";
+export { default as ProjectDashBoard } from "./ProjectDashBoard";
--- src/pages/admin/projects.tsx
+++ src/pages/admin/projects.tsx
@@ -1,9 +1,11 @@
+import { ProjectDashBoard } from "components/admin";
 import PanelLayout from "layouts/panel";
 const Projects = () => {
   return (
     <PanelLayout title="Projects - SY HR MS">
-      <div>Projects</div>
-      <h1>Work on Progress</h1>
+      <>
+        <ProjectDashBoard />
+      </>
     </PanelLayout>
   );
 };
