0af95a85cef5070a76d3c0dd721b57f77e6c05d6
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-04-24 12:49:16 +0530

    employee dashboard onGoing

--- src/components/admin/EmployDashboard.tsx
+++ src/components/admin/EmployDashboard.tsx
@@ -1,154 +1,120 @@
 import {
-  AccountTreeRounded,
-  DoNotTouchRounded,
-  HourglassBottomRounded,
-  People,
-  PlaylistAddCheckCircleRounded,
+	AccountTreeRounded,
+	DoNotTouchRounded,
+	HourglassBottomRounded,
+	People,
+	PlaylistAddCheckCircleRounded,
 } from "@mui/icons-material";
 import {
-  Grid,
-  IconButton,
-  LinearProgress,
-  Menu,
-  MenuItem,
+	Grid,
+	IconButton,
+	LinearProgress,
+	Menu,
+	MenuItem,
 } from "@mui/material";
 import { DEFAULTIMG } from "assets/home";
 import ICONS from "assets/icons";
+import AttendanceReport from "components/analytics/AttendanceReport";
+import DailyAttendance from "components/analytics/DailyAttendance";
 import React from "react";
 
 const EmployDashboard = () => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  return (
-    <div className="flex gap-2 py-4">
-      <div className="w-3/4 px-4 ">
-        <Grid container spacing={2}>
-          {cards?.map((item) => (
-            <Grid key={item?.id} item lg={3}>
-              <div className="h-56 bg-white w-full p-8 flex flex-col rounded-xl shadow-xl justify-between border-b-4 border-theme">
-                <div className="flex justify-end">
-                  <IconButton size="small" onClick={handleClick}>
-                    <ICONS.More />
-                  </IconButton>
-                  <Menu
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={handleClose}
-                    MenuListProps={{
-                      "aria-labelledby": "basic-button",
-                    }}
-                  >
-                    <MenuItem onClick={handleClose}>All Users</MenuItem>
-                    <MenuItem onClick={handleClose}>View Dashboard</MenuItem>
-                  </Menu>
-                </div>
-                <div>{item?.icon}</div>
-                <span className="text-lg">{item?.count}</span>
-                <span className="font-semibold tracking-wide text-sm">
-                  {item?.title}
-                </span>
-              </div>
-            </Grid>
-          ))}
-        </Grid>
-        <div className="w-full mt-8">
-          <h1 className="font-bold text-theme">TODAY</h1>
-          <div className="flex flex-col gap-1">
-            <div className="py-4 w-full mt-4 rounded-xl bg-white flex justify-between items-center px-8 shadow-lg border-b-[1px] border-dashed border-theme">
-              <div className="flex gap-4 items-center">
-                <HourglassBottomRounded
-                  fontSize="large"
-                  className="!text-theme"
-                />
-                <span className="text-secondary-700 font-semibold">
-                  John Doe is off sick today!
-                </span>
-              </div>
-              <div className="h-14 w-14 rounded-full overflow-hidden border-2 border-secondary-200">
-                <img
-                  className="h-full object-cover "
-                  src={DEFAULTIMG.src}
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="py-4 w-full mt-4 rounded-xl bg-white flex justify-between items-center px-8 shadow-lg border-b-[1px] border-dashed border-theme">
-              <div className="flex gap-4 items-center">
-                <HourglassBottomRounded
-                  fontSize="large"
-                  className="!text-theme"
-                />
-                <span className="text-secondary-700 font-semibold">
-                  John Doe is off sick today!
-                </span>
-              </div>
-              <div className="h-14 w-14 rounded-full overflow-hidden border-2 border-secondary-200">
-                <img
-                  className="h-full object-cover "
-                  src={DEFAULTIMG.src}
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="w-1/4 p-3 bg-white rounded-xl shadow-xl flex flex-col gap-3">
-        <h1 className="mt-2 font-bold text-theme">PROJECTS</h1>
-        <div className="h-40 w-full border-2 rounded-xl py-4 px-6 flex tracking-wide">
-          <div className="w-1/2 border-r-2 h-full flex flex-col gap-3 justify-center items-center">
-            <span className="text-lg font-semibold text-theme">21</span>
-            <span className="w-1/2 text-center font-semibold">Total Tasks</span>
-          </div>
-          <div className="w-1/2 h-full flex flex-col gap-3 justify-center items-center">
-            <span className="text-lg font-semibold text-theme">21</span>
-            <span className="w-1/2 text-center font-semibold">
-              Pending Tasks
-            </span>
-          </div>
-        </div>
-        <h1 className="mt-2 font-bold text-theme ">UPCOMING HOLIDAY</h1>
-        <div className="w-full h-16 border-2 grid place-items-center text-sm tracking-wide rounded-xl border-secondary-600">
-          Mon 20 May 2023 - Ramzan
-        </div>
-      </div>
-    </div>
-  );
+	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	return (
+		<div className="flex gap-2 py-4">
+			<div className="w-3/4 px-4 ">
+				<Grid container spacing={2}>
+					{cards?.map((item) => (
+						<Grid key={item?.id} item lg={3}>
+							<div className="h-56 bg-white w-full p-8 flex flex-col rounded-xl shadow-xl justify-between border-b-4 border-theme">
+								<div className="flex justify-end">
+									<IconButton size="small" onClick={handleClick}>
+										<ICONS.More />
+									</IconButton>
+									<Menu
+										anchorEl={anchorEl}
+										open={open}
+										onClose={handleClose}
+										MenuListProps={{
+											"aria-labelledby": "basic-button",
+										}}
+									>
+										<MenuItem onClick={handleClose}>All Users</MenuItem>
+										<MenuItem onClick={handleClose}>View Dashboard</MenuItem>
+									</Menu>
+								</div>
+								<div>{item?.icon}</div>
+								<span className="text-lg">{item?.count}</span>
+								<span className="font-semibold tracking-wide text-sm">
+									{item?.title}
+								</span>
+							</div>
+						</Grid>
+					))}
+				</Grid>
+				<div className="grid grid-cols-12 content-between gap-6  m-5 !mb-6">
+					<div className="col-span-12 pt-9 w-full  gap-5 md:col-span-12 lg:col-span-12 !border-grey-500 rounded-xl !shadow-xl">
+						<DailyAttendance text="Repairs Report" type="area" />
+					</div>
+				</div>
+			</div>
+			<div className="w-1/4 p-3 bg-white rounded-xl shadow-xl flex flex-col gap-3">
+				<h1 className="mt-2 font-bold text-theme">PROJECTS</h1>
+				<div className="h-40 w-full border-2 rounded-xl py-4 px-6 flex tracking-wide">
+					<div className="w-1/2 border-r-2 h-full flex flex-col gap-3 justify-center items-center">
+						<span className="text-lg font-semibold text-theme">21</span>
+						<span className="w-1/2 text-center font-semibold">Total Tasks</span>
+					</div>
+					<div className="w-1/2 h-full flex flex-col gap-3 justify-center items-center">
+						<span className="text-lg font-semibold text-theme">21</span>
+						<span className="w-1/2 text-center font-semibold">
+							Pending Tasks
+						</span>
+					</div>
+				</div>
+				<h1 className="mt-2 font-bold text-theme ">UPCOMING HOLIDAY</h1>
+				<div className="w-full h-16 border-2 grid place-items-center text-sm tracking-wide rounded-xl border-secondary-600">
+					Mon 20 May 2023 - Ramzan
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default EmployDashboard;
 
 const cards = [
-  {
-    id: 1,
-    icon: <People fontSize="large" className="text-theme" />,
-    count: "34",
-    title: "Total Employees",
-  },
-  {
-    id: 2,
-    icon: <AccountTreeRounded fontSize="large" className="text-theme" />,
-    count: "34",
-    title: "Total projects",
-  },
-  {
-    id: 3,
-    icon: (
-      <PlaylistAddCheckCircleRounded fontSize="large" className="text-theme" />
-    ),
-    count: "34",
-    title: "Today's Attendance",
-  },
-  {
-    id: 4,
-    icon: <DoNotTouchRounded fontSize="large" className="text-theme" />,
-    count: "34",
-    title: "Absentees",
-  },
+	{
+		id: 1,
+		icon: <People fontSize="large" className="text-theme" />,
+		count: "34",
+		title: "Total Employees",
+	},
+	{
+		id: 2,
+		icon: <AccountTreeRounded fontSize="large" className="text-theme" />,
+		count: "34",
+		title: "Total projects",
+	},
+	{
+		id: 3,
+		icon: (
+			<PlaylistAddCheckCircleRounded fontSize="large" className="text-theme" />
+		),
+		count: "34",
+		title: "Today's Attendance",
+	},
+	{
+		id: 4,
+		icon: <DoNotTouchRounded fontSize="large" className="text-theme" />,
+		count: "34",
+		title: "Absentees",
+	},
 ];
---
+++ src/components/analytics/DailyAttendance.tsx
@@ -0,0 +1,120 @@
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import React from "react";
+const ApexCharts = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+const DailyAttendance = ({
+	type,
+	text = "",
+}: {
+	type: "bar" | "area" | "line";
+	text?: string;
+}) => {
+	const [monthlyData, setMonthlyData] = useState([]);
+	const months: any = {
+		1: "JAN",
+		2: "FEB",
+		3: "MAR",
+		4: "APR",
+		5: "MAY",
+		6: "JUN",
+		7: "JUL",
+		8: "AUG",
+		9: "SEP",
+		10: "OCT",
+		11: "NOV",
+		12: "DEC",
+	};
+	let mounted = false;
+
+	const options = {
+		series: [
+			{
+				name: "Present",
+				data: [31, 40, 28, 51, 42],
+			},
+			{
+				name: "Absent",
+				data: [11, 32, 45, 32, 34],
+			},
+		],
+		chart: {
+			height: 350,
+			type: "area",
+		},
+		dataLabels: {
+			enabled: false,
+		},
+		stroke: {
+			curve: "smooth",
+		},
+		xaxis: {
+			type: "category",
+			categories: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+		},
+		tooltip: {
+			x: {
+				// format: "dd/MM/yy HH:mm",
+			},
+		},
+		title: {
+			text: "Attendance",
+			floating: true,
+			offsetY: 1,
+			align: "center",
+			style: {
+				color: "#444",
+			},
+		},
+	};
+
+	return (
+		<ApexCharts
+			height={"500"}
+			options={{
+				series: [
+					{
+						name: "Present",
+						data: [31, 40, 28, 51, 42],
+					},
+					{
+						name: "Absent",
+						data: [11, 32, 45, 32, 34],
+					},
+				],
+				chart: {
+					height: 350,
+					type: "area",
+				},
+				dataLabels: {
+					enabled: false,
+				},
+				stroke: {
+					curve: "smooth",
+				},
+				xaxis: {
+					type: "category",
+					categories: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+				},
+				tooltip: {
+					x: {
+						// format: "dd/MM/yy HH:mm",
+					},
+				},
+				title: {
+					text: "Attendance",
+					floating: true,
+					offsetY: 1,
+					align: "center",
+					style: {
+						color: "#444",
+					},
+				},
+			}}
+			series={options.series}
+			type={type}
+		/>
+	);
+};
+
+export default DailyAttendance;
