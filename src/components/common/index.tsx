475a9272c20b5504f2635e1dbc7b382c8c20c606
KumarGudu <kg150201@gmail.com>
2023-04-24 15:56:11 +0530

    emal copy btn

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -33,23 +33,6 @@ export function RenderIconRow({
           <ICONS.Copy />
         </button>
       </div>
-      {/* <div className="group flex items-center gap-2">
-           {isPhone ? (
-             <a href={`tel:${value}`} className="hover:text-blue-500">
-               <ICONS.Call />
-             </a>
-           ) : null}
-           <span>{value}</span>
-           <button
-             onClick={(e) => {
-               e.stopPropagation()
-               navigator.clipboard.writeText(value)
-             }}
-             className="scale-0 rounded p-1 transition hover:bg-indigo-100 active:bg-green-100 group-hover:scale-100 "
-           >
-             <ICONS.Copy />
-           </button>
-         </div> */}
     </>
   );
 }
