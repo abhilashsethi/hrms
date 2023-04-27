d2d856e34d53112a737d993511347bdaee5960bc
Srinu Reddy <73178183+srinureddy7@users.noreply.github.com>
2023-04-27 12:20:25 +0530

    react lottie animation .

--- src/components/admin/AttendanceDashBoard.tsx
+++ src/components/admin/AttendanceDashBoard.tsx
@@ -1,11 +1,41 @@
-import { Groups, HowToReg, MoreVert, PersonOff } from "@mui/icons-material";
+import {
+  Check,
+  Close,
+  Groups,
+  HowToReg,
+  MoreVert,
+  PersonOff,
+} from "@mui/icons-material";
 import { Grid, IconButton, Menu, MenuItem } from "@mui/material";
 import DailyAttendance from "components/analytics/DailyAttendance";
 import GenderRation from "components/analytics/GenderRation";
-import RolewiseStrength from "components/analytics/RolewiseStrength";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+// import RolewiseStrength from "components/analytics/RolewiseStrength";
 import React from "react";
+import { HeadText } from "components/core";
+import EmpAttendanceIndividual from "./EmpAttendanceIndividual";
+import EmployProjects from "./EmployProjects";
+import EmployLeaves from "./EmployLeaves";
+import EmployeeAllAttendance from "./EmployeeAllAttendance";
 
 const AttendanceDashBoard = () => {
+  function renderEventContent(eventInfo: any) {
+    return (
+      <>
+        {eventInfo.event.title === "PRESENT" && (
+          <div className="flex flex-col">
+            <div className="flex justify-center bg-green-300 text-green-600">
+              PRESENT : 24
+            </div>
+            <div className="flex justify-center bg-red-300 text-red-600">
+              ABSENT : 04
+            </div>
+          </div>
+        )}
+      </>
+    );
+  }
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -17,70 +47,76 @@ const AttendanceDashBoard = () => {
   return (
     <>
       <div className="flex gap-2 py-4">
-        <div className="w-3/4 px-4 ">
-          <Grid container spacing={2}>
-            {cards?.map((item) => (
-              <Grid key={item?.id} item lg={4}>
-                <div className="group card1 h-36 bg-white w-full p-2 flex flex-col rounded-xl shadow-xl justify-between cursor-pointer">
-                  <div className="flex justify-end">
-                    <IconButton size="small" onClick={handleClick}>
-                      <MoreVert className="group-hover:text-white" />
-                    </IconButton>
-                    <Menu
-                      anchorEl={anchorEl}
-                      open={open}
-                      onClose={handleClose}
-                      MenuListProps={{
-                        "aria-labelledby": "basic-button",
-                      }}
-                    >
-                      <MenuItem onClick={handleClose}>All Users</MenuItem>
-                      <MenuItem onClick={handleClose}>View Dashboard</MenuItem>
-                    </Menu>
-                  </div>
-                  <div className="flex justify-around items-center">
-                    <div>{item?.icon}</div>
-                    <span className="text-xl text-theme font-semibold group-hover:text-white">
-                      {item?.count}
-                    </span>
-                  </div>
-                  <span className="group-hover:text-white text-theme font-semibold text-center tracking-wide text-lg">
-                    {item?.title}
-                  </span>
-                </div>
+        <Grid container spacing={1}>
+          <Grid item lg={8}>
+            <div className="w-full px-4 ">
+              <Grid container spacing={2}>
+                {cards?.map((item) => (
+                  <Grid key={item?.id} item lg={4}>
+                    <div className="group card1 h-36 bg-white w-full p-2 flex flex-col rounded-xl shadow-xl justify-between cursor-pointer">
+                      <div className="flex justify-end">
+                        <IconButton size="small" onClick={handleClick}>
+                          <MoreVert className="group-hover:text-white" />
+                        </IconButton>
+                        <Menu
+                          anchorEl={anchorEl}
+                          open={open}
+                          onClose={handleClose}
+                          MenuListProps={{
+                            "aria-labelledby": "basic-button",
+                          }}
+                        >
+                          <MenuItem onClick={handleClose}>All Users</MenuItem>
+                          <MenuItem onClick={handleClose}>
+                            View Dashboard
+                          </MenuItem>
+                        </Menu>
+                      </div>
+                      <div className="flex justify-around items-center">
+                        <div>{item?.icon}</div>
+                        <span className="text-xl text-theme font-semibold group-hover:text-white">
+                          {item?.count}
+                        </span>
+                      </div>
+                      <span className="group-hover:text-white text-theme font-semibold text-center tracking-wide text-lg">
+                        {item?.title}
+                      </span>
+                    </div>
+                  </Grid>
+                ))}
               </Grid>
-            ))}
+              <div className="mt-6">
+                <div className="mb-4">
+                  <HeadText title="Monthwise attendance" />
+                </div>
+                <div className="bg-white p-4 rounded-lg shadow-xl">
+                  <FullCalendar
+                    plugins={[dayGridPlugin]}
+                    initialView="dayGridMonth"
+                    weekends={true}
+                    eventContent={renderEventContent}
+                    events={attendance}
+                  />
+                </div>
+              </div>
+            </div>
           </Grid>
-          <div className="grid grid-cols-12 content-between gap-6  m-5 !mb-6">
-            <div className="col-span-12 pt-9 w-full  gap-5 md:col-span-12 lg:col-span-12 !border-grey-500 rounded-xl !shadow-xl">
-              <DailyAttendance text="Attendance" type="area" />
+          <Grid item lg={4}>
+            <div className="w-full h-full">
+              <div className="w-full h-full">
+                <EmployeeAllAttendance />
+                <EmployLeaves />
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="w-1/4 p-3 bg-white rounded-xl shadow-xl flex flex-col gap-3">
-          <h1 className="mt-2 font-bold text-theme">Attendance</h1>
-          <div className="h-40 w-full border-2 rounded-xl py-4 px-6 flex tracking-wide">
-            <div className="w-1/2 border-r-2 h-full flex flex-col gap-3 justify-center items-center">
-              <span className="text-lg font-semibold text-theme">21</span>
-              <span className="w-1/2 text-center font-semibold">
-                Total Present
-              </span>
-            </div>
-            <div className="w-1/2 h-full flex flex-col gap-3 justify-center items-center">
-              <span className="text-lg font-semibold text-theme">10</span>
-              <span className="w-1/2 text-center font-semibold">
-                Total Absent
-              </span>
-            </div>
-          </div>
-        </div>
+          </Grid>
+        </Grid>
       </div>
       <div className="grid grid-cols-12 content-between gap-6  m-5 !mb-6">
         <div className="col-span-12 pt-9 w-full  gap-5 md:col-span-12 lg:col-span-7 !border-grey-500 rounded-xl !shadow-xl">
-          <RolewiseStrength text="Role-wise Strength" type="bar" />
+          <DailyAttendance text="Attendance from last week" type="area" />
         </div>
         <div className="col-span-12 pt-9 w-full flex flex-col justify-center gap-5 md:col-span-12 lg:col-span-5 !border-gray-500 rounded-xl !shadow-xl">
-          <GenderRation text="Employee Gender Ratio" type="donut" />
+          <GenderRation text="Today's Attendance" type="donut" />
         </div>
       </div>
     </>
@@ -95,7 +131,7 @@ const cards = [
     icon: (
       <HowToReg
         fontSize="large"
-        className="text-theme text-[3rem] group-hover:text-white shadow-xl rounded-lg"
+        className="text-theme group-hover:text-white shadow-xl rounded-lg"
       />
     ),
     count: "34",
@@ -106,7 +142,7 @@ const cards = [
     icon: (
       <PersonOff
         fontSize="large"
-        className="text-theme text-[3rem] group-hover:text-white shadow-xl rounded-lg"
+        className="text-theme group-hover:text-white shadow-xl rounded-lg"
       />
     ),
     count: "10",
@@ -117,10 +153,31 @@ const cards = [
     icon: (
       <Groups
         fontSize="large"
-        className="text-theme text-[3rem] group-hover:text-white shadow-xl rounded-lg"
+        className="text-theme group-hover:text-white shadow-xl rounded-lg"
       />
     ),
     count: "44",
     title: "Total Employees",
   },
+];
+
+const attendance = [
+  { title: "PRESENT", date: "2023-04-03" },
+  { title: "PRESENT", date: "2023-04-04" },
+  { title: "PRESENT", date: "2023-04-05" },
+  { title: "PRESENT", date: "2023-04-06" },
+  { title: "PRESENT", date: "2023-04-07" },
+  { title: "PRESENT", date: "2023-04-10" },
+  { title: "PRESENT", date: "2023-04-11" },
+  { title: "PRESENT", date: "2023-04-12" },
+  { title: "PRESENT", date: "2023-04-13" },
+  { title: "PRESENT", date: "2023-04-14" },
+  { title: "PRESENT", date: "2023-04-17" },
+  { title: "PRESENT", date: "2023-04-18" },
+  { title: "PRESENT", date: "2023-04-19" },
+  { title: "PRESENT", date: "2023-04-20" },
+  { title: "PRESENT", date: "2023-04-21" },
+  { title: "PRESENT", date: "2023-04-24" },
+  { title: "PRESENT", date: "2023-04-25" },
+  { title: "PRESENT", date: "2023-04-26" },
 ];
