29e116ccc1ea571f0bf30e3a13659b3e212eac58
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-05-03 11:03:13 +0530

    client dashboard ui onGoing

--- src/components/analytics/ClientLineCharts.tsx
+++ src/components/analytics/ClientLineCharts.tsx
@@ -3,102 +3,88 @@ import { useState } from "react";
 const ApexCharts = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 const ClientLineCharts = ({
-  type,
-  text = "",
+	type,
+	text = "",
 }: {
-  type: "bar" | "area" | "line";
-  text?: string;
+	type: "bar" | "area" | "line" | "donut";
+	text?: string;
 }) => {
-  const options = {
-    series: [
-      {
-        name: "Block",
-        data: [31, 40, 28, 51, 42],
-      },
-      {
-        name: "Unblock",
-        data: [11, 32, 45, 32, 34],
-      },
-    ],
-    chart: {
-      height: 350,
-      type: "area",
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      curve: "smooth",
-    },
-    xaxis: {
-      type: "category",
-      categories: ["Mon", "Tue", "Wed", "Thu", "Fri"],
-    },
-    tooltip: {
-      x: {
-        // format: "dd/MM/yy HH:mm",
-      },
-    },
-    title: {
-      text: text,
-      floating: true,
-      offsetY: 1,
-      align: "center",
-      style: {
-        color: "#444",
-      },
-    },
-  };
+	const options = {
+		labels: ["Blocked", "Un-Blocked"],
+		series: [120, 80],
+		chart: {
+			type: "donut",
+		},
+		responsive: [
+			{
+				breakpoint: 480,
+				options: {
+					chart: {
+						width: 200,
+					},
+					legend: {
+						position: "bottom",
+					},
+				},
+			},
+		],
+		colors: ["#b00b13", "#005d32"],
+		title: {
+			text: text,
+			floating: true,
+			offsetY: -40,
+			offsetX: -50,
+			align: "center",
+			style: {
+				color: "#444",
+			},
+		},
+		legend: {
+			position: "bottom",
+		},
+	};
 
-  return (
-    <ApexCharts
-      height={"500"}
-      options={{
-        series: [
-          {
-            name: "Block",
-            data: [31, 40, 28, 51, 42],
-          },
-          {
-            name: "Unblock",
-            data: [11, 32, 45, 32, 34],
-          },
-        ],
-        chart: {
-          height: 350,
-          type: "area",
-        },
-        dataLabels: {
-          enabled: false,
-        },
-        stroke: {
-          curve: "smooth",
-        },
-        xaxis: {
-          type: "category",
-          categories: ["Mon", "Tue", "Wed", "Thu", "Fri"],
-        },
-        tooltip: {
-          x: {
-            // format: "dd/MM/yy HH:mm",
-          },
-        },
-        colors: ["#106EAD", "#C33C5E", "#25d366", "#BD33B5", "#E60023"],
-
-        title: {
-          text: text,
-          floating: true,
-          offsetY: 1,
-          align: "center",
-          style: {
-            color: "#444",
-          },
-        },
-      }}
-      series={options.series}
-      type={type}
-    />
-  );
+	return (
+		<ApexCharts
+			height={"500"}
+			options={{
+				labels: ["Blocked", "Un-Blocked"],
+				series: [120, 80],
+				chart: {
+					type: "donut",
+				},
+				responsive: [
+					{
+						breakpoint: 480,
+						options: {
+							chart: {
+								width: 200,
+							},
+							legend: {
+								position: "bottom",
+							},
+						},
+					},
+				],
+				colors: ["#106ead", "#c33c5e"],
+				title: {
+					text: text,
+					floating: true,
+					offsetY: -40,
+					offsetX: -50,
+					align: "center",
+					style: {
+						color: "#444",
+					},
+				},
+				legend: {
+					position: "bottom",
+				},
+			}}
+			series={options.series}
+			type={type}
+		/>
+	);
 };
 
 export default ClientLineCharts;
