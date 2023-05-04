e83f42440c650b8e00b46b4496814bfc3f432ba9
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-05-04 18:45:52 +0530

    view document ui onProgress

--- src/components/drawer/index.ts
+++ src/components/drawer/index.ts
@@ -3,3 +3,4 @@ export { default as RoleInformation } from "./RoleInformation";
 export { default as SelectManager } from "./SelectManager";
 export { default as SelectMembers } from "./SelectMembers";
 export { default as ProjectURLS } from "./ProjectURLS";
+export { default as ViewDocumentDrawer } from "./ViewDocumentDrawer";
