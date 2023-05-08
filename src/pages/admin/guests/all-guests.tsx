f5c7f363467ce419551093ef8b92dbd82dfc9e16
Srinu Reddy <73178183+srinureddy7@users.noreply.github.com>
2023-05-08 15:43:49 +0530

    guests menus added.

---
+++ src/pages/admin/guests/all-guests.tsx
@@ -0,0 +1,13 @@
+import PanelLayout from "layouts/panel";
+
+const AllGuests = () => {
+  return (
+    <PanelLayout title="All Guests - Admin Panel">
+      <section className="h-[75vh] w-full flex justify-center items-center">
+        <p>Page in progress...</p>
+      </section>
+    </PanelLayout>
+  );
+};
+
+export default AllGuests;
--- src/pages/admin/roles/all-roles.tsx
+++ src/pages/admin/roles/all-roles.tsx
@@ -1,24 +1,34 @@
 import { Add, GridViewRounded, TableRowsRounded } from "@mui/icons-material";
-import { Button, IconButton } from "@mui/material";
+import { Button, IconButton, Pagination, Stack } from "@mui/material";
 import { AllRollColumn, AllRollGrid } from "components/admin";
-import { AdminBreadcrumbs, Loader, TextTitles } from "components/core";
+import {
+  AdminBreadcrumbs,
+  Loader,
+  LoaderAnime,
+  TextTitles,
+} from "components/core";
 import { CreateRole } from "components/dialogues";
 import { useFetch } from "hooks";
 import PanelLayout from "layouts/panel";
 import { useState } from "react";
+import { Role } from "types";
 
 const AllRoles = () => {
+  const [pageNumber, setPageNumber] = useState<number | null>(1);
   const [isGrid, setIsGrid] = useState(true);
   const [isCreate, setIsCreate] = useState(false);
   const [isUpdate, setisUpdate] = useState<{
     dialogue?: boolean;
     id?: string | null;
   }>({ dialogue: false, id: null });
+  // const { data: roleData, isLoading, mutate } = useFetch<Role>(`roles`);
   const {
     data: roleData,
     mutate,
     isLoading,
-  } = useFetch<[{ id: string; name: string }]>(`roles`);
+    pagination,
+  } = useFetch<any>(`roles?page=${pageNumber}&limit=8`);
+  console.log(roleData);
 
   return (
     <PanelLayout title="All Roles - Admin Panel">
@@ -62,9 +72,29 @@ const AllRoles = () => {
             </Button>
           </div>
         </div>
-        {/* <TextTitles title="ALL ROLES" /> */}
 
-        {isGrid ? <AllRollGrid /> : <AllRollColumn />}
+        {isGrid ? (
+          <AllRollGrid data={roleData?.roles} mutate={mutate} />
+        ) : (
+          <AllRollColumn />
+        )}
+        {!roleData?.roles?.length && <LoaderAnime />}
+        {roleData?.roles?.length ? (
+          <div className="flex justify-center py-8">
+            <Stack spacing={2}>
+              <Pagination
+                count={Math.ceil(
+                  Number(pagination?.total || 1) /
+                    Number(pagination?.limit || 1)
+                )}
+                onChange={(e, v: number) => {
+                  setPageNumber(v);
+                }}
+                variant="outlined"
+              />
+            </Stack>
+          </div>
+        ) : null}
       </section>
     </PanelLayout>
   );
