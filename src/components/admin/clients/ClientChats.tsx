bc4f51376c6b3e25f9a81dd7d3bae771f5219de1
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-05-11 16:48:06 +0530

    client ticket ui changed

--- src/components/admin/clients/ClientChats.tsx
+++ src/components/admin/clients/ClientChats.tsx
@@ -3,7 +3,7 @@ import moment from "moment";
 import { AccountTreeRounded, Add } from "@mui/icons-material";
 import { RenderIconRow } from "components/common";
 import { Avatar, AvatarGroup, Button, Tooltip } from "@mui/material";
-import { DEFAULTPROFILE } from "assets/home";
+import { DEFAULTPROFILE, DOC, IMG, PDF, XLS } from "assets/home";
 import { ViewTicketsDrawer } from "components/drawer";
 import React, { useState } from "react";
 
@@ -56,16 +56,34 @@ const ClientChats = () => {
 					Yes
 				</div>
 			</div>
-			<p className="font-bold mt-3">Assigned Members</p>
+			<p className="font-bold mt-3 mb-2">Assigned Members</p>
 			<div className="flex justify-start">
 				<AvatarGroup
 					className="!cursor-pointer"
 					//   onClick={() => setIsMembers({ dialogue: true })}
 					max={4}
 				>
-					<Avatar alt="Remy Sharp" src={DEFAULTPROFILE.src || " "} />
-					<Avatar alt="Remy Sharp" src={DEFAULTPROFILE.src || " "} />
-					<Avatar alt="Remy Sharp" src={DEFAULTPROFILE.src || " "} />
+					<Avatar
+						alt="Remy Sharp"
+						src={
+							"https://t4.ftcdn.net/jpg/02/14/74/61/360_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg" ||
+							" "
+						}
+					/>
+					<Avatar
+						alt="Remy Sharp"
+						src={
+							"https://image.shutterstock.com/image-photo/smiling-young-middle-eastern-man-260nw-2063524544.jpg" ||
+							" "
+						}
+					/>
+					<Avatar
+						alt="Remy Sharp"
+						src={
+							"https://us.123rf.com/450wm/gstockstudio/gstockstudio1702/gstockstudio170200322/71575852-how-may-i-help-you-good-looking-young-man-in-white-shirt-and-tie-working-on-his-digital-tablet-and.jpg?ver=6" ||
+							" "
+						}
+					/>
 					<Avatar alt="Remy Sharp" src={DEFAULTPROFILE.src || " "} />
 					<Avatar alt="Remy Sharp" src={DEFAULTPROFILE.src || " "} />
 				</AvatarGroup>
@@ -74,28 +92,33 @@ const ClientChats = () => {
 			<div className="flex justify-between">
 				<p className="font-bold mt-3 mb-2">Documents</p>
 				<Tooltip title="Add more">
-					<Button variant="contained" className="mt-5 bg-theme" size="small">
-						<Add />
-					</Button>
+					<button className="mt-5 bg-white text-theme hover:scale-95 transition duration-300 ease-in-out hover:bg-theme hover:text-white border border-theme rounded-lg px-4">
+						<Add className="" />
+					</button>
 				</Tooltip>
 			</div>
-			<div className="grid grid-cols-3 w-2/3 gap-y-2">
+			<div className="grid grid-cols-3 w-2/3 gap-6">
 				<div className="cursor-pointer">
-					<img className="w-12" src="/docs.png" alt="" />
+					<img className="w-12" src={PDF.src} alt="" />
+					<p className="text-xs">doc_1002...</p>
+				</div>
+				<div className="cursor-pointer">
+					<img className="w-12" src={DOC.src} alt="" />
+					<p className="text-xs">doc_1003...</p>
+				</div>
+				<div className="cursor-pointer">
+					<img className="w-12" src={XLS.src} alt="" />
+					<p className="text-xs">doc_1004...</p>
+				</div>
+				<div className="cursor-pointer">
+					<img className="w-12" src={IMG.src} alt="" />
+					<p className="text-xs">doc_1005...</p>
 				</div>
 				<div className="cursor-pointer">
 					<img className="w-12" src="/docs.png" alt="" />
+					<p className="text-xs">doc_1006...</p>
 				</div>
-				<div className="cursor-pointer">
-					<img className="w-12" src="/docs.png" alt="" />
-				</div>
-				<div className="cursor-pointer">
-					<img className="w-12" src="/docs.png" alt="" />
-				</div>
-				<div className="cursor-pointer">
-					<img className="w-12" src="/docs.png" alt="" />
-				</div>
-				<div className="border border-gray-400 h-8 mt-3 flex justify-center items-center rounded-lg text-sm bg-theme text-white hover:scale-95 transition duration-300 ease-in-out">
+				<div className="border border-theme h-8 mt-3 flex justify-center items-center rounded-lg text-sm text-theme hover:scale-95 transition duration-300 ease-in-out hover:bg-theme hover:text-white">
 					<button onClick={() => setTickets(true)}>View All</button>
 				</div>
 			</div>
