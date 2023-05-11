c47109ceac5545ad5cbeadb42f44345e7b16a952
Srinu Reddy <73178183+srinureddy7@users.noreply.github.com>
2023-05-11 17:34:10 +0530

    technologies menu added.

---
+++ src/pages/admin/technologies/all-technologies.tsx
@@ -0,0 +1,11 @@
+import PanelLayout from "layouts/panel";
+
+const AllTechnologies = () => {
+  return (
+    <PanelLayout title="Technologies - HRMS Searchingyard">
+      <section>Page in progress...</section>
+    </PanelLayout>
+  );
+};
+
+export default AllTechnologies;
---
+++ src/pages/admin/technologies/index.tsx
@@ -0,0 +1,11 @@
+import PanelLayout from "layouts/panel";
+
+const Technologies = () => {
+  return (
+    <PanelLayout title="Technologies - HRMS Searchingyard">
+      <section>Page in progress...</section>
+    </PanelLayout>
+  );
+};
+
+export default Technologies;
