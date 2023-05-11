f9df9780db7b8fa143ca6a6e1fc1a11e12b4ae6a
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-05-11 15:40:57 +0530

    ticket details page ui completed

--- src/components/admin/clients/TicketDetails.tsx
+++ src/components/admin/clients/TicketDetails.tsx
@@ -9,13 +9,15 @@ import { useState } from "react";
 import { User } from "types";
 import ClientChats from "./ClientChats";
 
+import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
+import { Send } from "@mui/icons-material";
+import { ViewTicketsDrawer } from "components/drawer";
+const ReactQuill = dynamic(import("react-quill"), { ssr: false });
+
 const TicketDetails = () => {
 	const router = useRouter();
-	const [isDialogue, setIsDialogue] = useState(false);
-	const [isProfile, setIsProfile] = useState(false);
-	const [isPersonal, setIsPersonal] = useState(false);
-	const [isBank, setIsBank] = useState(false);
-	const [tickets, setTickets] = useState(false);
+	const [value, setValue] = useState("");
 	const [viewTickets, setViewTickets] = useState<any>(null);
 	const {
 		data: employData,
@@ -31,107 +33,73 @@ const TicketDetails = () => {
 			<section className="mb-12 flex gap-3">
 				<Grid container spacing={2}>
 					<Grid item lg={8}>
-						<div className="w-full h-full rounded-lg bg-white shadow-xl p-4">
-							<div className="w-full bg-blue-100/50 rounded-lg p-8">
-								<Grid container spacing={3}>
-									<Grid item lg={9}>
-										<div className="tracking-wide w-full h-full">
-											<div className="flex items-center gap-2 -ml-5">
-												<div className="h-3 w-3 rounded-sm bg-theme"></div>
-												<p className="font-bold tracking-wide">
-													Requester Details
-												</p>
-											</div>
-											<p className="text-md text-slate-600 font-medium mt-1">
-												{employData?.name || "John Smith"}
-											</p>
-											{/* <p className="text-sm text-slate-600 font-medium mt-1">
-												{employData?.role?.name || "Client" || "---"}
-											</p> */}
-
-											<p className="text-sm text-slate-600 font-medium mt-1 flex items-center gap-3">
-												<RenderIconRow
-													value={
-														employData?.email || "johnsmith@gmail.com" || "---"
-													}
-													isEmail
-												/>
-											</p>
-											<p className="text-sm text-slate-600 font-medium mt-1 flex items-center gap-3">
-												<RenderIconRow
-													value={employData?.phone || "9974521485" || "---"}
-													isPhone
-												/>
-											</p>
-										</div>
-									</Grid>
-									<Grid item lg={3}>
-										{/* <div className="w-full h-full flex justify-center items-center">
-											<div className="h-24 w-24 rounded-full border-[4px] border-white flex justify-center items-center text-3xl">
-												<div className="relative h-full w-full flex justify-center items-center group">
-													{employData?.photo && (
-														<div className="h-full w-full bg-slate-300 rounded-full">
-															<img
-																className="h-full w-full object-cover rounded-full shadow-md"
-																src={
-																	employData?.photo ||
-																	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwzTSDO6sbQw9RJmGwMKYaubB1wDwyqOYAfuWM1fg&s"
-																}
-																alt="John Doe"
-															/>
-														</div>
-													)}
-													{!employData?.photo && (
-														<div className="h-full w-full text-white rounded-full uppercase shadow-lg flex justify-center items-center text-4xl font-bold bg-gradient-to-br from-theme-100 via-theme-50 to-secondary-100">
-															{employData?.name.slice(0, 1)}
-														</div>
-													)}
-													<div
-														onClick={() => setIsProfile(true)}
-														className="absolute cursor-pointer rounded-full w-full h-full group-hover:flex transition-all ease-in-out duration-300 justify-center items-center hidden  bg-[#0007]"
-													>
-														<Edit className="!text-white" />
+						<div className="w-full bg-white shadow-xl rounded-lg p-8 mt-4">
+							<div className="flex items-center gap-2 -ml-5">
+								<div className="h-3 w-3 rounded-sm bg-theme"></div>
+								<p className="font-bold tracking-wide">Conversations</p>
+							</div>
+							<Grid container spacing={3}>
+								<Grid item lg={12}>
+									<div className="flex flex-col gap-1 mt-4 max-h-[20rem] overflow-y-auto">
+										{chats?.map((item, i) => (
+											<div
+												key={i}
+												className="flex gap-3 py-3 px-1 border-b-[1px]"
+											>
+												<div className="flex justify-start items-center">
+													<div className=" bg-theme-100 rounded-full flex justify-center items-center">
+														{item?.icon}
 													</div>
 												</div>
+												<div className="">
+													<div className="flex relative pr-3 items-center">
+														<p className="text-sm font-semibold tracking-wide">
+															{item?.name}
+														</p>
+														<p className="pr-3 text-xs font-semibold text-gray-500 place-content-end tracking-wide absolute right-0">
+															{item?.time}
+														</p>
+													</div>
+													<p className="text-sm tracking-wide">
+														{/* Deadline : {moment(new Date()).format("ll")} */}
+														{item?.details}
+													</p>
+												</div>
 											</div>
-										</div> */}
-										<PhotoViewer
-											photo={
-												"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-											}
-											size="5.5rem"
-										/>
-									</Grid>
-								</Grid>
-								<div className="flex justify-between items-center pt-4">
-									<div className="w-1/2 flex justify-between gap-3 mb-7">
-										<div className="hover:scale-95 transition duration-500 ease-in-out cursor-pointer border border-gray-600 text-center w-1/2 py-5 rounded-md bg-slate-200 shadow-lg">
-											<p className={`text-xs  font-bold`}>PROJECTS COMPLETED</p>
-											<p className="text-md font-semibold">16</p>
-										</div>
-										<div className="hover:scale-95 transition duration-500 ease-in-out cursor-pointer border border-gray-600 text-center w-1/2 py-5 rounded-md bg-slate-200 shadow-lg">
-											<p className={`text-xs  font-bold`}>ONGOING PROJECTS</p>
-											<p className="text-md font-semibold">2</p>
-										</div>
+										))}
 									</div>
-								</div>
-								<div className="font-bold mb-1">Ticket Description :</div>
-								<p className="text-gray-600 font-medium text-sm">
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-									Voluptates adipisci a facere voluptatum debitis totam
-									asperiores temporibus cumque commodi labore consectetur fugiat
-									nihil eveniet doloribus doloremque corrupti voluptate, error
-									beatae ea culpa tempore nemo eum. Enim rerum nobis quaerat
-									molestiae.
-								</p>
+								</Grid>
+							</Grid>
+						</div>
+						<div className="mt-8">
+							<ReactQuill
+								placeholder="Reply message ..."
+								theme="snow"
+								value={value}
+								// onChange={(e: any) => {
+								//   setValue(e);
+								//   formik.setFieldValue("content", e);
+								// }}
+								style={{
+									height: "150px",
+									lineHeight: "0px",
+								}}
+							/>
+							<div className="flex justify-end items-end w-full pr-2">
+								<Button
+									variant="contained"
+									className="!bg-emerald-500 "
+									startIcon={<Send />}
+									size="small"
+								>
+									Send Message
+								</Button>
 							</div>
 						</div>
 					</Grid>
 					<Grid item lg={4}>
 						<div className="w-full h-full">
 							<ClientChats />
-							{/* <ClientProjects /> */}
-							{/* <EmployLeaves /> */}
 						</div>
 					</Grid>
 				</Grid>
@@ -141,3 +109,102 @@ const TicketDetails = () => {
 };
 
 export default TicketDetails;
+
+const chats = [
+	{
+		id: 1,
+		icon: (
+			<PhotoViewer
+				photo={
+					"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+				}
+				size="3rem"
+			/>
+		),
+		time: "Today at 9:00 AM",
+		name: "John Smith",
+		details:
+			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas nobis id tempora impedit facere cupiditate rem officiis repellendus quasi delectus!",
+		status: "COMPLETED",
+	},
+	{
+		id: 2,
+		icon: (
+			<PhotoViewer
+				photo={
+					"https://t4.ftcdn.net/jpg/02/14/74/61/360_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg"
+				}
+				size="3rem"
+			/>
+		),
+		time: "Today at 9:00 AM",
+		name: "Shrinu Readdy",
+		details:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, deserunt aliquam accusantium porro eligendi iusto expedita fuga veritatis consequuntur exercitationem!",
+		status: "COMPLETED",
+	},
+	{
+		id: 3,
+		icon: (
+			<PhotoViewer
+				photo={
+					"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+				}
+				size="3rem"
+			/>
+		),
+		time: "Today at 9:00 AM",
+		name: "John Smith",
+		details:
+			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas nobis id tempora impedit facere cupiditate rem officiis repellendus quasi delectus!",
+		status: "COMPLETED",
+	},
+	{
+		id: 4,
+		icon: (
+			<PhotoViewer
+				photo={
+					"https://t4.ftcdn.net/jpg/02/14/74/61/360_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg"
+				}
+				size="3rem"
+			/>
+		),
+		time: "Today at 9:00 AM",
+		name: "Shrinu Readdy",
+		details:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, deserunt aliquam accusantium porro eligendi iusto expedita fuga veritatis consequuntur exercitationem!",
+		status: "COMPLETED",
+	},
+	{
+		id: 5,
+		icon: (
+			<PhotoViewer
+				photo={
+					"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+				}
+				size="3rem"
+			/>
+		),
+		time: "Today at 9:00 AM",
+		name: "John Smith",
+		details:
+			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas nobis id tempora impedit facere cupiditate rem officiis repellendus quasi delectus!",
+		status: "COMPLETED",
+	},
+	{
+		id: 6,
+		icon: (
+			<PhotoViewer
+				photo={
+					"https://t4.ftcdn.net/jpg/02/14/74/61/360_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg"
+				}
+				size="3rem"
+			/>
+		),
+		time: "Today at 9:00 AM",
+		name: "Shrinu Readdy",
+		details:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, deserunt aliquam accusantium porro eligendi iusto expedita fuga veritatis consequuntur exercitationem!",
+		status: "COMPLETED",
+	},
+];
