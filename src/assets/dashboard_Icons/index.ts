c0b8437cb9f59cd3acc6c698986ad48dbb05b683
Srinu Reddy <73178183+srinureddy7@users.noreply.github.com>
2023-05-15 12:14:05 +0530

    meeting dashboard cards design changed.

---
+++ src/assets/dashboard_Icons/index.ts
@@ -0,0 +1,4 @@
+export { default as MEETINGICON } from "./meetingicon.png";
+export { default as MEETINGICON2 } from "./meetingicon2.png";
+export { default as MEETINGICON3 } from "./meetingicon3.png";
+export { default as MEETINGICON4 } from "./meetingicon4.png";
--- src/components/admin/MeetingDashBoard.tsx
+++ src/components/admin/MeetingDashBoard.tsx
@@ -1,67 +1,24 @@
-import { Grid, IconButton, Menu, MenuItem } from "@mui/material";
 import { MeetingAnalytics, MeetingDonutChart } from "components/analytics";
-import { useState, MouseEvent } from "react";
 import {
-	ContactPhone,
-	DevicesOther,
-	AssignmentTurnedIn,
-	PendingActions,
-	MoreVert,
+  ContactPhone,
+  DevicesOther,
+  AssignmentTurnedIn,
+  PendingActions,
 } from "@mui/icons-material";
+import { DashboardCard } from "components/core";
+import {
+  MEETINGICON,
+  MEETINGICON2,
+  MEETINGICON3,
+  MEETINGICON4,
+} from "assets/dashboard_Icons";
 
 const MeetingDashBoard = () => {
-	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-	const open = Boolean(anchorEl);
-	const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
-		setAnchorEl(event.currentTarget);
-	};
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
-
-	return (
-		<>
-			<div className="flex gap-2 py-4">
-				<div className="w-full px-4 ">
-					<Grid container spacing={2}>
-						{cards?.map((item) => (
-							<Grid key={item?.id} item lg={3}>
-								<div className="hover:scale-105 transition duration-500 ease-in-out bg-white w-full p-2 flex flex-col rounded-xl shadow-xl justify-between cursor-pointer border-4 border-b-theme">
-									<div className="flex justify-end">
-										<IconButton size="small" onClick={handleClick}>
-											<MoreVert />
-										</IconButton>
-										<Menu
-											anchorEl={anchorEl}
-											open={open}
-											onClose={handleClose}
-											MenuListProps={{
-												"aria-labelledby": "basic-button",
-											}}
-										>
-											<MenuItem onClick={handleClose}>All Users</MenuItem>
-											<MenuItem onClick={handleClose}>View Dashboard</MenuItem>
-										</Menu>
-									</div>
-									<div className="flex justify-around items-center pb-3">
-										<div className="w-16">{item?.icon}</div>
-
-										{/* <div>
-										<img className="w-16" src="/department.png" alt="" />
-									</div> */}
-										<span className="text-xl text-theme font-semibold ">
-											{item?.count}
-										</span>
-									</div>
-									<span className=" text-theme font-semibold text-center tracking-wide text-md">
-										{item?.title}
-									</span>
-								</div>
-							</Grid>
-						))}
-					</Grid>
-				</div>
-				{/* <div className="w-1/4 p-2 rounded-xl shadow-xl flex flex-col gap-3">
+  return (
+    <>
+      <div className="flex gap-2 py-4">
+        <DashboardCard data={cards} />
+        {/* <div className="w-1/4 p-2 rounded-xl shadow-xl flex flex-col gap-3">
 					{stats.map((item) => (
 						<div
 							key={item?.id}
@@ -83,70 +40,78 @@ const MeetingDashBoard = () => {
 
 					<MeetingStats />
 				</div> */}
-			</div>
-			<div className="grid grid-cols-12 content-between gap-6  m-5 !mb-6">
-				<div className="col-span-12 pt-9 w-full  gap-5 md:col-span-12 lg:col-span-7 !border-grey-500 rounded-xl !shadow-xl">
-					<p className="font-bold text-lg text-center">Meeting Overview</p>
-					<MeetingAnalytics text="" type="line" />
-				</div>
-				<div className="col-span-12 pt-9 w-full flex flex-col justify-center gap-5 md:col-span-12 lg:col-span-5 !border-gray-500 rounded-xl !shadow-xl">
-					<p className="font-bold text-lg text-center">Meeting Details</p>
-					<MeetingDonutChart text="" type="donut" />
-				</div>
-			</div>
-		</>
-	);
+      </div>
+      <div className="grid grid-cols-12 content-between gap-6  m-5 !mb-6">
+        <div className="col-span-12 pt-9 w-full  gap-5 md:col-span-12 lg:col-span-7 !border-grey-500 rounded-xl !shadow-xl">
+          <p className="font-bold text-lg text-center">Meeting Overview</p>
+          <MeetingAnalytics text="" type="line" />
+        </div>
+        <div className="col-span-12 pt-9 w-full flex flex-col justify-center gap-5 md:col-span-12 lg:col-span-5 !border-gray-500 rounded-xl !shadow-xl">
+          <p className="font-bold text-lg text-center">Meeting Details</p>
+          <MeetingDonutChart text="" type="donut" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MeetingDashBoard;
 
 const cards = [
-	{
-		id: 1,
-		icon: <ContactPhone fontSize="large" className="text-theme" />,
-		count: "34",
-		title: "Total Meetings",
-	},
-	{
-		id: 2,
-		icon: <PendingActions fontSize="large" className="text-theme" />,
-		count: "20",
-		title: "Upcoming Meetings",
-	},
-	{
-		id: 3,
-		icon: <AssignmentTurnedIn fontSize="large" className="text-theme" />,
-		count: "10",
-		title: "Completed Meetings",
-	},
-	{
-		id: 4,
-		icon: <DevicesOther fontSize="large" className="text-theme" />,
-		count: "18",
-		title: "Total Meeting Locations",
-	},
+  {
+    id: 1,
+    icon: <ContactPhone className="text-theme" />,
+    count: "34",
+    title: "Total Meetings",
+    img: MEETINGICON.src,
+    bg: "from-blue-500 to-blue-300",
+  },
+  {
+    id: 2,
+    icon: <PendingActions className="text-theme" />,
+    count: "20",
+    title: "Upcoming Meetings",
+    bg: "from-yellow-500 to-yellow-300",
+    img: MEETINGICON2.src,
+  },
+  {
+    id: 3,
+    icon: <AssignmentTurnedIn className="text-theme" />,
+    count: "10",
+    title: "Completed Meetings",
+    bg: "from-emerald-500 to-emerald-300",
+    img: MEETINGICON3.src,
+  },
+  {
+    id: 4,
+    icon: <DevicesOther className="text-theme" />,
+    count: "18",
+    title: "Total Meeting Locations",
+    bg: "from-purple-500 to-purple-300",
+    img: MEETINGICON4.src,
+  },
 ];
 
 const stats = [
-	{
-		id: 1,
-		title: "Upcoming Meetings",
-		growth: "+10%",
-		value: "10",
-		sub: "Overall Meetings 218",
-	},
-	{
-		id: 2,
-		title: "Pending Meetings",
-		growth: "+10%",
-		value: "10",
-		sub: "Overall Meetings 218",
-	},
-	{
-		id: 3,
-		title: "Dome Meetings",
-		growth: "+10%",
-		value: "10",
-		sub: "Overall Meetings 218",
-	},
+  {
+    id: 1,
+    title: "Upcoming Meetings",
+    growth: "+10%",
+    value: "10",
+    sub: "Overall Meetings 218",
+  },
+  {
+    id: 2,
+    title: "Pending Meetings",
+    growth: "+10%",
+    value: "10",
+    sub: "Overall Meetings 218",
+  },
+  {
+    id: 3,
+    title: "Dome Meetings",
+    growth: "+10%",
+    value: "10",
+    sub: "Overall Meetings 218",
+  },
 ];
