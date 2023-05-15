4089689b3678f36c4b7f2e554e8886a1ea7f5a7e
KumarGudu <kg150201@gmail.com>
2023-05-15 13:27:18 +0530

    crud and dashboard change

--- src/components/admin/RolesDashBoard.tsx
+++ src/components/admin/RolesDashBoard.tsx
@@ -9,6 +9,7 @@ import {
   Pending,
   MoreVert,
 } from "@mui/icons-material";
+import { AdminBreadcrumbs } from "components/core";
 
 const RolesDashBoard = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -67,73 +68,79 @@ const RolesDashBoard = () => {
 
   return (
     <>
-      <div className="flex gap-2 py-4">
-        <div className="w-full px-4 ">
-          <Grid container spacing={2}>
-            {cards?.map((item) => (
-              <Grid key={item?.id} item lg={3}>
-                <div className="border-4 border-b-theme h-32 bg-white w-full p-2 flex flex-col rounded-xl shadow-xl justify-between cursor-pointer hover:scale-105 transition duration-300 ease-in-out">
-                  <div className="flex justify-end">
-                    <IconButton size="small" onClick={handleClick}>
-                      <MoreVert />
-                    </IconButton>
-                    <Menu
-                      anchorEl={anchorEl}
-                      open={open}
-                      onClose={handleClose}
-                      MenuListProps={{
-                        "aria-labelledby": "basic-button",
-                      }}
-                    >
-                      <MenuItem onClick={handleClose}>All Users</MenuItem>
-                      <MenuItem onClick={handleClose}>View Dashboard</MenuItem>
-                    </Menu>
-                  </div>
-                  <div className="flex justify-around items-center">
-                    <div>{item?.icon}</div>
-                    <span className="text-xl text-theme font-semibold">
-                      {item?.count}
+      <section className="px-8 py-4">
+        <AdminBreadcrumbs links={links} />
+        <div className="flex gap-2 py-4">
+          <div className="w-full px-4 ">
+            <Grid container spacing={2}>
+              {cards?.map((item) => (
+                <Grid key={item?.id} item lg={3}>
+                  <div className="border-4 border-b-theme h-32 bg-white w-full p-2 flex flex-col rounded-xl shadow-xl justify-between cursor-pointer hover:scale-105 transition duration-300 ease-in-out">
+                    <div className="flex justify-end">
+                      <IconButton size="small" onClick={handleClick}>
+                        <MoreVert />
+                      </IconButton>
+                      <Menu
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                          "aria-labelledby": "basic-button",
+                        }}
+                      >
+                        <MenuItem onClick={handleClose}>All Users</MenuItem>
+                        <MenuItem onClick={handleClose}>
+                          View Dashboard
+                        </MenuItem>
+                      </Menu>
+                    </div>
+                    <div className="flex justify-around items-center">
+                      <div>{item?.icon}</div>
+                      <span className="text-xl text-theme font-semibold">
+                        {item?.count}
+                      </span>
+                    </div>
+                    <span className=" text-theme font-semibold text-center tracking-wide text-lg">
+                      {item?.title}
                     </span>
                   </div>
-                  <span className=" text-theme font-semibold text-center tracking-wide text-lg">
-                    {item?.title}
-                  </span>
-                </div>
-              </Grid>
-            ))}
-          </Grid>
+                </Grid>
+              ))}
+            </Grid>
+          </div>
         </div>
-      </div>
-      <div className="grid grid-cols-12 content-between gap-6  m-5 !mb-6">
-        <div className="px-2 col-span-12 pt-9 w-full flex flex-col justify-center gap-2 md:col-span-12 lg:col-span-7 !border-gray-500 rounded-xl !shadow-xl">
-          <p className="font-bold text-lg text-center">Role Overview</p>
-          <RoleBarChart
-            labels={roleLabels?.length ? roleLabels : []}
-            data={dataValue?.length ? dataValue : []}
-            type="bar"
-            text=""
-          />
+        <div className="grid grid-cols-12 content-between gap-6  m-5 !mb-6">
+          <div className="px-2 col-span-12 pt-9 w-full flex flex-col justify-center gap-2 md:col-span-12 lg:col-span-7 !border-gray-500 rounded-xl !shadow-xl">
+            <p className="font-bold text-lg text-center">Role Overview</p>
+            <RoleBarChart
+              labels={roleLabels?.length ? roleLabels : []}
+              data={dataValue?.length ? dataValue : []}
+              type="bar"
+              text=""
+            />
+          </div>
+          <div className="col-span-12 w-full flex flex-col justify-center md:col-span-12 lg:col-span-5 !border-gray-500 rounded-xl !shadow-xl">
+            <p className="text-lg font-bold text-center">Role Details</p>
+            <RoleDonutChart
+              labels={roleLabels?.length ? roleLabels : []}
+              series={dataValue?.length ? dataValue : []}
+              text=""
+              type="donut"
+              colors={[
+                "#106EAD",
+                "#C33C5E",
+                "#25d366",
+                "#BD33B5",
+                "#E60023",
+                "#005d32",
+              ]}
+            />
+          </div>
         </div>
-        <div className="col-span-12 w-full flex flex-col justify-center md:col-span-12 lg:col-span-5 !border-gray-500 rounded-xl !shadow-xl">
-          <p className="text-lg font-bold text-center">Role Details</p>
-          <RoleDonutChart
-            labels={roleLabels?.length ? roleLabels : []}
-            series={dataValue?.length ? dataValue : []}
-            text=""
-            type="donut"
-            colors={[
-              "#106EAD",
-              "#C33C5E",
-              "#25d366",
-              "#BD33B5",
-              "#E60023",
-              "#005d32",
-            ]}
-          />
-        </div>
-      </div>
+      </section>
     </>
   );
 };
 
 export default RolesDashBoard;
+const links = [{ id: 1, page: "Roles", link: "/admin/roles" }];
