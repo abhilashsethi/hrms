e20f90f97536a4e57092d7a90db44f248e59a73d
KumarGudu <kg150201@gmail.com>
2023-05-15 17:29:35 +0530

    ticket details

--- src/components/admin/clients/ClientMeetings.tsx
+++ src/components/admin/clients/ClientMeetings.tsx
@@ -4,82 +4,103 @@ import { AccountTreeRounded, Receipt } from "@mui/icons-material";
 import { Tooltip } from "@mui/material";
 import { ViewTicketsDrawer } from "components/drawer";
 import React, { useState } from "react";
+import { useFetch } from "hooks";
+import { useRouter } from "next/router";
 
-const ClientMeetings = () => {
-	const [tickets, setTickets] = useState(false);
-	const [viewTickets, setViewTickets] = useState<any>(null);
-
-	return (
-		<section className="w-full p-6 rounded-lg bg-white shadow-xl">
-			<ViewTicketsDrawer
-				open={tickets}
-				onClose={() => setTickets(false)}
-				setViewTickets={setViewTickets}
-			/>
-			<div className="flex justify-between">
-				<HeadText title="Tickets (20)" />
-				<Tooltip title="View All Tickets">
-					<div
-						onClick={() => setTickets(true)}
-						className=" rounded-full group flex justify-start items-center hover:scale-105 ease-in-out transition-all duration-400 cursor-pointer !text-blue-500 flex-col gap-2 "
-					>
-						<span className="bg-[#dbe3ff] p-2 shadow-lg rounded-md transition-all ease-in-out duration-200">
-							<Receipt />
-						</span>
-					</div>
-				</Tooltip>
-			</div>
-			<div className="flex flex-col gap-1 mt-4 max-h-[15rem] overflow-y-auto">
-				{cards?.map((item, i) => (
-					<div key={i} className="flex gap-1 py-3 border-b-[1px]">
-						<div className="w-1/5 flex justify-center items-center">
-							<div className="h-12 w-12 bg-theme-100 rounded-full flex justify-center items-center">
-								<AccountTreeRounded className="!text-secondary" />
-							</div>
-						</div>
-						<div className="w-4/5 h-20">
-							<div className="flex justify-between pr-3 items-center">
-								<p className="text-sm font-semibold tracking-wide">
-									{item?.title}
-								</p>
-							</div>
-							<p className="text-sm tracking-wide my-2">
-								issueDate : {moment(new Date()).format("ll")}
-							</p>
-							<p className="text-sm tracking-wide my-2">
-								Status :{" "}
-								<span className="py-1 px-3 rounded-md bg-emerald-100 tracking-wide border-green-400 border-[1px] text-green-500 text-xs font-semibold">
-									{item?.status}
-								</span>
-							</p>
-							{"  "}
-						</div>
-					</div>
-				))}
-			</div>
-		</section>
-	);
+const ClientMeetings = (uId: any) => {
+  const router = useRouter();
+  const [tickets, setTickets] = useState(false);
+  const [viewTickets, setViewTickets] = useState<any>(null);
+  const { data: ticketsData } = useFetch<any>(
+    `tickets?${uId ? `&clientId=${uId}` : ""}`
+  );
+  console.log(uId, ticketsData);
+  return (
+    <section className="w-full p-6 rounded-lg bg-white shadow-xl">
+      <ViewTicketsDrawer
+        open={tickets}
+        onClose={() => setTickets(false)}
+        setViewTickets={setViewTickets}
+        ticket={ticketsData}
+      />
+      <div className="flex justify-between">
+        <HeadText title={`Tickets (${ticketsData?.length})`} />
+        <Tooltip title="View All Tickets">
+          <div
+            onClick={() => setTickets(true)}
+            className=" rounded-full group flex justify-start items-center hover:scale-105 ease-in-out transition-all duration-400 cursor-pointer !text-blue-500 flex-col gap-2 "
+          >
+            <span className="bg-[#dbe3ff] p-2 shadow-lg rounded-md transition-all ease-in-out duration-200">
+              <Receipt />
+            </span>
+          </div>
+        </Tooltip>
+      </div>
+      <div className="flex flex-col gap-1 mt-4 max-h-[15rem] overflow-y-auto">
+        {ticketsData?.length ? (
+          ticketsData?.map((item: any, i: any) => (
+            <>
+              <div key={i} className="flex gap-1 py-3 border-b-[1px]">
+                <div className="w-1/5 flex justify-center items-center">
+                  <div className="h-12 w-12 bg-theme-100 rounded-full flex justify-center items-center">
+                    <AccountTreeRounded className="!text-secondary" />
+                  </div>
+                </div>
+                <div className="w-4/5 h-20">
+                  <div className="flex justify-between pr-3 items-center">
+                    <p className="text-sm font-semibold tracking-wide">
+                      {item?.title}
+                    </p>
+                  </div>
+                  <p className="text-sm tracking-wide my-2">
+                    issueDate : {moment(new Date()).format("ll")}
+                  </p>
+                  <p className="text-sm tracking-wide my-2">
+                    Status :{" "}
+                    <span
+                      className={`py-1 px-3 rounded-md tracking-wide ${
+                        item?.isResolved
+                          ? "bg-emerald-100 border-green-400 border-[1px] text-green-500"
+                          : "bg-red-100 border-red-400 border-[1px] text-red-500"
+                      } text-xs font-semibold`}
+                    >
+                      {item?.isResolved ? "Yes" : "No"}
+                    </span>
+                  </p>
+                  {"  "}
+                </div>
+              </div>
+            </>
+          ))
+        ) : (
+          <>
+            <p className="text-center">No Tickets Available</p>
+          </>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default ClientMeetings;
 
 const cards = [
-	{
-		id: 1,
-		title: "Arrange Meeting",
-		issueDate: "May 10, 2023",
-		status: "Yes",
-	},
-	{
-		id: 2,
-		title: "Server Issue",
-		issueDate: "May 10, 2023",
-		status: "Yes",
-	},
-	{
-		id: 3,
-		title: "Arrange Meeting",
-		issueDate: "May 10, 2023",
-		status: "Yes",
-	},
+  {
+    id: 1,
+    title: "Arrange Meeting",
+    issueDate: "May 10, 2023",
+    status: "Yes",
+  },
+  {
+    id: 2,
+    title: "Server Issue",
+    issueDate: "May 10, 2023",
+    status: "Yes",
+  },
+  {
+    id: 3,
+    title: "Arrange Meeting",
+    issueDate: "May 10, 2023",
+    status: "Yes",
+  },
 ];
