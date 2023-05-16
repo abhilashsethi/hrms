e31cdee00a23a34c8da5783182cd7a3ae63ade40
KumarGudu <kg150201@gmail.com>
2023-05-16 17:11:45 +0530

    guest view

--- src/components/drawer/CardAssign.tsx
+++ src/components/drawer/CardAssign.tsx
@@ -31,6 +31,7 @@ const CardAssign = ({ open, onClose, cardId, mutate }: Props) => {
   const [searchedUser, setSearchedUser] = useState<any>([]);
   const { change } = useChange();
   const { data: users } = useFetch<User[]>(`users`);
+  const { data: guest } = useFetch<any[]>(`guests`);
   useEffect(() => {
     if (users) {
       const filtered = users.filter((user) =>
@@ -203,7 +204,69 @@ const CardAssign = ({ open, onClose, cardId, mutate }: Props) => {
               </>
             ) : (
               <>
-                <LoaderAnimeLarge />
+                {/* {!searchedUser?.length && (
+                  <p className="py-8 text-center flex gap-3 items-center justify-center">
+                    <Search /> No results found!
+                  </p>
+                )} */}
+                {guest?.map((item: any) => (
+                  <div className="w-full rounded-l-full shadow-xl border-t flex items-center gap-2 px-4 py-2">
+                    <div className="w-1/5">
+                      <div className="h-[4rem] w-[4rem] rounded-full overflow-hidden shadow-lg">
+                        {item?.photo && (
+                          <img
+                            className="h-full w-full object-cover"
+                            src={item?.photo || DEFAULTPROFILE.src}
+                            alt=""
+                          />
+                        )}
+                        {!item?.photo ? (
+                          <div className="h-full w-full text-white rounded-full flex justify-center items-center text-2xl font-semibold bg-gradient-to-br from-theme-100 via-theme-50 to-secondary-300">
+                            {item?.name?.slice(0, 1)}
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>
+                    <div className="w-4/5 flex justify-between items-start h-full">
+                      <div className="flex flex-col h-full justify-center">
+                        <>
+                          <p className="text-sm">
+                            {item?.name?.slice(0, 18)}
+                            {item?.name?.length > 18 ? "..." : ""}
+                          </p>
+                          <p className="mt-1 text-sm font-semibold text-gray-500">
+                            {item?.designation}
+                          </p>
+                        </>
+                      </div>
+
+                      {selectedUser && selectedUser === item?.id ? (
+                        <Button
+                          onClick={handleAssign}
+                          size="small"
+                          startIcon={
+                            loading ? (
+                              <CircularProgress size={20} />
+                            ) : (
+                              <CheckCircle />
+                            )
+                          }
+                          className="!bg-emerald-500"
+                          variant="contained"
+                        >
+                          ASSIGN
+                        </Button>
+                      ) : (
+                        <div>
+                          <Radio
+                            onChange={() => setSelectedUser(item?.id)}
+                            checked={selectedUser === item?.id}
+                          />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                ))}
               </>
             )}
           </div>
