57e6acac8a71d62bb571525c09cd340e2b86076e
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-05-16 10:39:41 +0530

    meeting page onGoing

---
+++ src/pages/admin/meetings/meeting-details.tsx
@@ -0,0 +1,34 @@
+import PanelLayout from "layouts/panel";
+import { AdminBreadcrumbs } from "components/core";
+import { ClientDetails } from "components/admin/clients";
+import { MeetingData } from "components/admin/meetings";
+
+const MeetingDetails = () => {
+	return (
+		<PanelLayout title="User Profile - SY HR MS">
+			<section className="px-8 mx-auto p-4">
+				<div className="pb-4">
+					<AdminBreadcrumbs links={links} />
+				</div>
+				<MeetingData />
+			</section>
+		</PanelLayout>
+	);
+};
+
+export default MeetingDetails;
+
+const links = [
+	{ id: 1, page: "Meetings", link: "/admin/meetings" },
+	{
+		id: 2,
+		page: "Meeting Details",
+		link: "/admin/meetings/meeting-details",
+	},
+];
+
+// const attendance = [
+//   { title: "PRESENT", date: "2023-04-03" },
+//   { title: "ABSENT", date: "2023-04-04" },
+//   { title: "PRESENT", date: "2023-04-05" },
+// ];
