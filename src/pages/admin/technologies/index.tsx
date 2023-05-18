da5a29f1c0fb6ed2a603a15ccf753a4a634c9f87
KumarGudu <kg150201@gmail.com>
2023-05-18 15:57:50 +0530

    technologies dashboard ui

--- src/pages/admin/technologies/index.tsx
+++ src/pages/admin/technologies/index.tsx
@@ -1,11 +1,35 @@
+import {
+  DashBoardCardTech,
+  DashboardChartsTech,
+} from "components/admin/Technology";
+import { AdminBreadcrumbs } from "components/core";
+import { useFetch } from "hooks";
 import PanelLayout from "layouts/panel";
+import { useState } from "react";
 
 const Technologies = () => {
+  const [pageNumber, setPageNumber] = useState<number | null>(1);
+  const { data: roleDataCard } = useFetch<any>(
+    `roles?page=${pageNumber}&limit=3`
+  );
+  const { data: roleDashboard } = useFetch<any>(`roles/dashboard`);
+
   return (
     <PanelLayout title="Technologies - HRMS Searchingyard">
-      <section>Page in progress...</section>
+      <section className="px-8 py-4">
+        <AdminBreadcrumbs links={links} />
+        <DashBoardCardTech
+          dashboardData={roleDashboard}
+          roleCard={roleDataCard}
+        />
+        <DashboardChartsTech
+          dashboardData={roleDashboard}
+          roleCard={roleDataCard}
+        />
+      </section>
     </PanelLayout>
   );
 };
 
 export default Technologies;
+const links = [{ id: 1, page: "Technologies", link: "/admin/technologies" }];
