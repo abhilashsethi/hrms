97fc2e08f95c4fa83159c389a447106120ca10ec
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-05-23 10:27:21 +0530

    login page changed

--- src/components/loginAuth.tsx
+++ src/components/loginAuth.tsx
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 import * as Yup from "yup";
 import { Field, Form, Formik } from "formik";
 import { Hide, Logo, View } from "assets/dashboard_Icons";
+import { useAuth, useMutation } from "hooks";
+import { useRouter } from "next/router";
+import Swal from "sweetalert2";
+import { User } from "types";
 
 const LoginSchema = [
 	{
@@ -46,13 +50,38 @@ const validationSchema = LoginSchema.reduce((accumulator, currentValue) => {
 
 const LoginAuth = () => {
 	const [showPassword, setShowPassword] = useState(false);
-	const handleLogin = (values: any) => {
-		console.log(values);
+	const { isMutating, trigger } = useMutation(`auth/login`);
+	const router = useRouter();
+	const { setUser, setToken } = useAuth();
+	const handleLogin = async (values: any, submitProps: any) => {
+		try {
+			const res = await trigger(values);
+			// console.log(res);
+			if (!res.success) return Swal.fire("Error", res.msg, "error");
+			const user: User = { ...res.data.user };
+			// console.log(user);
+			setUser(user);
+			setToken(res.data.accessToken);
+			if (user?.role?.name === "CEO")
+				return Swal.fire("Welcome Back!", "Login Successful!", "success").then(
+					() => router.push(`/admin`)
+				);
+			if (user?.role?.name === "HR")
+				return Swal.fire("Welcome Back!", "Login Successful!", "success").then(
+					() => router.push(`/admin/hr`)
+				);
+			return Swal.fire("Error", "You Don't Have Access To This Page", "error");
+		} catch (error) {
+			submitProps.setSubmitting(false);
+			Swal.fire("Error", "Invalid login credentials", "error");
+			console.log(error);
+		}
 	};
+
 	return (
 		<article className="w-full md:flex-row flex flex-col justify-center items-center gap-5 text-themeDarkGray min-h-screen">
 			<section className="lg:w-9/12 w-full md:flex-row flex flex-col">
-				<div className="md:w-3/5 w-full md:h-[30rem] h-[22rem] relative overflow-hidden shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] bg-blue-300 rounded-l bg-gradient-to-t from-blue-400 to-white py-3">
+				<div className="md:w-1/2 md:block hidden w-full md:h-[30rem] h-[22rem] relative overflow-hidden shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] bg-blue-300 rounded-l bg-gradient-to-t from-blue-400 to-white py-3">
 					<div className="flex md:w-[12rem] w-[10rem] md:h-[12rem] h-[10rem] bg-blue-200 rotate-animation-one absolute top-[110px] left-[-50px] items-center justify-center rotate-[38deg]">
 						<div className="md:w-[7rem] w-[5rem] md:h-[7rem] h-[5rem]  bg-[#FFB774]"></div>
 					</div>
@@ -60,10 +89,10 @@ const LoginAuth = () => {
 						<img src={Logo.src} alt="logo" className="w-full h-10" />
 					</div>
 					<div className="flex  absolute md:top-60 z-[30] top-20 left-4 flex-col h-full gap-4 px-8">
-						<h1 className="md:text-4xl text-3xl text-purple-600 font-bold text-start">
-							Welcome to <span className="text-purple-600">H</span>Rms{" "}
+						<h1 className="md:text-4xl text-3xl text-blue-600 font-bold text-start tracking-wide">
+							Welcome to HRMS
 						</h1>
-						<p className="text-themeDarkGray md:font-semibold text-base md:tracking-wide tracking-normal  leading-6 md:text-start text-justify">
+						<p className="text-themeDarkGray md:font-medium text-sm md:tracking-wide tracking-normal  leading-6 md:text-start text-justify">
 							It is a long established fact that a reader will be distracted by
 							the readable content of a page when looking at its layout. The
 							point of using Lorem Ipsum is that it has a more-or-less normal
@@ -74,8 +103,14 @@ const LoginAuth = () => {
 						<div className="md:w-[7rem] w-[5rem] md:h-[7rem] h-[5rem]  bg-[#525fa7] "></div>
 					</div>
 				</div>
-				<div className="md:w-2/5 w-full h-[30rem] bg-white rounded-r">
+				<div className="md:w-1/2 w-full md:h-[30rem] h-[41.7rem] bg-white md:rounded-r rounded-none">
 					<div className="flex flex-col items-start justify-center h-full gap-4 px-8">
+						<div className="md:hidden w-full flex flex-col items-center justify-center">
+							<img src={Logo.src} alt="logo" className="h-6 object-contain" />
+							<p className="text-themeDarkGray text-sm font-medium">
+								Welcome to HRMS
+							</p>
+						</div>
 						<h1 className="text-2xl text-themeDarkGray font-bold text-start">
 							Login
 						</h1>
@@ -130,11 +165,6 @@ const LoginAuth = () => {
 																props.field.onBlur(e);
 																formik.handleBlur(e);
 															}}
-															//   value={
-															//     item.type === "password"
-															//       ? formik.values[item.name].replace(/./g, "*")
-															//       : formik.values[item.name]
-															//   }
 														/>
 														{item.type === "password" && (
 															<div
