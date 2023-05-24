e8dfd1398d14576842462bca754d48bb0b74e8e7
KumarGudu <kg150201@gmail.com>
2023-05-24 17:34:05 +0530

    client

--- src/pages/admin/clients/all-clients.tsx
+++ src/pages/admin/clients/all-clients.tsx
@@ -45,7 +45,6 @@ const AllClients = () => {
       isOrderBy ? `&orderBy=${isOrderBy}` : ""
     }${isIssue ? `&issueResolved=${isIssue}` : ""}`
   );
-  console.log(pagination);
   return (
     <>
       <PanelLayout title="All Clients - SY HR MS">
