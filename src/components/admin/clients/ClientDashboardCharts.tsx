96cc0deeef0d31c41f7d660ed4f1a18db8c5e820
KumarGudu <kg150201@gmail.com>
2023-05-24 16:45:43 +0530

    country

--- src/components/admin/clients/ClientDashboardCharts.tsx
+++ src/components/admin/clients/ClientDashboardCharts.tsx
@@ -7,7 +7,7 @@ const ClientDashboardCharts = ({ cards }: Props) => {
   return (
     <div className="grid grid-cols-12 content-between gap-6  m-5 !mb-6">
       <div className="col-span-12 pt-9 w-full bg-white gap-5 md:col-span-12 lg:col-span-8 !border-grey-500 rounded-xl !shadow-xl">
-        <p className="text-lg text-center font-bold">Client Strength</p>
+        <p className="text-lg text-center font-bold">Country Strength</p>
         {/* <ClientMultiDataChart text="Client Strength" type="bar" /> */}
         <ClientBarGraph
           labels={
@@ -29,7 +29,7 @@ const ClientDashboardCharts = ({ cards }: Props) => {
         />
       </div>
       <div className="col-span-12 px-2 py-4 pt-9 bg-white w-full gap-5 md:col-span-12 lg:col-span-4 !border-grey-500 rounded-xl !shadow-xl">
-        <p className="text-center text-lg font-bold">Country Strength</p>
+        <p className="text-center text-lg font-bold">Gender Strength</p>
         <ClientLineCharts
           labels={
             cards?.clients?.genderWiseClientCount?.length
