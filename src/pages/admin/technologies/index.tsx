996340fabfbbb5e9669199b27716de014ef51d0f
KumarGudu <kg150201@gmail.com>
2023-05-24 13:14:34 +0530

    technology loader add

--- src/pages/admin/technologies/index.tsx
+++ src/pages/admin/technologies/index.tsx
@@ -2,13 +2,13 @@ import {
   DashBoardCardTech,
   DashboardChartsTech,
 } from "components/admin/Technology";
-import { AdminBreadcrumbs } from "components/core";
+import { AdminBreadcrumbs, Loader } from "components/core";
 import { useFetch } from "hooks";
 import PanelLayout from "layouts/panel";
 import { useState } from "react";
 
 const Technologies = () => {
-  const { data: techDashboard } = useFetch<any>(
+  const { data: techDashboard, isLoading } = useFetch<any>(
     `technologies/dashboard/details`
   );
   const { data: tech } = useFetch<any[]>(`technologies?page=1&limit=3`);
@@ -16,6 +16,7 @@ const Technologies = () => {
     <PanelLayout title="Technologies - HRMS Searchingyard">
       <section className="lg:px-8 px-4 py-4">
         <AdminBreadcrumbs links={links} />
+        {isLoading && <Loader />}
         <DashBoardCardTech dashboardData={techDashboard} tech={tech} />
         <DashboardChartsTech dashboardData={techDashboard} />
       </section>
