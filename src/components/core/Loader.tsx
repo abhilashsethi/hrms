1e603f350d47533b85e63c31fcb16ea87477f493
KumarGudu <kg150201@gmail.com>
2023-05-24 13:18:22 +0530

    loader bug fix

--- src/components/core/Loader.tsx
+++ src/components/core/Loader.tsx
@@ -17,7 +17,7 @@ const Loader = ({ image, animeHight = 250, animeWidth = 250 }: Props) => {
   };
 
   return (
-    <div className="h-[70vh] z-10 w-full flex flex-col justify-center items-center">
+    <div className="h-[100vh] fixed z-100 w-full flex flex-col justify-center items-center">
       <Lottie options={defaultOptions} height={animeHight} width={animeWidth} />
     </div>
   );
