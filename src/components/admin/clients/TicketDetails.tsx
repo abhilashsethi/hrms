e5992404eefdc380dd657ed546c37fefb95e285e
KumarGudu <kg150201@gmail.com>
2023-05-26 10:43:26 +0530

    ticket conversation

--- src/components/admin/clients/TicketDetails.tsx
+++ src/components/admin/clients/TicketDetails.tsx
@@ -1,7 +1,8 @@
-import { Button } from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 import { Loader, PhotoViewer } from "components/core";
-import { useFetch } from "hooks";
+import { useChange, useFetch } from "hooks";
 import { useRouter } from "next/router";
+import { Form, Formik } from "formik";
 import { useState } from "react";
 import { Tickets, TicketsConversations, User } from "types";
 import ClientChats from "./ClientChats";
@@ -9,13 +10,19 @@ import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
 import { Send } from "@mui/icons-material";
 import moment from "moment";
-interface Props  {
+import Swal from "sweetalert2";
+interface Props {
   ticketsData?: Tickets | null;
   ticketLoading?: any;
 }
+const initialValues = {
+  text: "",
+};
 const ReactQuill = dynamic(import("react-quill"), { ssr: false });
 
-const TicketDetails = ({ticketsData, ticketLoading}:Props) => {
+const TicketDetails = ({ ticketsData, ticketLoading }: Props) => {
+  const [loading, setLoading] = useState(false);
+  const { change, isChanging } = useChange();
   const [value, setValue] = useState("");
   const {
     data: ticketConversation,
@@ -23,80 +30,124 @@ const TicketDetails = ({ticketsData, ticketLoading}:Props) => {
     isLoading,
   } = useFetch<TicketsConversations>(`ticket-conversation`);
 
- 
+  const handleSubmit = async (values: any) => {
+    console.log("value", values);
+    return
+    setLoading(true);
+    try {
+      delete values.confirmPassword;
+      const res = await change(`ticket-conversation`, {
+        body: values,
+      });
+      setLoading(false);
+      if (res?.status !== 201) {
+        Swal.fire("Error", res?.results?.msg || "Unable to Submit", "error");
+        setLoading(false);
+        return;
+      }
+      Swal.fire(`Success`, `You have successfully Created!`, `success`);
+      return;
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-      <section className="mb-12 flex gap-3">
-        <div className="grid lg:grid-cols-3 gap-4">
-          <div className="lg:col-span-2">
-            <div className="w-full bg-white shadow-xl rounded-lg p-8 mt-4">
-              <div className="flex items-center gap-2 -ml-5">
-                <div className="h-3 w-3 rounded-sm bg-theme"></div>
-                <p className="font-bold tracking-wide">Conversations</p>
-              </div>
-              <div className="grid">
-                {ticketLoading ? <p>Loading Please wait .....</p> :(
-                  <div className="flex flex-col gap-1 mt-4 max-h-[20rem] overflow-y-auto">
-                   {ticketsData?.conversations?.length ? (
-                    <>
-                    {ticketsData?.conversations?.map((item, i) => (
-                      <div
-                        key={i}
-                        className="flex gap-3 py-3 px-1 border-b-[1px]"
-                      >
-                        <div className="flex justify-start items-center">
-                          <div className=" bg-theme-100 rounded-full flex justify-center items-center">
-                            <PhotoViewer photo={ "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} size="3rem" />
+    <section className="mb-12 flex gap-3">
+      <div className="grid lg:grid-cols-3 gap-4">
+        <div className="lg:col-span-2">
+          <div className="w-full bg-white shadow-xl rounded-lg p-8 mt-4">
+            <div className="flex items-center gap-2 -ml-5">
+              <div className="h-3 w-3 rounded-sm bg-theme"></div>
+              <p className="font-bold tracking-wide">Conversations</p>
+            </div>
+            <div className="grid">
+              <div className="flex flex-col gap-1 mt-4 max-h-[20rem] overflow-y-auto">
+                {ticketLoading ? <p>Loading Please wait .....</p> : (
+                  <>
+
+                    {ticketsData?.conversations?.length ? (
+                      <>
+                        {ticketsData?.conversations?.map((item, i) => (
+                          <div
+                            key={i}
+                            className="flex gap-3 py-3 px-1 border-b-[1px]"
+                          >
+                            <div className="flex justify-start items-center">
+                              <div className=" bg-theme-100 rounded-full flex justify-center items-center">
+                                <PhotoViewer photo={"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} size="3rem" />
+                              </div>
+                            </div>
+                            <div className="w-full">
+                              <div className="flex w-full justify-between pr-3 items-center">
+                                <p className="text-sm font-semibold tracking-wide">
+                                  {item?.userInfo?.name}
+                                </p>
+                                <p className="pr-3 text-xs font-semibold text-gray-500 tracking-wide">
+                                  {moment(item?.createdAt).format('ll')}
+                                </p>
+                              </div>
+                              <p className="text-sm tracking-wide">
+                                {/* Deadline : {moment(new Date()).format("ll")} */}
+                                {item?.text}
+                              </p>
+                            </div>
                           </div>
-                        </div>
-                        <div className="">
-                          <div className="flex justify-between pr-3 items-center">
-                            <p className="text-sm font-semibold tracking-wide">
-                              {item?.userInfo?.name}
-                            </p>
-                            <p className="pr-3 text-xs font-semibold text-gray-500 place-content-end tracking-wide absolute right-0">
-                              {moment(item?.createdAt).format('ll')}
-                            </p>
-                          </div>
-                          <p className="text-sm tracking-wide">
-                            {/* Deadline : {moment(new Date()).format("ll")} */}
-                            {item?.text}
-                          </p>
-                        </div>
-                      </div>
-                    ))} </>): <p>No conversation</p> }
-                  </div>
+                        ))} </>) : <p>No conversation</p>}
+                  </>
                 )}
               </div>
             </div>
-            <div className="mt-8">
-              <ReactQuill
-                placeholder="Reply message ..."
-                theme="snow"
-                value={value}
-                style={{
-                  height: "150px",
-                  lineHeight: "0px",
-                }}
-              />
-              <div className="flex justify-end items-end w-full pr-2">
-                <Button
-                  variant="contained"
-                  className="!bg-emerald-500 "
-                  startIcon={<Send />}
-                  size="small"
-                >
-                  Send Message
-                </Button>
-              </div>
-            </div>
           </div>
-          <div>
-            <div className="w-full h-full">
-              <ClientChats ticketsData={ticketsData} ticketLoading={ticketLoading}/>
-            </div>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              setFieldValue,
+            }) => (
+              <Form>
+                <div className="mt-8 bg-white">
+
+                  <ReactQuill
+                    placeholder="Reply message ..."
+                    theme="snow"
+                    value={values.text}
+                    className="h-[150px] "
+                  />
+                  <div className="flex justify-end items-end w-full pr-2">
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      className="!bg-emerald-500 "
+                      disabled={loading}
+                      startIcon={
+                        loading ? <CircularProgress size={20} /> : <Send />
+                      }
+                      size="small"
+                    >
+                      Send Message
+                    </Button>
+                  </div>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+        <div>
+          <div className="w-full h-full">
+            <ClientChats ticketsData={ticketsData} ticketLoading={ticketLoading} />
           </div>
         </div>
-      </section>
+      </div>
+    </section>
   );
 };
 
