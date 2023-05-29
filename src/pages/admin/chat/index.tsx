5ef4a03e2b9cc973cd2d92070453f6be22743906
Srinu Reddy <73178183+srinureddy7@users.noreply.github.com>
2023-05-29 10:38:21 +0530

    chat section added .

---
+++ src/pages/admin/chat/index.tsx
@@ -0,0 +1,17 @@
+import { AdminBreadcrumbs, ChatSection } from "components/core";
+import PanelLayout from "layouts/panel";
+
+const Chats = () => {
+  return (
+    <PanelLayout title="Chats - Admin Panel">
+      <section className="px-6 py-4">
+        <AdminBreadcrumbs links={links} />
+        <ChatSection />
+      </section>
+    </PanelLayout>
+  );
+};
+
+export default Chats;
+
+const links = [{ id: 1, page: "Chats", link: "/admin/chat" }];
