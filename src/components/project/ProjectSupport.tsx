798e09d9239a4049da3447196bfcd15343bfd9ce
KumarGudu <kg150201@gmail.com>
2023-05-29 12:21:18 +0530

    no support bu

--- src/components/project/ProjectSupport.tsx
+++ src/components/project/ProjectSupport.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarGroup } from "@mui/material";
 import { DEFAULTPROFILE } from "assets/home";
+import { LoaderAnime } from "components/core";
 import { useFetch } from "hooks";
 import moment from "moment";
 import React from "react";
@@ -14,24 +15,25 @@ const ProjectSupport = () => {
     <section className="">
       {isLoading && <p className="text-center">Please wait...</p>}
       <div className="grid gap-3">
-      {ticketsData?.map((data)=>(
-      <div key={data?.id} className="w-full rounded-md p-6 shadow-jubilation">
-        <div className="flex justify-end">
-          <span className="text-xs mb-1">
-          {moment(data?.createdAt).format("ll")}
-          </span>
-        </div>
+        {ticketsData?.map((data) => (
+          <div key={data?.id} className="w-full rounded-md p-6 shadow-jubilation">
+            <div className="flex justify-end">
+              <span className="text-xs mb-1">
+                {moment(data?.createdAt).format("ll")}
+              </span>
+            </div>
 
-        <h1 className="font-semibold text-slate-700 text-sm">
-          {data?.ticket?.title}
-        </h1>
-        <p className="mt-2 text-sm tracking-wide text-slate-500">
-          {data?.ticket?.description}
-        </p>
-        <h1 className="text-sm font-semibold text-theme mt-2">User Info :</h1>
-         <p>{data?.userInfo?.name}</p>
-      </div>
-      ))}
+            <h1 className="font-semibold text-slate-700 text-sm">
+              {data?.ticket?.title}
+            </h1>
+            <p className="mt-2 text-sm tracking-wide text-slate-500">
+              {data?.ticket?.description}
+            </p>
+            <h1 className="text-sm font-semibold text-theme mt-2">User Info :</h1>
+            <p>{data?.userInfo?.name}</p>
+          </div>
+        ))}
+        {ticketsData?.length === 0 ? <LoaderAnime text="No support Found" /> : null}
       </div>
     </section>
   );
