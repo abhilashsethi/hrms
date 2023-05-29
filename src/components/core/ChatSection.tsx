fd116fa67d8939a64e06b38bfcb29cf82b37ecaa
Srinu Reddy <73178183+srinureddy7@users.noreply.github.com>
2023-05-29 19:10:01 +0530

    chat section group and chat individual and details sectuion done.

--- src/components/core/ChatSection.tsx
+++ src/components/core/ChatSection.tsx
@@ -1,11 +1,30 @@
 import { ChatLeftBar, ChatRightSection } from "components/chat";
+import { useState } from "react";
 
 const ChatSection = () => {
+  const [activeProfile, setActiveProfile] = useState<{
+    id?: number;
+    photo?: string | undefined;
+    name?: string;
+    message?: string;
+    type?: string;
+  }>({
+    id: 1,
+    photo:
+      "https://www.bollywoodhungama.com/wp-content/uploads/2023/01/Hrithik-Roshan-opens-up-about-620.jpg",
+    name: "Loushik Giri",
+    message: "Talk to you...",
+    type: "person",
+  });
+
   return (
     <section>
       <div className="h-[35rem] w-full rounded-md bg-white mt-4 shadow-md flex">
-        <ChatLeftBar />
-        <ChatRightSection />
+        <ChatLeftBar
+          activeProfile={activeProfile}
+          setActiveProfile={setActiveProfile}
+        />
+        <ChatRightSection activeProfile={activeProfile} />
       </div>
     </section>
   );
--- src/components/drawer/ChatProfileDrawer.tsx
+++ src/components/drawer/ChatProfileDrawer.tsx
@@ -1,19 +1,28 @@
 import {
+  ArrowBack,
   Block,
   Close,
   Delete,
   KeyboardArrowRight,
   ThumbDown,
 } from "@mui/icons-material";
-import { Container, Drawer, IconButton } from "@mui/material";
-import { useState } from "react";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Box, Container, Drawer, IconButton, Tab } from "@mui/material";
+import { DEFAULTPROFILE } from "assets/home";
+import React, { useState } from "react";
 
 type Props = {
   open?: boolean | any;
   onClose: () => void;
+  profileData?: any;
 };
-const ChatProfileDrawer = ({ open, onClose }: Props) => {
+const ChatProfileDrawer = ({ open, onClose, profileData }: Props) => {
   const [isMedia, setIsMedia] = useState(false);
+  const [value, setValue] = React.useState("1");
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
   return (
     <Drawer anchor="right" open={open} onClose={() => onClose && onClose()}>
       <Container
@@ -21,67 +30,94 @@ const ChatProfileDrawer = ({ open, onClose }: Props) => {
           width: "30vw",
         }}
       >
-        <section className="py-4 w-full">
-          <div className="flex gap-2 items-center">
-            <span className="cursor-pointer" onClick={() => onClose()}>
-              <Close fontSize="small" className="!text-red-600" />
-            </span>
-            <h1>Profile Info</h1>
-          </div>
-          {/* ------------------Image section----------------- */}
-          <div className="flex flex-col items-center gap-3 my-8">
-            <div className="h-40 w-40 rounded-full bg-slate-200 overflow-hidden">
-              <img
-                className="h-full object-cover w-full"
-                src="https://media.npr.org/assets/img/2022/11/08/ap22312071681283-0d9c328f69a7c7f15320e8750d6ea447532dff66-s1100-c50.jpg"
-                alt="image"
-              />
+        <section className="relative w-full overflow-hidden">
+          <section className="py-4 w-full">
+            <div className="flex gap-2 items-center">
+              <span className="cursor-pointer" onClick={() => onClose()}>
+                <Close fontSize="small" className="!text-red-600" />
+              </span>
+              <h1>Profile Info</h1>
             </div>
-            <h1 className="font-semibold">John Doe</h1>
-          </div>
-          {/* ------------------About Section-------------------- */}
-          <div>
-            <SectionTitle title="About" />
-            <div className="mt-2 flex flex-col gap-2">
-              {aboutDetails?.map((item) => (
-                <div key={item?.id} className="flex items-start text-sm">
-                  <span className="w-1/2 font-medium">{item?.title} : </span>
-                  <span>{item?.value} </span>
+            {/* ------------------Image section----------------- */}
+            <div className="flex flex-col items-center gap-3 my-8">
+              <div className="h-40 w-40 rounded-full bg-slate-200 overflow-hidden">
+                <img
+                  className="h-full object-cover w-full"
+                  src={profileData?.photo || DEFAULTPROFILE.src}
+                  alt="image"
+                />
+              </div>
+              <div className="flex flex-col gap-1 items-center">
+                <h1 className="font-semibold">{profileData?.name}</h1>
+                <h1 className="">demo@sy.com</h1>
+              </div>
+            </div>
+            {/* ------------------About Section-------------------- */}
+            <div>
+              <SectionTitle title="Department" />
+              <h1 className="px-4 mt-2">Web Development</h1>
+            </div>
+            <div className="w-full rounded-md bg-slate-100 border-[1px] shadow-md tracking-wide mt-4 flex justify-between items-center px-4 py-2">
+              <h1>Media, links and Docs</h1>
+              <IconButton
+                onClick={() => setIsMedia((prev) => !prev)}
+                size="small"
+              >
+                <KeyboardArrowRight
+                  className={`!cursor-pointer !transition-all !ease-in-out !duration-200 }`}
+                />
+              </IconButton>
+            </div>
+            <div className="mt-4">
+              {configs?.map((item) => (
+                <div
+                  key={item?.id}
+                  className="py-3 px-2 cursor-pointer hover:bg-slate-200"
+                >
+                  <h1 className="flex gap-4 items-center text-red-600 font-medium">
+                    {item?.icon}
+                    {item?.title}
+                  </h1>
                 </div>
               ))}
             </div>
-          </div>
-          <div className="w-full bg-white shadow-sleek tracking-wide mt-4 flex justify-between items-center px-4 py-2">
-            <h1>Media, links and Docs</h1>
-            <IconButton
-              onClick={() => setIsMedia((prev) => !prev)}
-              size="small"
-            >
-              <KeyboardArrowRight
-                className={`!cursor-pointer !transition-all !ease-in-out !duration-200 }`}
-              />
-            </IconButton>
-          </div>
-          {isMedia && (
-            <section className="h-40 w-full border-2 mt-4">
-              Media Section
-            </section>
-          )}
-          <div className="mt-4">
-            {configs?.map((item) => (
-              <div
-                key={item?.id}
-                className="py-3 px-2 cursor-pointer hover:bg-slate-200"
+          </section>
+          {/* --------------------Media and links section------------------------- */}
+          <section
+            className={`w-full h-screen absolute bg-white top-0 left-0 transition-all ease-in-out duration-200 ${
+              !isMedia ? `translate-x-[100%]` : `translate-x-[0%]`
+            }`}
+          >
+            <div className="flex items-center gap-3 py-3 text-red-600">
+              <IconButton
+                onClick={() => setIsMedia((prev) => !prev)}
+                size="small"
               >
-                <h1 className="flex gap-4 items-center text-red-600 font-medium">
-                  {item?.icon}
-                  {item?.title}
-                </h1>
-              </div>
-            ))}
-          </div>
+                <ArrowBack className="!text-red-600" />
+              </IconButton>
+              <h1>Go Back</h1>
+            </div>
+            <div>
+              <Box sx={{ width: "100%", typography: "body1" }}>
+                <TabContext value={value}>
+                  <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                    <TabList
+                      onChange={handleChange}
+                      aria-label="lab API tabs example"
+                    >
+                      <Tab label="Media" value="1" />
+                      <Tab label="Docs" value="2" />
+                      <Tab label="Links" value="3" />
+                    </TabList>
+                  </Box>
+                  <TabPanel value="1">No Media Files...</TabPanel>
+                  <TabPanel value="2">Docs</TabPanel>
+                  <TabPanel value="3">Links</TabPanel>
+                </TabContext>
+              </Box>
+            </div>
+          </section>
         </section>
-        <section></section>
       </Container>
     </Drawer>
   );
