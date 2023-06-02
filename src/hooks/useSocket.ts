f12f76b9d20553dd4b58de965e35074568889d98
yard-tech <searchingyardpc@gmail.com>
2023-06-02 12:52:54 +0530

    delivered and connect user added

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -2,7 +2,8 @@ import { ClientToServerEvents, ServerToClientEvents } from "types";
 import { create } from "zustand";
 import { Socket, io } from "socket.io-client";
 
-const SOCKET_URL = `wss://hrms.yardiot.com`;
+// const SOCKET_URL = `wss://hrms.yardiot.com`;
+const SOCKET_URL = `ws://localhost:8080`;
 
 type socketState = {
   socketRef?: Socket<ServerToClientEvents, ClientToServerEvents>;
