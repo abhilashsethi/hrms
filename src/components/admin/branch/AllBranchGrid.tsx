b92b7a833f14029feabbfbdc621731341281b775
KumarGudu <kg150201@gmail.com>
2023-06-02 11:39:14 +0530

    render icon change

--- src/components/admin/branch/AllBranchGrid.tsx
+++ src/components/admin/branch/AllBranchGrid.tsx
@@ -1,3 +1,4 @@
+import { RenderIconRow } from "components/common";
 import { CountryNameFlag } from "components/core";
 import { UpdateDepartment } from "components/dialogues";
 import { DepartmentInformation } from "components/drawer";
@@ -84,17 +85,30 @@ const AllBranchGrid = ({ data, mutate }: Props) => {
                   <div className="py-1 pt-2 px-4">
                     <h1 className="inline-block py-1 title-font text-xl font-extrabold text-gray-800 tracking-wide cursor-pointer">{item?.name}</h1>
                     <p className="text-gray-500 cursor-pointer">{item?.manager}</p>
-                    <p className="text-gray-500 cursor-pointer">{item?.phone}</p>
-                    <p className="text-gray-500 cursor-pointer">{item?.email}</p>
+                    <p className="text-gray-500 cursor-pointer">
+                      <RenderIconRow
+                        value={item?.phone || "---"}
+                        isPhone
+                        longText={false}
+                      />
+                    </p>
+
+                    <p className="text-gray-500 cursor-pointer">
+                      <RenderIconRow
+                        value={item?.email || "---"}
+                        isEmail
+                        longText={false}
+                      />
+                    </p>
                     <p className="text-sm text-slate-600 font-medium py-1 flex items-center gap-3">
                       <CountryNameFlag
                         countryName={item?.country || "---"}
                       />
                     </p>
-                  <h2 className="py-1 pb-1 inline-block text-xs title-font font-semibold text-red-400 uppercase tracking-widest cursor-pointer hover:font-bold">{item?.location}</h2>
-                 
+                    <h2 className="py-1 pb-1 inline-block text-xs title-font font-semibold text-red-400 uppercase tracking-widest cursor-pointer hover:font-bold">{item?.location}</h2>
+
                   </div>
-                  
+
 
                 </div>
               </div>
