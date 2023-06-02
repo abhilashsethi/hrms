a8b07f83717198efffc805f739b86d7fb8d3fd7f
KumarGudu <kg150201@gmail.com>
2023-06-02 10:50:33 +0530

    style change

--- src/components/admin/branch/AllBranchGrid.tsx
+++ src/components/admin/branch/AllBranchGrid.tsx
@@ -82,12 +82,12 @@ const AllBranchGrid = ({ data, mutate }: Props) => {
                   <img className="lg:h-48 md:h-36 w-full object-cover object-center transition duration-500 ease-in-out transform group-hover:scale-105"
                     src={item?.photo} alt="blog" />
                   <div className="py-1 pt-2 px-6">
-                    <h1 className="mb-3 inline-block title-font text-xl font-extrabold text-gray-800 tracking-wide cursor-pointer">{item?.name}</h1>
-                    <p className="line-clamp-6 mb-3 overflow-hidden leading-relaxed text-gray-500 cursor-pointer">{item?.manager}</p>
-                    <p className="line-clamp-6 mb-3 overflow-hidden leading-relaxed text-gray-500 cursor-pointer">{item?.phone}</p>
-                    <p className="line-clamp-6 mb-3 overflow-hidden leading-relaxed text-gray-500 cursor-pointer">{item?.email}</p>
-                  <h2 className="pt-4 pb-1 inline-block text-xs title-font font-semibold text-red-400 uppercase tracking-widest cursor-pointer hover:font-bold">{item?.location}</h2>
-                  <p className="text-sm text-slate-600 font-medium mt-1 flex items-center gap-3">
+                    <h1 className="inline-block py-1 title-font text-xl font-extrabold text-gray-800 tracking-wide cursor-pointer">{item?.name}</h1>
+                    <p className="line-clamp-6 py-1 overflow-hidden leading-relaxed text-gray-500 cursor-pointer">{item?.manager}</p>
+                    <p className="line-clamp-6 py-1 overflow-hidden leading-relaxed text-gray-500 cursor-pointer">{item?.phone}</p>
+                    <p className="line-clamp-6 py-1 overflow-hidden leading-relaxed text-gray-500 cursor-pointer">{item?.email}</p>
+                  <h2 className="py-1 pb-1 inline-block text-xs title-font font-semibold text-red-400 uppercase tracking-widest cursor-pointer hover:font-bold">{item?.location}</h2>
+                  <p className="text-sm text-slate-600 font-medium py-1 flex items-center gap-3">
                       <CountryNameFlag
                         countryName={item?.country || "---"}
                       />
