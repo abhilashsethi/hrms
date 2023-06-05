572433a96b1ab8fc4e653e5b3a5e82f9df2fbdd0
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-06-05 11:18:36 +0530

    All assets page ui onGoing

---
+++ src/components/admin/assets/AssetsGrid.tsx
@@ -0,0 +1,270 @@
+import { LOCATION, MANAGER } from "assets/dashboard_Icons";
+import { RenderIconRow } from "components/common";
+import { CountryNameFlag, ReverseIOSSwitch } from "components/core";
+import { UpdateDepartment } from "components/dialogues";
+import { DepartmentInformation } from "components/drawer";
+import { useChange } from "hooks";
+import { useState } from "react";
+import Swal from "sweetalert2";
+import Slider from "react-slick";
+import { DeleteRounded, Edit } from "@mui/icons-material";
+interface Props {
+	data?: any;
+	mutate?: any;
+}
+const settings = {
+	dots: false,
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	speed: 400,
+	cssEase: "linear",
+	autoplaySpeed: 3000,
+	pauseOnHover: false,
+	arrows: false,
+	responsive: [
+		{
+			breakpoint: 940,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				arrows: false,
+			},
+		},
+		{
+			breakpoint: 760,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				arrows: false,
+			},
+		},
+	],
+};
+const AssetsGrid = ({ data, mutate }: Props) => {
+	return (
+		<>
+			<section className="py-6 ">
+				<div className="grid lg:grid-cols-4 md:grid-cols-2 gap-6 items-center justify-center">
+					{data?.map((item: any, index: any) => (
+						<div key={index}>
+							<MoreOption item={item} mutate={mutate} />
+						</div>
+					))}
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default AssetsGrid;
+const MoreOption = ({ item, mutate }: any) => {
+	const [loading, setLoading] = useState(false);
+	const [isInfo, setIsInfo] = useState<{ dialogue?: boolean; role?: any }>({
+		dialogue: false,
+		role: null,
+	});
+	const { change } = useChange();
+	const [isUpdate, setIsUpdate] = useState<{
+		dialogue?: boolean;
+		departmentData?: string | null;
+	}>({ dialogue: false, departmentData: null });
+
+	const handleDelete = async (id: string) => {
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You want to delete?",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes, delete!",
+		}).then(async (result) => {
+			if (result.isConfirmed) {
+				setLoading(true);
+				Swal.fire("", "Please Wait...", "info");
+				try {
+					Swal.fire(`Info`, "It will take some time", "info");
+					const res = await change(`departments/${id}`, { method: "DELETE" });
+					setLoading(false);
+					if (res?.status !== 200) {
+						Swal.fire(
+							"Error",
+							res?.results?.msg || "Something went wrong!",
+							"error"
+						);
+						setLoading(false);
+						return;
+					}
+					mutate();
+					Swal.fire(`Success`, `Deleted Successfully!`, `success`);
+					return;
+				} catch (error) {
+					console.log(error);
+					setLoading(false);
+				} finally {
+					setLoading(false);
+				}
+			}
+		});
+	};
+	const handleBlock = async (e: any, userId: string) => {
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You want to update status?",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes, update!",
+		}).then(async (result) => {
+			if (result.isConfirmed) {
+				const res = await change(`users/${userId}`, {
+					method: "PATCH",
+					body: { isBlocked: !e.target?.checked },
+				});
+				mutate();
+				if (res?.status !== 200) {
+					Swal.fire(`Error`, "Something went wrong!", "error");
+					return;
+				}
+				Swal.fire(`Success`, "User Blocked successfully!!", "success");
+				return;
+			}
+		});
+	};
+
+	return (
+		<>
+			<UpdateDepartment
+				departmentData={isUpdate?.departmentData}
+				open={isUpdate?.dialogue}
+				handleClose={() => setIsUpdate({ dialogue: false })}
+				mutate={mutate}
+			/>
+			<DepartmentInformation
+				open={isInfo?.dialogue}
+				onClose={() => setIsInfo({ dialogue: false })}
+				roleId={isInfo?.role?.id}
+			/>
+
+			<div key={item?.id} className="mb-4 w-full">
+				<div
+					className="group h-full w-full border-2 border-gray-200 
+                border-opacity-60 rounded-lg overflow-hidden shadow-lg"
+				>
+					{item?.photos?.length > 1 ? (
+						<>
+							<Slider {...settings} className="">
+								{item?.photos?.map((data: any, k: any) => (
+									<img
+										key={k}
+										className="lg:h-48 md:h-36 w-full object-cover object-center 
+                        transition duration-500 ease-in-out transform group-hover:scale-105"
+										src={data?.photo}
+										alt="blog"
+									/>
+								))}
+							</Slider>
+						</>
+					) : (
+						<>
+							{item?.photos?.map((data: any, k: any) => (
+								<img
+									key={k}
+									className="lg:h-48 md:h-36 w-full object-cover object-center 
+                        transition duration-500 ease-in-out transform group-hover:scale-105"
+									src={data?.photo}
+									alt="blog"
+								/>
+							))}
+						</>
+					)}
+					<div className="py-1 pt-2 px-4">
+						<h1
+							className="inline-block py-1 title-font text-xl font-extrabold 
+                    text-gray-800 tracking-wide cursor-pointer"
+						>
+							{item?.name}
+						</h1>
+						<p className="text-gray-500 flex items-start">
+							<span className="group flex text-sm items-center justify-center gap-2">
+								<span className="group flex items-center justify-center gap-2">
+									{/* <img src={MANAGER.src} className="w-8 pr-2" alt="" /> */}
+									<span>Model No : </span>
+								</span>
+								{item?.manager}
+							</span>
+						</p>
+						<p className="text-gray-500 flex items-start">
+							<span className="group flex text-sm items-center justify-center gap-2">
+								<span className="group flex items-center justify-center gap-2">
+									{/* <img src={MANAGER.src} className="w-8 pr-2" alt="" /> */}
+									<span>Brand Name : </span>
+								</span>
+								{item?.manager}
+							</span>
+						</p>
+
+						<p className="text-gray-500 flex items-start">
+							<span className="group flex text-sm items-center justify-center gap-2">
+								<span className="group flex items-center justify-center gap-2">
+									{/* <img src={MANAGER.src} className="w-8 pr-2" alt="" /> */}
+									<span>Date Of Purchase : </span>
+								</span>
+								{item?.manager}
+							</span>
+						</p>
+						<p className="text-sm text-slate-600 font-medium py-1 flex items-center gap-3">
+							<span className="group flex text-sm items-center justify-center gap-2">
+								<span className="group flex items-center justify-center gap-2">
+									{/* <img src={MANAGER.src} className="w-8 pr-2" alt="" /> */}
+									<span>Bill amount : </span>
+								</span>
+								{item?.manager}
+							</span>
+						</p>
+						<p className="text-sm text-slate-600 font-medium py-1 flex items-center gap-3">
+							<span className="group flex text-sm items-center justify-center gap-2">
+								<span className="group flex items-center justify-center gap-2">
+									{/* <img src={MANAGER.src} className="w-8 pr-2" alt="" /> */}
+									<span>Current Market Price : </span>
+								</span>
+								{item?.manager}
+							</span>
+						</p>
+						<p className="text-sm text-slate-600 font-medium py-1 flex items-center gap-3">
+							<span className="group flex text-sm items-center justify-center gap-2">
+								<span className="group flex items-center justify-center gap-2">
+									{/* <img src={MANAGER.src} className="w-8 pr-2" alt="" /> */}
+									<span>Docs : </span>
+								</span>
+								{item?.manager}
+							</span>
+						</p>
+
+						<div className="flex bottom-0 ">
+							<span className="group w-full flex border-2 px-2 py-1 items-center justify-center ">
+								<DeleteRounded fontSize="small" color="error" />
+							</span>
+							<span className="group w-full flex border-2 px-2 py-1 items-center justify-center ">
+								<Edit color="primary" fontSize="small" />
+							</span>
+							{/* <div className="group w-full gap-2 flex border-2 px-2 py-1 items-center justify-center ">
+								<p className="font-semibold tracking-wide text-sm">STATUS</p>
+								<ReverseIOSSwitch
+									size="small"
+									checked={item?.isBlocked}
+									onChange={(e) => handleBlock(e, item?.id)}
+								/>
+							</div> */}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
---
+++ src/pages/admin/assets/all-assets.tsx
@@ -0,0 +1,329 @@
+import {
+	Add,
+	Close,
+	FilterListRounded,
+	GridViewRounded,
+	TableRowsRounded,
+} from "@mui/icons-material";
+import {
+	Button,
+	IconButton,
+	MenuItem,
+	TextField,
+	Tooltip,
+} from "@mui/material";
+import { AssetsColumn, AssetsGrid } from "components/admin/assets";
+import { AllBranchColumn, AllBranchGrid } from "components/admin/branch";
+import { AdminBreadcrumbs, Loader } from "components/core";
+import { CreateDepartment } from "components/dialogues";
+import { useFetch } from "hooks";
+import PanelLayout from "layouts/panel";
+import Link from "next/link";
+import { useState } from "react";
+
+const AllAssets = () => {
+	const [isGrid, setIsGrid] = useState(true);
+	const [isCreate, setIsCreate] = useState(false);
+	const [pageNumber, setPageNumber] = useState<number>(1);
+	const [userName, setUsername] = useState<string | null>(null);
+	const [isOrderBy, setIsOrderBy] = useState<string | null>(null);
+
+	const {
+		data: departmentData,
+		mutate,
+		isLoading,
+		pagination,
+	} = useFetch<any>(
+		`departments?page=${pageNumber}&limit=8${
+			userName ? `&contains=${userName}` : ""
+		}${isOrderBy ? `&orderBy=${isOrderBy}` : ""}`
+	);
+
+	return (
+		<PanelLayout title="All Assets - Admin Panel">
+			<section className="px-8 py-4">
+				<CreateDepartment
+					open={isCreate}
+					handleClose={() => setIsCreate(false)}
+					mutate={mutate}
+				/>
+
+				<div className="lg:flex justify-between items-center py-4">
+					<AdminBreadcrumbs links={links} />
+					<div className="md:flex gap-4 items-center">
+						<div className="flex gap-1">
+							<IconButton onClick={() => setIsGrid(true)} size="small">
+								<div
+									className={` p-2 rounded-md grid place-items-center transition-all ease-in-out duration-500 ${
+										isGrid && `border-2 border-theme`
+									}`}
+								>
+									<GridViewRounded className={`${isGrid && `!text-theme`}`} />
+								</div>
+							</IconButton>
+							<IconButton onClick={() => setIsGrid(false)} size="small">
+								<div
+									className={` p-2 rounded-md grid place-items-center transition-all ease-in-out duration-500 ${
+										!isGrid && `border-2 border-theme`
+									}`}
+								>
+									<TableRowsRounded className={`${!isGrid && `!text-theme`}`} />
+								</div>
+							</IconButton>
+						</div>
+						<Link href="/admin/assets/create-assets">
+							<Button
+								variant="contained"
+								className="!bg-theme"
+								startIcon={<Add />}
+							>
+								CREATE ASSETS
+							</Button>
+						</Link>
+					</div>
+				</div>
+				<div>
+					<div className="md:flex gap-4 justify-between w-full py-2">
+						<div
+							className={`w-10 h-10 flex justify-center items-center rounded-md shadow-lg bg-theme
+                `}
+						>
+							<IconButton
+								onClick={() => {
+									setIsOrderBy(null);
+									setUsername(null);
+								}}
+							>
+								<Tooltip
+									title={
+										isOrderBy != null || userName != null
+											? `Remove Filters`
+											: `Filter`
+									}
+								>
+									{isOrderBy != null || userName != null ? (
+										<Close className={"!text-white"} />
+									) : (
+										<FilterListRounded className={"!text-white"} />
+									)}
+								</Tooltip>
+							</IconButton>
+						</div>
+
+						<div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+							<TextField
+								fullWidth
+								size="small"
+								id="name"
+								value={userName ? userName : ""}
+								onChange={(e) => {
+									setPageNumber(1), setUsername(e.target.value);
+								}}
+								placeholder="Asset Name"
+								name="name"
+							/>
+							<TextField
+								fullWidth
+								select
+								label="Ascending/Descending"
+								size="small"
+								value={isOrderBy ? isOrderBy : ""}
+								onChange={(e) => {
+									setPageNumber(1), setIsOrderBy(e?.target?.value);
+								}}
+							>
+								{short.map((option) => (
+									<MenuItem key={option.id} value={option.value}>
+										{option.name}
+									</MenuItem>
+								))}
+							</TextField>
+						</div>
+					</div>
+				</div>
+				{isGrid ? (
+					<>
+						{isLoading && <Loader />}
+						<AssetsGrid data={branchData} mutate={mutate} />
+					</>
+				) : (
+					<>
+						{isLoading && <Loader />}
+						<AssetsColumn data={branchData} mutate={mutate} />
+					</>
+				)}
+				{/* {branchData?.length === 0 ? <LoaderAnime /> : null}
+        {Math.ceil(
+          Number(pagination?.total || 1) /
+          Number(pagination?.limit || 1)
+        ) > 1 ? (
+          <div className="flex justify-center py-8">
+            <Stack spacing={2}>
+              <Pagination
+                count={Math.ceil(
+                  Number(pagination?.total || 1) /
+                  Number(pagination?.limit || 1)
+                )}
+                onChange={(e, v: number) => {
+                  setPageNumber(v);
+                }}
+                variant="outlined"
+                page={pageNumber}
+              />
+            </Stack>
+          </div>
+        ) : (
+          ""
+        )} */}
+			</section>
+		</PanelLayout>
+	);
+};
+
+export default AllAssets;
+
+const links = [
+	// { id: 1, page: "Branch", link: "/admin/branch" },
+	{
+		id: 2,
+		page: "All Assets",
+		link: "/admin/assets/all-assets",
+	},
+];
+const short = [
+	{ id: 1, value: "name:asc", name: "Name Ascending" },
+	{ id: 2, value: "name:desc", name: "Name Descending" },
+	{ id: 3, value: "createdAt:asc", name: "CreatedAt Ascending" },
+	{ id: 4, value: "createdAt:desc", name: "CreatedAt Descending" },
+];
+
+const branchData = [
+	{
+		id: "01",
+		name: "Laptop",
+		photos: [
+			{
+				i: 1,
+				photo:
+					"https://img.freepik.com/free-photo/laptop-with-blank-black-screen-white-table_53876-97915.jpg?w=996&t=st=1685941336~exp=1685941936~hmac=600279e95b0695fc35146572dbae5a835b4b173cda718b594fff338d67c92c1f",
+			},
+			{
+				i: 2,
+				photo:
+					"https://img.freepik.com/free-photo/work-desk-with-computer-cup-with-pens-pencils-against-white-wall_181624-44978.jpg?w=996&t=st=1685941433~exp=1685942033~hmac=dcd1ecea9d29a5b13041ca4b54294595ad87ef525867f1b8cf5c1d3c3be44f88",
+			},
+			{
+				i: 3,
+				photo:
+					"https://img.freepik.com/free-photo/empty-meeting-room-with-table-whiteboard_1262-3763.jpg?w=996&t=st=1685941618~exp=1685942218~hmac=9cd100c941a6b9bceb1c82b24a870c0ba7da1bbe0d50a6acd83e162bc284d87c",
+			},
+			{
+				i: 4,
+				photo:
+					"https://img.freepik.com/free-photo/home-printer-based-toner_23-2149287461.jpg?w=996&t=st=1685943942~exp=1685944542~hmac=ca684816e145f3b09d5192377ff31eb0fed21d348b67d326b8582c36defce9e0",
+			},
+		],
+		ModelNo: "Cuttack, 754008, Odisha,",
+		phone: "2131335465",
+		email: "workspace1@yard.com",
+		country: "India",
+		manager: "Abhilash",
+		managerPhoto: "photo",
+	},
+	{
+		id: "02",
+		name: "Desktop",
+		photos: [
+			{
+				i: 1,
+				photo:
+					"https://img.freepik.com/free-photo/laptop-with-blank-black-screen-white-table_53876-97915.jpg?w=996&t=st=1685941336~exp=1685941936~hmac=600279e95b0695fc35146572dbae5a835b4b173cda718b594fff338d67c92c1f",
+			},
+			{
+				i: 2,
+				photo:
+					"https://img.freepik.com/free-photo/work-desk-with-computer-cup-with-pens-pencils-against-white-wall_181624-44978.jpg?w=996&t=st=1685941433~exp=1685942033~hmac=dcd1ecea9d29a5b13041ca4b54294595ad87ef525867f1b8cf5c1d3c3be44f88",
+			},
+			{
+				i: 3,
+				photo:
+					"https://img.freepik.com/free-photo/empty-meeting-room-with-table-whiteboard_1262-3763.jpg?w=996&t=st=1685941618~exp=1685942218~hmac=9cd100c941a6b9bceb1c82b24a870c0ba7da1bbe0d50a6acd83e162bc284d87c",
+			},
+			{
+				i: 4,
+				photo:
+					"https://img.freepik.com/free-photo/home-printer-based-toner_23-2149287461.jpg?w=996&t=st=1685943942~exp=1685944542~hmac=ca684816e145f3b09d5192377ff31eb0fed21d348b67d326b8582c36defce9e0",
+			},
+		],
+		location: "Bhubaneswar, 751024, Odisha,",
+		email: "workspace2@yard.com",
+		country: "India",
+		phone: "2131335465",
+		manager: "Ashutosh",
+		managerPhoto: "photo",
+	},
+	{
+		id: "03",
+		name: "Office Chairs",
+		location: "Electronic City, Karnatak,",
+		photos: [
+			{
+				i: 1,
+				photo:
+					"https://img.freepik.com/free-photo/laptop-with-blank-black-screen-white-table_53876-97915.jpg?w=996&t=st=1685941336~exp=1685941936~hmac=600279e95b0695fc35146572dbae5a835b4b173cda718b594fff338d67c92c1f",
+			},
+			{
+				i: 2,
+				photo:
+					"https://img.freepik.com/free-photo/work-desk-with-computer-cup-with-pens-pencils-against-white-wall_181624-44978.jpg?w=996&t=st=1685941433~exp=1685942033~hmac=dcd1ecea9d29a5b13041ca4b54294595ad87ef525867f1b8cf5c1d3c3be44f88",
+			},
+			{
+				i: 3,
+				photo:
+					"https://img.freepik.com/free-photo/empty-meeting-room-with-table-whiteboard_1262-3763.jpg?w=996&t=st=1685941618~exp=1685942218~hmac=9cd100c941a6b9bceb1c82b24a870c0ba7da1bbe0d50a6acd83e162bc284d87c",
+			},
+			{
+				i: 4,
+				photo:
+					"https://img.freepik.com/free-photo/home-printer-based-toner_23-2149287461.jpg?w=996&t=st=1685943942~exp=1685944542~hmac=ca684816e145f3b09d5192377ff31eb0fed21d348b67d326b8582c36defce9e0",
+			},
+		],
+		email: "workspace3@yard.com",
+		phone: "2131335465",
+		country: "India",
+		manager: "Prasad",
+		managerPhoto: "photo",
+	},
+	{
+		id: "04",
+		name: "Printer",
+		photos: [
+			{
+				i: 1,
+				photo:
+					"https://img.freepik.com/free-photo/laptop-with-blank-black-screen-white-table_53876-97915.jpg?w=996&t=st=1685941336~exp=1685941936~hmac=600279e95b0695fc35146572dbae5a835b4b173cda718b594fff338d67c92c1f",
+			},
+			{
+				i: 2,
+				photo:
+					"https://img.freepik.com/free-photo/work-desk-with-computer-cup-with-pens-pencils-against-white-wall_181624-44978.jpg?w=996&t=st=1685941433~exp=1685942033~hmac=dcd1ecea9d29a5b13041ca4b54294595ad87ef525867f1b8cf5c1d3c3be44f88",
+			},
+			{
+				i: 3,
+				photo:
+					"https://img.freepik.com/free-photo/empty-meeting-room-with-table-whiteboard_1262-3763.jpg?w=996&t=st=1685941618~exp=1685942218~hmac=9cd100c941a6b9bceb1c82b24a870c0ba7da1bbe0d50a6acd83e162bc284d87c",
+			},
+			{
+				i: 4,
+				photo:
+					"https://img.freepik.com/free-photo/home-printer-based-toner_23-2149287461.jpg?w=996&t=st=1685943942~exp=1685944542~hmac=ca684816e145f3b09d5192377ff31eb0fed21d348b67d326b8582c36defce9e0",
+			},
+		],
+		location: "BTM Layout, Karnatak,",
+		phone: "2131335465",
+		email: "workspace3@yard.com",
+		country: "India",
+		manager: "Srinu",
+		managerPhoto: "photo",
+	},
+];
