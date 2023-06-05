5560b6c0b222d0d5261d62afd8c4a5dc2d180c49
yard-tech <searchingyardpc@gmail.com>
2023-06-05 18:29:59 +0530

    working on socket

--- src/components/chat/ChatRightSection.tsx
+++ src/components/chat/ChatRightSection.tsx
@@ -4,7 +4,6 @@ import {
   ImageOutlined,
   InsertLink,
   Send,
-  SentimentSatisfiedAlt,
 } from "@mui/icons-material";
 import { CircularProgress, IconButton, Tooltip } from "@mui/material";
 import {
@@ -12,13 +11,14 @@ import {
   ChatSendFiles,
   ChatSendImage,
 } from "components/dialogues";
-import { useAuth, useChange, useChatData } from "hooks";
+import { useChange, useChatData } from "hooks";
 import { useRef, useState } from "react";
 import ChatHead from "./ChatHead";
-import ChatMessage from "./ChatMessage";
+
 import DefaultChatView from "./DefaultChatView";
-import { MessageCategory } from "types";
 import ChatSendLink from "components/dialogues/ChatSendLink";
+import { IChatMessages } from "types";
+import MainChatViewContainer from "./MainChatViewContainer";
 
 interface Props {
   id?: number;
@@ -28,12 +28,14 @@ interface Props {
 }
 
 const ChatRightSection = () => {
+  const [pageNo, setPageNo] = useState(1);
   const [isUpload, setIsUpload] = useState(false);
   const [isCode, setIsCode] = useState(false);
   const [isLink, setIsLink] = useState(false);
   const [isImage, setIsImage] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [isMessage, setIsMessage] = useState<string | null>(null);
+  const [allChatData, setAllChatData] = useState<IChatMessages[]>([]);
   const textRef = useRef<HTMLInputElement | null>(null);
   const { change } = useChange();
   const handleClick = () => {
@@ -42,7 +44,7 @@ const ChatRightSection = () => {
       textRef.current.focus();
     }
   };
-  const { user } = useAuth();
+
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClose = () => {
@@ -54,6 +56,7 @@ const ChatRightSection = () => {
     currentChatProfileDetails,
     handleSendNewMessage,
     revalidateCurrentChat,
+    handleNextChatPage,
   } = useChatData();
 
   const handleSend = async () => {
@@ -89,7 +92,6 @@ const ChatRightSection = () => {
       }
     }
   };
-  console.log(currentChatMessage);
 
   return (
     <>
@@ -120,51 +122,7 @@ const ChatRightSection = () => {
           <div className="w-full h-full">
             <ChatHead />
             <div className="h-[72%] overflow-y-auto">
-              <div className="px-4 pb-4">
-                {currentChatMessage?.map((item) => (
-                  <div
-                    key={item?.id}
-                    className={`mt-4 flex w-full ${
-                      item?.category === "event"
-                        ? `justify-center`
-                        : item?.sender?.id === user?.id
-                        ? `justify-end`
-                        : "justify-start"
-                    }`}
-                  >
-                    <>
-                      {item?.category === "event" ? (
-                        <EventTemplate data={item} />
-                      ) : (
-                        <ChatMessage
-                          data={item}
-                          activeProfile={currentChatProfileDetails}
-                        />
-                      )}
-
-                      {/* {item?.type === "text" ? (
-                        <TextMessage
-                          data={item}
-                          activeProfile={currentChatProfileDetails}
-                        />
-                      ) : item?.type === "image" ? (
-                        <ImageMessage
-                          data={item}
-                          activeProfile={currentChatProfileDetails}
-                        />
-                      ) : item?.type === "event" ? (
-                        <EventTemplate data={item} />
-                      ) : item?.type === "code" ? (
-                        <CodeMessage data={item} />
-                      ) : item?.type === "code" ? (
-                        <DocMessage data={item} />
-                      ) : (
-                        "No format specified"
-                      )} */}
-                    </>
-                  </div>
-                ))}
-              </div>
+              <MainChatViewContainer />
             </div>
             <div className="h-20 w-full border-2 flex items-center px-8 justify-between">
               <div className="h-10 px-3 rounded-full w-[70%] border-2 flex justify-between items-center">
@@ -218,110 +176,3 @@ const ChatRightSection = () => {
 };
 
 export default ChatRightSection;
-
-const chats = [
-  {
-    id: 1,
-    text: "Hey when you woke up?",
-    sendBy: "sender",
-    type: "text",
-    reaction: { text: "👍" },
-    author: {
-      id: 2,
-      name: "Srinu Reddy",
-      message: "Okay",
-      type: "person",
-      photo:
-        "https://media.npr.org/assets/img/2022/11/08/ap22312071681283-0d9c328f69a7c7f15320e8750d6ea447532dff66-s1100-c50.jpg",
-    },
-  },
-  { id: 2, text: "Yeah Early morning. 7:30 AM.", sendBy: "you", type: "text" },
-  {
-    id: 9,
-    text: "Abhilash changed this group's icon",
-    type: "event",
-  },
-  {
-    id: 8,
-    text: "If any case i will be available, i will let you know in the office only.",
-    link: "https://w0.peakpx.com/wallpaper/1008/1001/HD-wallpaper-tiger-black-look-thumbnail.jpg",
-    sendBy: "you",
-    type: "image",
-  },
-  {
-    id: 13,
-    text: "By the way when we will meet for this discussion?",
-    sendBy: "sender",
-    type: "doc",
-  },
-  {
-    id: 12,
-    text: "By the way when we will meet for this discussion?",
-    sendBy: "you",
-    type: "code",
-  },
-  {
-    id: 3,
-    text: "Have you gone through this new document shared by our sales team?",
-    sendBy: "you",
-    type: "text",
-  },
-  {
-    id: 4,
-    text: "Yeah, i had. I found some points confusing. let's meet and discuss about those topics today.",
-    sendBy: "sender",
-    type: "text",
-    author: {
-      id: 2,
-      name: "Srinu Reddy",
-      message: "Okay",
-      type: "person",
-      photo:
-        "https://media.npr.org/assets/img/2022/11/08/ap22312071681283-0d9c328f69a7c7f15320e8750d6ea447532dff66-s1100-c50.jpg",
-    },
-  },
-  {
-    id: 5,
-    text: "By the way when we will meet for this discussion?",
-    sendBy: "you",
-    type: "text",
-  },
-  {
-    id: 6,
-    text: "I am not sure whether i can be available for today or not. Because today i have many works and a hectic busy schedules. So we may discuss later.",
-    sendBy: "sender",
-    type: "text",
-    author: {
-      id: 2,
-      name: "Srinu Reddy",
-      message: "Okay",
-      type: "person",
-      photo:
-        "https://media.npr.org/assets/img/2022/11/08/ap22312071681283-0d9c328f69a7c7f15320e8750d6ea447532dff66-s1100-c50.jpg",
-    },
-  },
-  {
-    id: 7,
-    text: "If any case i will be available, i will let you know in the office only.",
-    sendBy: "sender",
-    type: "text",
-    author: {
-      id: 2,
-      name: "Srinu Reddy",
-      message: "Okay",
-      type: "person",
-      photo:
-        "https://media.npr.org/assets/img/2022/11/08/ap22312071681283-0d9c328f69a7c7f15320e8750d6ea447532dff66-s1100-c50.jpg",
-    },
-  },
-];
-
-const EventTemplate = ({ data }: any) => {
-  return (
-    <div className="flex justify-center items-center">
-      <span className="text-xs tracking-wide px-4 py-1 bg-slate-200 rounded-full">
-        {data?.text}
-      </span>
-    </div>
-  );
-};
