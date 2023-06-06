bf608207e928ecfc119cd63c4aa826b7ae426f8c
KumarGudu <kg150201@gmail.com>
2023-06-06 10:50:15 +0530

    update branch

--- src/components/dialogues/UpdateBranch.tsx
+++ src/components/dialogues/UpdateBranch.tsx
@@ -80,15 +80,13 @@ const UpdateBranch = ({
   return (
     <Dialog
       onClose={handleClose}
+      maxWidth="lg"
       aria-labelledby="customized-dialog-title"
       open={open}
     >
-      <DialogTitle
-        id="customized-dialog-title"
-        sx={{ p: 2, minWidth: "18rem !important" }}
-      >
-        <p className="text-center text-xl font-bold text-theme tracking-wide">
-          UPDATE BRANCH
+      <DialogTitle id="customized-dialog-title">
+        <p className="text-center text-md font-bold text-theme te tracking-wide">
+          UPDATE
         </p>
         <IconButton
           aria-label="close"
@@ -105,166 +103,167 @@ const UpdateBranch = ({
           </Tooltip>
         </IconButton>
       </DialogTitle>
-      <DialogContent className="app-scrollbar" sx={{ p: 2 }}>
+      <DialogContent className="app-scrollbar" sx={{ p: 3 }}>
         <div className="md:w-[50rem] w-[72vw] md:px-4 px-2 tracking-wide flex flex-col gap-3 text-sm py-4">
           <div className="flex flex-col items-center w-full">
-            <form onSubmit={formik.handleSubmit} className="grid lg:grid-cols-2 gap-4">
-              <div className="w-full">
-                <p className="text-theme font-semibold">
-                  Branch Name <span className="text-red-600">*</span>
-                </p>
-                <TextField
-                  fullWidth
-                  size="small"
-                  placeholder="Enter Branch Name"
-                  name="name"
-                  value={formik.values.name}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={formik.touched.name && !!formik.errors.name}
-                  helperText={formik.touched.name && formik.errors.name}
-                />
-              </div>
-              <div className="w-full">
-                <p className="text-theme font-semibold">
-                  Assign Manager <span className="text-red-600">*</span>
-                </p>
-                <Autocomplete
-                  fullWidth
-                  size="small"
-                  id="managerId"
-                  options={userData || []}
-                  getOptionLabel={(option: any) =>
-                    option.name ? option.name : ""
-                  }
-                  isOptionEqualToValue={(option, value) =>
-                    option.id === value.userId
-                  }
-                  value={
-                    formik.values?.managerId
-                      ? userData?.find(
-                        (option: any) => option.id === formik.values.managerId
-                      )
-                      : {}
-                  }
-                  onChange={(e: any, r: any) => {
-                    formik.setFieldValue("managerId", r?.id);
-                  }}
-                  renderOption={(props, option) => (
-                    <Box
-                      component="li"
-                      sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
-                      {...props}
-                    >
-                      {option.name}
-                    </Box>
-                  )}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      placeholder="Manager Name"
-                      onBlur={formik.handleBlur}
-                      error={formik.touched.managerId && !!formik.errors.managerId}
-                      helperText={formik.touched.managerId && formik.errors.managerId}
-                    />
-                  )}
-                />
-              </div>
-              <div className="w-full">
-                <p className="text-theme font-semibold">
-                  Phone
-                </p>
-                <TextField
-                  size="small"
-                  fullWidth
-                  placeholder="Phone"
-                  name="phone"
-                  value={formik.values.phone}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={formik.touched.phone && !!formik.errors.phone}
-                  helperText={formik.touched.phone && formik.errors.phone}
-                />
-              </div>
-              <div className="w-full">
-                <p className="text-theme font-semibold">
-                  Email
-                </p>
-                <TextField
-                  size="small"
-                  fullWidth
-                  placeholder="Email"
-                  name="email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={formik.touched.email && !!formik.errors.email}
-                  helperText={formik.touched.email && formik.errors.email}
-                />
-              </div>
-              <div className="w-full">
-                <p className="text-theme font-semibold">
-                  Location
-                </p>
-                <TextField
-                  size="small"
-                  fullWidth
-                  placeholder="Enter Location"
-                  name="location"
-                  value={formik.values.location}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={formik.touched.location && !!formik.errors.location}
-                  helperText={formik.touched.location && formik.errors.location}
-                />
-              </div>
-              <div className="w-full">
-                <p className="text-theme font-semibold">
-                  Country <span className="text-red-600">*</span>
-                </p>
-                <Autocomplete
-                  size="small"
-                  options={countries}
-                  autoHighlight
-                  value={formik.values?.country as any}
-                  fullWidth
-                  onChange={(e, r: any) =>
-                    formik.setFieldValue("country", r?.label)
-                  }
-                  renderOption={(props, option: any) => (
-                    <Box
-                      component="li"
-                      sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
-                      {...props}
-                    >
-                      <img
-                        loading="lazy"
-                        width="20"
-                        src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
-                        srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
-                        alt=""
+            <form onSubmit={formik.handleSubmit} className="w-full">
+              <div className="grid lg:grid-cols-2 gap-4">
+                <div className="w-full">
+                  <p className="text-theme font-semibold">
+                    Branch Name <span className="text-red-600">*</span>
+                  </p>
+                  <TextField
+                    fullWidth
+                    size="small"
+                    placeholder="Enter Branch Name"
+                    name="name"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.name && !!formik.errors.name}
+                    helperText={formik.touched.name && formik.errors.name}
+                  />
+                </div>
+                <div className="w-full">
+                  <p className="text-theme font-semibold">
+                    Assign Manager <span className="text-red-600">*</span>
+                  </p>
+                  <Autocomplete
+                    fullWidth
+                    size="small"
+                    id="managerId"
+                    options={userData || []}
+                    getOptionLabel={(option: any) =>
+                      option.name ? option.name : ""
+                    }
+                    isOptionEqualToValue={(option, value) =>
+                      option.id === value.userId
+                    }
+                    value={
+                      formik.values?.managerId
+                        ? userData?.find(
+                          (option: any) => option.id === formik.values.managerId
+                        )
+                        : {}
+                    }
+                    onChange={(e: any, r: any) => {
+                      formik.setFieldValue("managerId", r?.id);
+                    }}
+                    renderOption={(props, option) => (
+                      <Box
+                        component="li"
+                        sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+                        {...props}
+                      >
+                        {option.name}
+                      </Box>
+                    )}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        placeholder="Manager Name"
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.managerId && !!formik.errors.managerId}
+                        helperText={formik.touched.managerId && formik.errors.managerId}
                       />
-                      {option?.label}
-                    </Box>
-                  )}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label="Choose a country"
-                      name="country"
-                      error={
-                        formik.touched?.country && Boolean(formik.errors?.country)
-                      }
-                      onBlur={formik.handleBlur}
-                      helperText={formik.touched?.country && formik.errors?.country}
-                      inputProps={{
-                        ...params.inputProps,
-                      }}
-                    />
-                  )}
-                />
+                    )}
+                  />
+                </div>
+                <div className="w-full">
+                  <p className="text-theme font-semibold">
+                    Phone
+                  </p>
+                  <TextField
+                    size="small"
+                    fullWidth
+                    placeholder="Phone"
+                    name="phone"
+                    value={formik.values.phone}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.phone && !!formik.errors.phone}
+                    helperText={formik.touched.phone && formik.errors.phone}
+                  />
+                </div>
+                <div className="w-full">
+                  <p className="text-theme font-semibold">
+                    Email
+                  </p>
+                  <TextField
+                    size="small"
+                    fullWidth
+                    placeholder="Email"
+                    name="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.email && !!formik.errors.email}
+                    helperText={formik.touched.email && formik.errors.email}
+                  />
+                </div>
+                <div className="w-full">
+                  <p className="text-theme font-semibold">
+                    Location
+                  </p>
+                  <TextField
+                    size="small"
+                    fullWidth
+                    placeholder="Enter Location"
+                    name="location"
+                    value={formik.values.location}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.location && !!formik.errors.location}
+                    helperText={formik.touched.location && formik.errors.location}
+                  />
+                </div>
+                <div className="w-full">
+                  <p className="text-theme font-semibold">
+                    Country <span className="text-red-600">*</span>
+                  </p>
+                  <Autocomplete
+                    size="small"
+                    options={countries}
+                    autoHighlight
+                    value={formik.values?.country as any}
+                    fullWidth
+                    onChange={(e, r: any) =>
+                      formik.setFieldValue("country", r?.label)
+                    }
+                    renderOption={(props, option: any) => (
+                      <Box
+                        component="li"
+                        sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+                        {...props}
+                      >
+                        <img
+                          loading="lazy"
+                          width="20"
+                          src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+                          srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+                          alt=""
+                        />
+                        {option?.label}
+                      </Box>
+                    )}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        label="Choose a country"
+                        name="country"
+                        error={
+                          formik.touched?.country && Boolean(formik.errors?.country)
+                        }
+                        onBlur={formik.handleBlur}
+                        helperText={formik.touched?.country && formik.errors?.country}
+                        inputProps={{
+                          ...params.inputProps,
+                        }}
+                      />
+                    )}
+                  />
+                </div>
               </div>
-
               <Button
                 type="submit"
                 fullWidth
