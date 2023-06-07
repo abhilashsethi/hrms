7631e45422579581047d982babd5b63087d8dce9
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-06-07 10:08:48 +0530

    view assigned assets onGoing

--- src/components/admin/assets/index.tsx
+++ src/components/admin/assets/index.tsx
@@ -1,2 +1,4 @@
 export { default as AssetsGrid } from "./AssetsGrid";
 export { default as AssetsColumn } from "./AssetsColumn";
+export { default as AssignedAssetsGrid } from "./AssignedAssetsGrid";
+export { default as AssignedAssetsColumn } from "./AssignedAssetsColumn";
