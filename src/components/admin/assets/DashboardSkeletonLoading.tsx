d773fc40d6e8dbc3d62b8dacd0b6a15a815da410
KumarGudu <kg150201@gmail.com>
2023-06-08 10:33:36 +0530

    is loading

--- src/components/admin/assets/DashboardSkeletonLoading.tsx
+++ src/components/admin/assets/DashboardSkeletonLoading.tsx
@@ -2,12 +2,12 @@
 const DashboardSkeletonLoading = () => {
   return (
     <div className="grid gap-4">
-      <div className="py-2 bg-slate-300 shadow-lg rounded-lg px-2 w-full animate-pulse">
+      <div className="py-2 shadow-lg rounded-lg px-2 w-full animate-pulse">
         <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-4">
-          <div className="w-20 h-32 rounded bg-gray-700"></div>
-          <div className="w-20 h-32 rounded bg-gray-700"></div>
-          <div className="w-20 h-32 rounded bg-gray-700"></div>
-          <div className="w-20 h-32 rounded bg-gray-700"></div>
+          <div className="w-full h-36 rounded bg-gray-700"></div>
+          <div className="w-full h-36 rounded bg-gray-700"></div>
+          <div className="w-full h-36 rounded bg-gray-700"></div>
+          <div className="w-full h-36 rounded bg-gray-700"></div>
         </div>
       </div>
       <div className="grid lg:grid-cols-2 content-between gap-6">
