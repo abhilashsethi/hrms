e44523ce147880793643ed1e54989cc10750a1bf
Srinu Reddy <73178183+srinureddy7@users.noreply.github.com>
2023-06-12 12:18:00 +0530

    photo section updated.

--- src/components/drawer/ViewLeaveDrawer.tsx
+++ src/components/drawer/ViewLeaveDrawer.tsx
@@ -9,180 +9,137 @@ import { useEffect, useState } from "react";
 import { User } from "types";
 
 type Props = {
-	open?: boolean | any;
-	onClose: () => void;
-	setViewLeaves?: any;
-	employeeId?: any;
+  open?: boolean | any;
+  onClose: () => void;
+  setViewLeaves?: any;
+  employeeId?: any;
 };
 
 const useStyles = makeStyles((theme) => ({
-	container: {
-		width: "100vw",
-		[theme.breakpoints.up("sm")]: {
-			maxWidth: "50vw",
-		},
-		[theme.breakpoints.up("md")]: {
-			maxWidth: "80vw",
-		},
-		[theme.breakpoints.up("lg")]: {
-			maxWidth: "30vw",
-		},
-	},
+  container: {
+    width: "100vw",
+    [theme.breakpoints.up("sm")]: {
+      maxWidth: "50vw",
+    },
+    [theme.breakpoints.up("md")]: {
+      maxWidth: "80vw",
+    },
+    [theme.breakpoints.up("lg")]: {
+      maxWidth: "30vw",
+    },
+  },
 }));
 
-const style = {
-	position: "absolute" as "absolute",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	width: 400,
-	// height: 600,
-	bgcolor: "background.paper",
-	// border: "2px solid #000",
-	borderRadius: "10px",
-	boxShadow: 24,
-	p: 4,
-};
-
-const Projects_Details = [
-	{
-		id: 1,
-		date: "10-05-2023",
-		status: "Approved",
-		credit: -1,
-	},
-	{
-		id: 2,
-		date: "12-05-2023",
-		status: "Rejected",
-		credit: -2,
-	},
-	{
-		id: 3,
-		date: "14-05-2023",
-		status: "Approved",
-		credit: -1,
-	},
-	{
-		id: 4,
-		date: "16-05-2023",
-		status: "Approved",
-		credit: -2,
-	},
-];
-
 const ViewLeaveDrawer = ({
-	open,
-	onClose,
-	setViewLeaves,
-	employeeId,
+  open,
+  onClose,
+  setViewLeaves,
+  employeeId,
 }: Props) => {
-	const router = useRouter();
-	const [loading, setLoading] = useState(false);
-	const [searchTerm, setSearchTerm] = useState("");
-	const [selectedUser, setSelectedUser] = useState<string | null>(null);
-	const [searchedUser, setSearchedUser] = useState<any>([]);
-	const [openInfoModal, setOpenInfoModal] = useState(false);
-	const handleInfoOpen = () => {
-		setOpenInfoModal(true);
-	};
-	const handleInfoCloseModal = () => setOpenInfoModal(false);
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedUser, setSelectedUser] = useState<string | null>(null);
+  const [searchedUser, setSearchedUser] = useState<any>([]);
+  const [openInfoModal, setOpenInfoModal] = useState(false);
+  const handleInfoOpen = () => {
+    setOpenInfoModal(true);
+  };
+  const handleInfoCloseModal = () => setOpenInfoModal(false);
 
-	const { data: leaveData, isLoading: leaveDataLoading } = useFetch<any[]>(
-		`leaves?employeeID=${employeeId}`
-	);
+  const { data: leaveData, isLoading: leaveDataLoading } = useFetch<any[]>(
+    `leaves?employeeID=${employeeId}`
+  );
 
-	const { data: users, isLoading } = useFetch<User[]>(`users`);
-	useEffect(() => {
-		if (users) {
-			const filtered = users.filter((user) =>
-				user.name.toLowerCase().includes(searchTerm.toLowerCase())
-			);
-			setSearchedUser(filtered);
-		}
-	}, [users, searchTerm]);
+  const { data: users, isLoading } = useFetch<User[]>(`users`);
+  useEffect(() => {
+    if (users) {
+      const filtered = users.filter((user) =>
+        user.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setSearchedUser(filtered);
+    }
+  }, [users, searchTerm]);
 
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-		<>
-			<Drawer anchor="right" open={open} onClose={() => onClose && onClose()}>
-				<Container style={{ marginTop: "1rem" }} className={classes.container}>
-					{/* Drawer Element */}
+  return (
+    <>
+      <Drawer anchor="right" open={open} onClose={() => onClose && onClose()}>
+        <Container style={{ marginTop: "1rem" }} className={classes.container}>
+          <div className="flex items-center justify-between pb-4">
+            <p className="text-lg font-bold text-theme">View Leaves</p>
+            <IconButton onClick={() => onClose()}>
+              <Close
+                fontSize="small"
+                className="text-red-500 block md:hidden"
+              />
+            </IconButton>
+          </div>
 
-					<div className="flex items-center justify-between pb-4">
-						<p className="text-lg font-bold text-theme">View Leaves</p>
-						<IconButton onClick={() => onClose()}>
-							<Close
-								fontSize="small"
-								className="text-red-500 block md:hidden"
-							/>
-						</IconButton>
-					</div>
-
-					{isLoading && <Loader />}
-					<div className="mt-4 flex flex-col gap-4">
-						{leaveData?.map((item, index) => {
-							return (
-								<div className="">
-									<div
-										key={index}
-										className={`w-full h-full rounded-l-xl shadow-xl px-2 py-2 bg-[#edf4fe] my-3`}
-									>
-										<div className="flex flex-col gap-3 font-semibold text-blue-700">
-											{item?.variant === "MultipleDays" ? (
-												<>
-													<div className="flex gap-2">
-														Start Date :{" "}
-														<span className="text-black font-medium">
-															{moment(item?.startDate)?.format("DD/MM/YYYY")}
-														</span>
-													</div>
-													<div className="flex gap-2">
-														End Date :{" "}
-														<span className="text-black font-medium">
-															{moment(item?.endDate)?.format("DD/MM/YYYY")}
-														</span>
-													</div>
-												</>
-											) : (
-												<div className="flex gap-2">
-													Date :{" "}
-													<span className="text-black font-medium">
-														{moment(item?.startDate)?.format("DD/MM/YYYY")}
-													</span>
-												</div>
-											)}
-											<div>
-												Status :{" "}
-												<span
-													className={`text-sm ${
-														item?.status === "Rejected"
-															? "bg-red-500"
-															: item?.status === "Pending"
-															? "bg-yellow-500"
-															: "bg-green-500"
-													} text-white p-1 rounded-lg`}
-												>
-													{item?.status}
-												</span>
-											</div>
-											<div>
-												Leave Type :{" "}
-												<span className="text-black font-medium">
-													{item?.type}
-												</span>
-											</div>
-										</div>
-									</div>
-								</div>
-							);
-						})}
-					</div>
-				</Container>
-			</Drawer>
-		</>
-	);
+          {isLoading && <Loader />}
+          <div className="mt-4 flex flex-col gap-4">
+            {leaveData?.map((item, index) => {
+              return (
+                <div className="">
+                  <div
+                    key={index}
+                    className={`w-full h-full rounded-l-xl shadow-xl px-2 py-2 bg-[#edf4fe] my-3`}
+                  >
+                    <div className="flex flex-col gap-3 font-semibold text-blue-700">
+                      {item?.variant === "MultipleDays" ? (
+                        <>
+                          <div className="flex gap-2">
+                            Start Date :{" "}
+                            <span className="text-black font-medium">
+                              {moment(item?.startDate)?.format("DD/MM/YYYY")}
+                            </span>
+                          </div>
+                          <div className="flex gap-2">
+                            End Date :{" "}
+                            <span className="text-black font-medium">
+                              {moment(item?.endDate)?.format("DD/MM/YYYY")}
+                            </span>
+                          </div>
+                        </>
+                      ) : (
+                        <div className="flex gap-2">
+                          Date :{" "}
+                          <span className="text-black font-medium">
+                            {moment(item?.startDate)?.format("DD/MM/YYYY")}
+                          </span>
+                        </div>
+                      )}
+                      <div>
+                        Status :{" "}
+                        <span
+                          className={`text-sm ${
+                            item?.status === "Rejected"
+                              ? "bg-red-500"
+                              : item?.status === "Pending"
+                              ? "bg-yellow-500"
+                              : "bg-green-500"
+                          } text-white p-1 rounded-lg`}
+                        >
+                          {item?.status}
+                        </span>
+                      </div>
+                      <div>
+                        Leave Type :{" "}
+                        <span className="text-black font-medium">
+                          {item?.type}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </Container>
+      </Drawer>
+    </>
+  );
 };
 
 export default ViewLeaveDrawer;
