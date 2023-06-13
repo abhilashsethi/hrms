7ec4092a049ebc5c76db5269c2dff20156038038
KumarGudu <kg150201@gmail.com>
2023-06-13 10:50:58 +0530

    drawer ui

--- src/components/admin/roles/AllRollGrid.tsx
+++ src/components/admin/roles/AllRollGrid.tsx
@@ -1,7 +1,7 @@
 import { Delete, DoorSliding, Edit, Info } from "@mui/icons-material";
-import { Grid, IconButton, Tooltip } from "@mui/material";
+import { IconButton, Tooltip } from "@mui/material";
 import { UpdateRole } from "components/dialogues";
-import { RoleInformation } from "components/drawer";
+import { RoleInformation, RoleWisePageAccess } from "components/drawer";
 import { useChange } from "hooks";
 import { useState } from "react";
 import Swal from "sweetalert2";
@@ -13,6 +13,8 @@ interface Props {
 }
 
 const AllRollGrid = ({ data, mutate }: Props) => {
+  const [assetHistory, setAssetHistory] = useState(false);
+  const [assetId, setAssetId] = useState("");
   const [roleData, setRoleData] = useState("");
   const [loading, setLoading] = useState(false);
   const [isInfo, setIsInfo] = useState<{
@@ -66,6 +68,11 @@ const AllRollGrid = ({ data, mutate }: Props) => {
   };
   return (
     <>
+      <RoleWisePageAccess
+        open={assetHistory}
+        onClose={() => setAssetHistory(false)}
+        assetId={assetId}
+      />
       <UpdateRole
         id={isUpdate?.id}
         open={isUpdate?.dialogue}
@@ -125,12 +132,13 @@ const AllRollGrid = ({ data, mutate }: Props) => {
                         </IconButton>
                       </Tooltip>
                     </div>
-                    <div className="h-10 w-10 cursor-pointer hover:shadow-xl rounded-full bg-gradient-to-r from-yellow-600 to-yellow-400 flex justify-center items-center text-lg font-semibold">
-                      <Tooltip title="Access">
+                    <div className="h-10 w-10 cursor-pointer hover:shadow-xl rounded-full bg-gradient-to-r from-green-600 to-green-400 flex justify-center items-center text-lg font-semibold">
+                      <Tooltip title="Page Access">
                         <IconButton
-                        // onClick={() =>
-                        //   setIsInfo({ dialogue: true, role: item })
-                        // }
+                          onClick={() => {
+                            setAssetHistory(true);
+                            setAssetId(item?.id);
+                          }}
                         >
                           <DoorSliding className="!text-white" />
                         </IconButton>
