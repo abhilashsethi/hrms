e3729f6de4f71b1dba8836085706da51123d4dd6
KumarGudu <kg150201@gmail.com>
2023-06-14 13:12:01 +0530

    dashboard Loader addd

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,8 +1,12 @@
 import { AdminDashboard, WelcomeUser } from "components/admin";
+import { DashboardSkeletonLoading } from "components/admin/assets";
 import { MainEmployeeDashboardCards, MainEmployeeDashboardCharts } from "components/admin/employee";
+import { useFetch } from "hooks";
 import PanelLayout from "layouts/panel";
 
 const AdminHome = () => {
+  const { data: employeeDetails, isLoading } = useFetch<any>(`users/dashboard/details`);
+
   return (
     <PanelLayout title="Dashboard - Admin Panel">
       <>
@@ -10,8 +14,13 @@ const AdminHome = () => {
         <AdminDashboard /> */}
         <WelcomeUser title="Welcome Developer Name!" />
         <section className="lg:px-8 px-4 py-4">
-          <MainEmployeeDashboardCards data={""} />
-          <MainEmployeeDashboardCharts data={""} />
+          {isLoading ? <DashboardSkeletonLoading />
+            : (
+              <>
+                <MainEmployeeDashboardCards data={""} />
+                <MainEmployeeDashboardCharts data={""} />
+              </>
+            )}
         </section>
       </>
     </PanelLayout>
