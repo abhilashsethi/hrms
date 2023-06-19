3e8f8bafe22b80f1a0082f6b80f186fcb672f923
KumarGudu <kg150201@gmail.com>
2023-06-19 11:03:02 +0530

    manager

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,33 +1,33 @@
 import { AdminDashboard, WelcomeUser } from "components/admin";
 import { DashboardSkeletonLoading } from "components/admin/assets";
 import {
-	MainEmployeeDashboardCards,
-	MainEmployeeDashboardCharts,
-	MainHrDashboardCards,
-	MainHrDashboardCharts,
+  MainEmployeeDashboardCards,
+  MainEmployeeDashboardCharts,
+  MainHrDashboardCards,
+  MainHrDashboardCharts,
 } from "components/admin/dashboardComponent";
 import { useFetch } from "hooks";
 import PanelLayout from "layouts/panel";
 
 const AdminHome = () => {
-	const { data: employeeDetails, isLoading } = useFetch<any>(
-		`users/dashboard/details`
-	);
+  const { data: employeeDetails, isLoading } = useFetch<any>(
+    `users/dashboard/details`
+  );
 
-	return (
-		<PanelLayout title="Dashboard - Admin Panel">
-			<>
-				{isLoading ? (
-					<DashboardSkeletonLoading />
-				) : (
-					<>
-						<WelcomeUser title="Welcome Superadmin!" />
-						<AdminDashboard />
-					</>
-				)}
-				{/* Dashboard for developer or employee */}
+  return (
+    <PanelLayout title="Dashboard - Admin Panel">
+      <>
+        {isLoading ? (
+          <DashboardSkeletonLoading />
+        ) : (
+          <>
+            <WelcomeUser title="Welcome Superadmin!" />
+            <AdminDashboard />
+          </>
+        )}
+        {/* Dashboard for developer or employee */}
 
-				{/* <WelcomeUser title="Welcome Developer Name!" />
+        {/* <WelcomeUser title="Welcome Developer Name!" />
         <section className="lg:px-8 px-4 py-4">
           {isLoading ? <DashboardSkeletonLoading />
             : (
@@ -38,9 +38,9 @@ const AdminHome = () => {
             )}
         </section> */}
 
-				{/* Dashboard for HR */}
+        {/* Dashboard for HR */}
 
-				{/* <WelcomeUser title="Welcome Hr!" />
+        {/* <WelcomeUser title="Welcome Hr!" />
         <section className="lg:px-8 px-4 py-4">
           {isLoading ? <DashboardSkeletonLoading />
             : (
@@ -51,8 +51,8 @@ const AdminHome = () => {
             )}
         </section> */}
 
-				{/* Dashboard for Client */}
-				{/* <WelcomeUser title="Welcome Client Name!" />
+        {/* Dashboard for Manager */}
+        {/* <WelcomeUser title="Welcome Client Name!" />
         <section className="lg:px-8 px-4 py-4">
           {isLoading ? <DashboardSkeletonLoading />
             : (
@@ -62,9 +62,21 @@ const AdminHome = () => {
               </>
             )}
         </section> */}
-			</>
-		</PanelLayout>
-	);
+
+        {/* Dashboard for Client */}
+        {/* <WelcomeUser title="Welcome Client Name!" />
+        <section className="lg:px-8 px-4 py-4">
+          {isLoading ? <DashboardSkeletonLoading />
+            : (
+              <>
+                <MainHrDashboardCards data={""} />
+                <MainHrDashboardCharts data={""} />
+              </>
+            )}
+        </section> */}
+      </>
+    </PanelLayout>
+  );
 };
 
 export default AdminHome;
