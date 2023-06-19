fb9c2592667a1694908e6c256029edd2af823d2b
KumarGudu <kg150201@gmail.com>
2023-06-19 15:38:24 +0530

    different login

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -14,58 +14,83 @@ import { useAuth, useFetch } from "hooks";
 import PanelLayout from "layouts/panel";
 
 const AdminHome = () => {
+  const { user } = useAuth();
+  console.log({ user });
   const { data: employeeDetails, isLoading } = useFetch<any>(
     `users/dashboard/details`
   );
 
+
   return (
     <PanelLayout title="Dashboard - Admin Panel">
       <>
-        {isLoading ? (
-          <DashboardSkeletonLoading />
+        {user?.role?.name == "CEO" ? (
+          <>
+            {isLoading ? (
+              <DashboardSkeletonLoading />
+            ) : (
+              <>
+                <WelcomeUser title="Welcome Superadmin!" />
+                <AdminDashboard />
+              </>
+            )}
+          </>
+        ) : user?.role?.name == "DEVELOPER" ? (
+          <>
+            <WelcomeUser title="Welcome Developer Name!" />
+            <section className="lg:px-8 px-4 py-4">
+              {isLoading ? <DashboardSkeletonLoading />
+                : (
+                  <>
+                    <MainEmployeeDashboardCards data={""} />
+                    <MainEmployeeDashboardCharts data={""} />
+                  </>
+                )}
+            </section>
+          </>
+        ) : user?.role?.name == "HR" ? (
+          <>
+            <WelcomeUser title="Welcome Hr!" />
+            <section className="lg:px-8 px-4 py-4">
+              {isLoading ? <DashboardSkeletonLoading />
+                : (
+                  <>
+                    <MainHrDashboardCards data={""} />
+                    <MainHrDashboardCharts data={""} />
+                  </>
+                )}
+            </section>
+          </>
+        ) : user?.role?.name == "HR" ? (
+          <>
+            <WelcomeUser title="Welcome Manager Name!" />
+            <section className="lg:px-8 px-4 py-4">
+              {isLoading ? <DashboardSkeletonLoading />
+                : (
+                  <>
+                    <MainManagerDashboardCards data={""} />
+                    <MainManagerDashboardCharts data={""} />
+                  </>
+                )}
+            </section>
+          </>
         ) : (
           <>
-            <WelcomeUser title="Welcome Superadmin!" />
-            <AdminDashboard />
+            <WelcomeUser title="Welcome Client Name!" />
+            <section className="lg:px-8 px-4 py-4">
+              {isLoading ? (
+                <DashboardSkeletonLoading />
+              ) : (
+                <>
+                  <MainClientDashboardCards data={""} />
+                  <MainClientDashboardCharts data={""} />
+                </>
+              )}
+            </section>
           </>
+
         )}
-        {/* Dashboard for developer or employee */}
 
-        {/* <WelcomeUser title="Welcome Developer Name!" />
-        <section className="lg:px-8 px-4 py-4">
-          {isLoading ? <DashboardSkeletonLoading />
-            : (
-              <>
-                <MainEmployeeDashboardCards data={""} />
-                <MainEmployeeDashboardCharts data={""} />
-              </>
-            )}
-        </section> */}
-
-        {/* Dashboard for HR */}
-
-        {/* <WelcomeUser title="Welcome Hr!" />
-        <section className="lg:px-8 px-4 py-4">
-          {isLoading ? <DashboardSkeletonLoading />
-            : (
-              <>
-                <MainHrDashboardCards data={""} />
-                <MainHrDashboardCharts data={""} />
-              </>
-            )}
-        </section> */}
-
-        {/* Dashboard for Manager */}
-        {/* <WelcomeUser title="Welcome Manager Name!" />
-        <section className="lg:px-8 px-4 py-4">
-          {isLoading ? <DashboardSkeletonLoading />
-            : (
-              <>
-                <MainManagerDashboardCards data={""} />
-                <MainManagerDashboardCharts data={""} />
-              </>
-            )}
-        </section> */}
 
         {/* Dashboard for Client */}
         {/* <WelcomeUser title="Welcome Client Name!" />
