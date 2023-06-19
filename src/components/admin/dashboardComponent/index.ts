6b5cc89a91fa9f6958d086a744516583c8f41da6
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-06-19 15:13:23 +0530

    changes in index file

--- src/components/admin/dashboardComponent/index.ts
+++ src/components/admin/dashboardComponent/index.ts
@@ -3,10 +3,7 @@ export { default as MainEmployeeDashboardCards } from "./MainEmployeeDashboardCa
 export { default as MainHrDashboardCards } from "./MainHrDashboardCards";
 export { default as MainHrDashboardCharts } from "./MainHrDashboardCharts";
 export { default as AreaChart } from "./AreaChart";
-<<<<<<< HEAD
 export { default as MainClientDashboardCards } from "./MainClientDashboardCards";
 export { default as MainClientDashboardCharts } from "./MainClientDashboardCharts";
-=======
 export { default as MainManagerDashboardCharts } from "./MainManagerDashboardCharts";
 export { default as MainManagerDashboardCards } from "./MainManagerDashboardCards";
->>>>>>> 677ca2fd2c662b3803362dab3d01dd3fa13e099c
