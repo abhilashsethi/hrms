5a70fee5f93d459c70b7c3bae6822ca95284713e
yard-tech <searchingyardpc@gmail.com>
2023-06-20 11:40:38 +0530

    working on email

--- src/components/email/InboxHeader.tsx
+++ src/components/email/InboxHeader.tsx
@@ -3,26 +3,66 @@ import {
   ChevronLeft,
   ChevronRight,
   Delete,
+  MoreVert,
   Refresh,
+  Search,
 } from "@mui/icons-material";
-import { Checkbox, IconButton } from "@mui/material";
+import { Checkbox, IconButton, Menu, MenuItem, TextField } from "@mui/material";
+import { useState, MouseEvent } from "react";
 
 const InboxHeader = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className="flex flex-col md:flex-row gap-2 shadow-md rounded-lg justify-between p-4 bg-white py-4  w-full items-center ">
       <div className="flex gap-2 items-center">
-        <Checkbox size="small" /> <span className="text-gray-800/20  ">|</span>
+        <Checkbox size="small" /> <span className="text-gray-800/20">|</span>
         <IconButton>
           <Delete />
         </IconButton>
         <IconButton>
           <Refresh />
         </IconButton>
+        <IconButton onClick={handleClick}>
+          <MoreVert />
+        </IconButton>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <MenuItem onClick={handleClose}>Unread Email</MenuItem>
+          <MenuItem onClick={handleClose}>Read Email</MenuItem>
+          <MenuItem onClick={handleClose}>Reset</MenuItem>
+        </Menu>
         <span className="text-gray-800/20  ">|</span>
         <button className="flex ml-8 items-center gap-2 bg-blue-500 hover:bg-white border border-blue-500 text-white hover:text-blue-500 transition-all ease-in-out duration-300 justify-center px-4 py-2 rounded-md shadow-lg ">
           <Add />
           <p>Compose</p>
         </button>
+      </div>
+      <div className="w-full flex items-center justify-center">
+        <TextField
+          className="w-full"
+          id="outlined-basic"
+          variant="outlined"
+          size="small"
+          fullWidth
+          InputProps={{
+            startAdornment: <Search />,
+          }}
+        />
       </div>
       <div className="flex gap-2 items-center">
         <IconButton>
