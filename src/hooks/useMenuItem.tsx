bc9088a95649497633d56b09786957e6294024b9
KumarGudu <kg150201@gmail.com>
2023-06-22 18:34:17 +0530

    leave menu change

--- src/hooks/useMenuItem.tsx
+++ src/hooks/useMenuItem.tsx
@@ -235,7 +235,7 @@ export default () => {
         key: "9ss",
         title: "My Leaves",
         icon: <ICONS.Employee_Leaves />,
-        route: "/admin/leaves/all-leaves",
+        route: "/admin/leaves/leave-requests",
       },
     {
       key: "6",
