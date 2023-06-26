7e7b71dd20ee5a6316a981346f3b2f076aab949c
yard-tech <searchingyardpc@gmail.com>
2023-06-26 13:07:51 +0530

    chat all bug fixed

--- src/components/dialogues/ChatSendLink.tsx
+++ src/components/dialogues/ChatSendLink.tsx
@@ -24,6 +24,7 @@ interface Props {
 const ChatSendLink = ({ open, handleClose, sendId }: Props) => {
   const [loading, setLoading] = useState(false);
   const [isLink, setIsLink] = useState<string | null>(null);
+  const [isError, setIsError] = useState(false);
   const { change } = useChange();
 
   const {
@@ -35,57 +36,70 @@ const ChatSendLink = ({ open, handleClose, sendId }: Props) => {
   const { socketRef } = useSocket();
   const { user } = useAuth();
 
+  const handleSubmit = () => {
+    if (
+      isLink
+        ?.trim()
+        ?.match(
+          "(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})"
+        )
+    ) {
+      setIsError(false);
+      handleSend();
+      return;
+    }
+    setIsError(true);
+  };
+
   const handleSend = async () => {
-    if (isLink) {
-      try {
-        setLoading(true);
-        if (currentChatProfileDetails?.isNewChat) {
-          handleSendNewMessage({
-            messageTo: currentChatProfileDetails?.id,
+    if (isError || !isLink?.trim()) return;
+    try {
+      setLoading(true);
+      if (currentChatProfileDetails?.isNewChat) {
+        handleSendNewMessage({
+          messageTo: currentChatProfileDetails?.id,
+          message: isLink,
+          category: "link",
+        });
+        handleClose();
+        setLoading(false);
+        setIsLink(null);
+        socketRef?.emit("SENT_MESSAGE", {
+          groupId: currentChatProfileDetails?.id,
+          userId: user?.id,
+        });
+        revalidateCurrentChat(currentChatProfileDetails?.id);
+        return;
+      } else {
+        const res = await change(`chat/message/${sendId}`, {
+          body: {
             message: isLink,
             category: "link",
-          });
-          handleClose();
+          },
+        });
+        if (res?.status !== 200) {
+          Swal.fire(
+            "Error",
+            res?.results?.msg || "Something went wrong!",
+            "error"
+          );
           setLoading(false);
-          setIsLink(null);
-          socketRef?.emit("SENT_MESSAGE", {
-            groupId: currentChatProfileDetails?.id,
-            userId: user?.id,
-          });
-          revalidateCurrentChat(currentChatProfileDetails?.id);
-          return;
-        } else {
-          const res = await change(`chat/message/${sendId}`, {
-            body: {
-              message: isLink,
-              category: "link",
-            },
-          });
-          if (res?.status !== 200) {
-            Swal.fire(
-              "Error",
-              res?.results?.msg || "Something went wrong!",
-              "error"
-            );
-            setLoading(false);
-            return;
-          }
-          socketRef?.emit("SENT_MESSAGE", {
-            groupId: currentChatProfileDetails?.id,
-            userId: user?.id,
-          });
-          revalidateCurrentChat(currentChatProfileDetails?.id);
-          setLoading(false);
-          handleClose();
-          setIsLink(null);
           return;
         }
-      } catch (error) {
-        console.log(error);
+        socketRef?.emit("SENT_MESSAGE", {
+          groupId: currentChatProfileDetails?.id,
+          userId: user?.id,
+        });
+        revalidateCurrentChat(currentChatProfileDetails?.id);
         setLoading(false);
-      } finally {
-        setLoading(false);
+        handleClose();
+        setIsLink(null);
+        return;
       }
+    } catch (error) {
+      setLoading(false);
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -126,9 +140,11 @@ const ChatSendLink = ({ open, handleClose, sendId }: Props) => {
             placeholder="InsertLink"
             value={isLink ? isLink : ""}
             onChange={(e: any) => setIsLink(e.target.value)}
+            error={isError}
+            helperText={isError ? "Enter a valid link" : ""}
           />
           <Button
-            onClick={() => handleSend()}
+            onClick={() => handleSubmit()}
             variant="contained"
             className="!bg-theme"
             disabled={loading}
