03f54ec7b322f8469ec51dc141f0962e954d9ec8
KumarGudu <kg150201@gmail.com>
2023-06-26 17:59:21 +0530

    w change

--- src/components/dialogues/TenderCreateNote.tsx
+++ src/components/dialogues/TenderCreateNote.tsx
@@ -73,7 +73,7 @@ const TenderCreateNote = ({ open, handleClose, mutate, tenderData }: Props) => {
           </IconButton>
         </DialogTitle>
         <DialogContent className="app-scrollbar" sx={{ p: 2 }}>
-          <div className="w-1/2 md:px-4 px-2 tracking-wide">
+          <div className="w-[50rem] md:px-4 px-2 tracking-wide">
             <Formik
               initialValues={initialValues}
               validationSchema={validationSchema}
