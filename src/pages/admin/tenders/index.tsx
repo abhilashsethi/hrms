dce7bf989a9b0b1d4f77c01a8adafb05f30bb3f9
KumarGudu <kg150201@gmail.com>
2023-07-03 17:50:50 +0530

    loader

--- src/pages/admin/tenders/index.tsx
+++ src/pages/admin/tenders/index.tsx
@@ -4,6 +4,7 @@ import {
   TENDERICONS3,
 } from "assets/dashboard_Icons";
 import { CLOSE } from "assets/home";
+import { DashboardSkeletonLoading } from "components/admin/assets";
 import { CommonBarChart, CommonDonutChart } from "components/analytics";
 import { AdminBreadcrumbs, DashboardCard } from "components/core";
 import { useFetch } from "hooks";
@@ -23,28 +24,28 @@ const TenderDashboard = () => {
     () => [
       {
         id: 1,
-        count: tenderData?.tenderCount,
+        count: tenderData?.tenderCount || 0,
         title: "Total Tenders",
         bg: "from-blue-500 to-blue-300",
         img: TENDERICONS1.src,
       },
       {
         id: 2,
-        count: tenderData?.totalOpenTenderCount,
+        count: tenderData?.totalOpenTenderCount || 0,
         title: "Opened Tenders",
         bg: "from-green-500 to-green-300",
         img: TENDERICONS2.src,
       },
       {
         id: 4,
-        count: tenderData?.totalClosedTenderCount,
+        count: tenderData?.totalClosedTenderCount || 0,
         title: "Closed Tenders",
         bg: "from-green-500 to-green-300",
         img: CLOSE.src,
       },
       {
         id: 3,
-        count: tenderData?.totalSubmittedTenderCount,
+        count: tenderData?.totalSubmittedTenderCount || 0,
         title: "Submitted Tenders",
         bg: "from-yellow-500 to-yellow-300",
         img: TENDERICONS3.src,
@@ -54,49 +55,51 @@ const TenderDashboard = () => {
   );
   return (
     <PanelLayout title="Tenders Dashboard - Admin Panel">
-      <section className="px-8 py-4">
-        <AdminBreadcrumbs links={links} />
-        <div className="mt-4">
-          <DashboardCard data={cards} />
-        </div>
-        <div className="grid grid-cols-2 gap-8 mt-6">
-          <div className="p-6 rounded-md shadow-lg bg-white">
-            <h1 className="font-bold">Tender Ratio</h1>
-            <CommonDonutChart
-              labels={tenderData?.allTenderCountStatusWise?.length ?
-                tenderData?.allTenderCountStatusWise?.map((item: any) => item?.status)
-                : []}
-              text="Tender Ratio"
-              series={tenderData?.allTenderCountStatusWise?.length ?
-                tenderData?.allTenderCountStatusWise?.map((item: any) => item?._count)
-                : []}
-              type="pie"
-              colors={[
-                "#106EAD",
-                "#C33C5E",
-                "#25d366",
-                "#BD33B5",
-                "#E60023",
-                "#005d32",
-              ]}
-            />
+      {isLoading ? <DashboardSkeletonLoading /> :
+        <section className="px-8 py-4">
+          <AdminBreadcrumbs links={links} />
+          <div className="mt-4">
+            <DashboardCard data={cards} />
           </div>
-          <div className="p-6 rounded-md shadow-lg bg-white">
-            <h1 className="font-bold">Recent month status</h1>
-            <CommonBarChart
-              labels={tenderData?.allTenderCountMonthWiseOfCurrentYear?.length ?
-                tenderData?.allTenderCountMonthWiseOfCurrentYear?.map((item: any) => item?.monthAbbreviation)
-                : null}
-              data={tenderData?.allTenderCountMonthWiseOfCurrentYear?.length ?
-                tenderData?.allTenderCountMonthWiseOfCurrentYear?.map((item: any) => item?.count)
-                : null
-              }
-              type="bar"
-              text=""
-            />
+          <div className="grid grid-cols-2 gap-8 mt-6">
+            <div className="p-6 rounded-md shadow-lg bg-white">
+              <h1 className="font-bold">Tender Status Overview</h1>
+              <CommonDonutChart
+                labels={tenderData?.allTenderCountStatusWise?.length ?
+                  tenderData?.allTenderCountStatusWise?.map((item: any) => item?.status)
+                  : []}
+                text="Tender Ratio"
+                series={tenderData?.allTenderCountStatusWise?.length ?
+                  tenderData?.allTenderCountStatusWise?.map((item: any) => item?._count)
+                  : []}
+                type="pie"
+                colors={[
+                  "#106EAD",
+                  "#C33C5E",
+                  "#25d366",
+                  "#BD33B5",
+                  "#E60023",
+                  "#005d32",
+                ]}
+              />
+            </div>
+            <div className="p-6 rounded-md shadow-lg bg-white">
+              <h1 className="font-bold">This Year Tender Overview</h1>
+              <CommonBarChart
+                labels={tenderData?.allTenderCountMonthWiseOfCurrentYear?.length ?
+                  tenderData?.allTenderCountMonthWiseOfCurrentYear?.map((item: any) => item?.monthAbbreviation)
+                  : null}
+                data={tenderData?.allTenderCountMonthWiseOfCurrentYear?.length ?
+                  tenderData?.allTenderCountMonthWiseOfCurrentYear?.map((item: any) => item?.count)
+                  : null
+                }
+                type="bar"
+                text=""
+              />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      }
     </PanelLayout>
   );
 };
