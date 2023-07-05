9d78e83884f4bbc8fefb6385e4dd6c420b4c1605
KumarGudu <kg150201@gmail.com>
2023-07-05 11:56:10 +0530

    quatation

--- src/components/admin/quotation/QuotationData.tsx
+++ src/components/admin/quotation/QuotationData.tsx
@@ -1,216 +1,221 @@
 import { Delete, Edit } from "@mui/icons-material";
 import { IconButton, Tooltip } from "@mui/material";
 import {
-	EditAdditionalQuotationDetails,
-	EditQuotationDetails,
+  EditAdditionalQuotationDetails,
+  EditQuotationDetails,
 } from "components/dialogues";
 import EditTermsAndConditionDialogue from "components/dialogues/EditTermsAndConditionDialogue";
 import { TenderLayout } from "components/tender";
 import { useState } from "react";
+import { Quotation } from "types";
+interface Props {
+  quotationData?: Quotation;
+  isLoading?: any;
+  mutate?: any;
+}
+const QuotationData = ({ quotationData, isLoading, mutate }: Props) => {
+  const [editDetails, setEditDetails] = useState<boolean>(false);
+  const [additionDetails, setAdditionDetails] = useState<boolean>(false);
+  const [termsAndConditionDetails, setTermsAndConditionDetails] =
+    useState<boolean>(false);
+  console.log({ quotationData });
+  const basicDetails = [
+    {
+      id: 1,
+      title: "Client Name",
+      value: "Piyush Agrawal",
+    },
+    {
+      id: 2,
+      title: "Client Email",
+      value: "piyush@gmail.com",
+    },
+    {
+      id: 3,
+      title: "Client Address",
+      value:
+        "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016",
+    },
+    {
+      id: 4,
+      title: "Quotation Title",
+      value: "YardERP",
+    },
+    {
+      id: 5,
+      title: "Quotation Number",
+      value: "SY202306043QU",
+    },
+  ];
 
-const QuotationData = () => {
-	const [editDetails, setEditDetails] = useState<boolean>(false);
-	const [additionDetails, setAdditionDetails] = useState<boolean>(false);
-	const [termsAndConditionDetails, setTermsAndConditionDetails] =
-		useState<boolean>(false);
+  return (
+    <section>
+      <EditQuotationDetails
+        open={editDetails}
+        handleClose={() => setEditDetails(false)}
+      />
+      <EditAdditionalQuotationDetails
+        open={additionDetails}
+        handleClose={() => setAdditionDetails(false)}
+      />
+      <EditTermsAndConditionDialogue
+        open={termsAndConditionDetails}
+        handleClose={() => setTermsAndConditionDetails(false)}
+      />
 
-	const basicDetails = [
-		{
-			id: 1,
-			title: "Client Name",
-			value: "Piyush Agrawal",
-		},
-		{
-			id: 2,
-			title: "Client Email",
-			value: "piyush@gmail.com",
-		},
-		{
-			id: 3,
-			title: "Client Address",
-			value:
-				"Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016",
-		},
-		{
-			id: 4,
-			title: "Quotation Title",
-			value: "YardERP",
-		},
-		{
-			id: 5,
-			title: "Quotation Number",
-			value: "SY202306043QU",
-		},
-	];
+      <div className="mt-8">
+        <TenderLayout title="Basic Details">
+          <div className="flex justify-end absolute right-[10px] top-[10px]">
+            <Tooltip title="Edit">
+              <IconButton
+                size="small"
+                onClick={() => {
+                  setEditDetails(true);
+                }}
+              >
+                <Edit />
+              </IconButton>
+            </Tooltip>
+          </div>
+          <table className="w-full">
+            <tbody>
+              <tr>
+                <td className="w-1/5 text-sm font-semibold py-2">Status</td>
+                <td className="w-3/5">
+                  <span className="text-sm py-1 px-2 text-white tracking-wide shadow-md bg-green-500 rounded-md">
+                    {/* {tenderData?.status} */}Accepted
+                  </span>
+                </td>
+              </tr>
+              {basicDetails?.map((item) => (
+                <tr>
+                  <td className="w-1/5 text-sm font-semibold py-2">
+                    {item?.title}
+                  </td>
+                  <td className="w-3/5">
+                    <span className="text-sm text-gray-600 py-2">
+                      {item?.value}
+                    </span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TenderLayout>
+      </div>
+      <div className="mt-14">
+        <TenderLayout title="Additional Details">
+          <div>
+            <table className="w-full">
+              <tbody className="border-2">
+                <tr className="border-b-2">
+                  <th className="w-[10%] text-sm font-semibold py-2 border-r-2">
+                    S.No
+                  </th>
+                  <th className="w-[40%] text-sm border-r-2">Description</th>
+                  <th className="w-[30%] text-sm border-r-2">Qty</th>
+                  <th className="w-[30%] text-sm border-r-2">Cost</th>
+                  <th className="w-[30%] text-sm">Actions</th>
+                </tr>
 
-	return (
-		<section>
-			<EditQuotationDetails
-				open={editDetails}
-				handleClose={() => setEditDetails(false)}
-			/>
-			<EditAdditionalQuotationDetails
-				open={additionDetails}
-				handleClose={() => setAdditionDetails(false)}
-			/>
-			<EditTermsAndConditionDialogue
-				open={termsAndConditionDetails}
-				handleClose={() => setTermsAndConditionDetails(false)}
-			/>
-
-			<div className="mt-8">
-				<TenderLayout title="Basic Details">
-					<div className="flex justify-end absolute right-[10px] top-[10px]">
-						<Tooltip title="Edit">
-							<IconButton
-								size="small"
-								onClick={() => {
-									setEditDetails(true);
-								}}
-							>
-								<Edit />
-							</IconButton>
-						</Tooltip>
-					</div>
-					<table className="w-full">
-						<tbody>
-							<tr>
-								<td className="w-1/5 text-sm font-semibold py-2">Status</td>
-								<td className="w-3/5">
-									<span className="text-sm py-1 px-2 text-white tracking-wide shadow-md bg-green-500 rounded-md">
-										{/* {tenderData?.status} */}Accepted
-									</span>
-								</td>
-							</tr>
-							{basicDetails?.map((item) => (
-								<tr>
-									<td className="w-1/5 text-sm font-semibold py-2">
-										{item?.title}
-									</td>
-									<td className="w-3/5">
-										<span className="text-sm text-gray-600 py-2">
-											{item?.value}
-										</span>
-									</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				</TenderLayout>
-			</div>
-			<div className="mt-14">
-				<TenderLayout title="Additional Details">
-					<div>
-						<table className="w-full">
-							<tbody className="border-2">
-								<tr className="border-b-2">
-									<th className="w-[10%] text-sm font-semibold py-2 border-r-2">
-										S.No
-									</th>
-									<th className="w-[40%] text-sm border-r-2">Description</th>
-									<th className="w-[30%] text-sm border-r-2">Qty</th>
-									<th className="w-[30%] text-sm border-r-2">Cost</th>
-									<th className="w-[30%] text-sm">Actions</th>
-								</tr>
-
-								<>
-									<tr className="border-b-2">
-										<td
-											align="center"
-											className="w-[10%] text-sm py-2 border-r-2"
-										>
-											1
-										</td>
-										<td align="center" className="w-[40%] text-sm border-r-2">
-											{/* {item?.title} */}Android App & Admin Panel Development
-										</td>
-										<td align="center" className="w-[30%] text-sm border-r-2">
-											<div className="flex gap-2 items-center justify-center">
-												<p className="text-xs">1</p>
-											</div>
-										</td>
-										<td align="center" className="w-[30%] text-sm border-r-2">
-											<div className="flex gap-2 items-center justify-center">
-												<p className="text-xs">1,20,000</p>
-											</div>
-										</td>
-										<td align="center" className="w-[20%] text-sm">
-											<div className="flex gap-1 py-2 justify-center">
-												<Tooltip title="Edit Document">
-													<IconButton
-														size="small"
-														onClick={() => {
-															setAdditionDetails(true);
-														}}
-													>
-														<Edit />
-													</IconButton>
-												</Tooltip>
-												<Tooltip title="Delete Document">
-													<IconButton size="small">
-														<Delete
-														// onClick={() => handleDelete(item)}
-														/>
-													</IconButton>
-												</Tooltip>
-											</div>
-										</td>
-									</tr>
-								</>
-							</tbody>
-						</table>
-					</div>
-				</TenderLayout>
-			</div>
-			<div className="mt-14">
-				<TenderLayout title="Terms & Condition">
-					<div className="">
-						<div className="flex justify-end ">
-							<Tooltip title="Edit">
-								<IconButton
-									size="small"
-									onClick={() => {
-										setTermsAndConditionDetails(true);
-									}}
-								>
-									<Edit />
-								</IconButton>
-							</Tooltip>
-						</div>
-						<p className="text-gray-500 text-justify text-sm">
-							Welcome to SearchingYard! These terms and conditions outline the
-							rules and regulations for the use of SearchingYard's Website,
-							located at https://searchingyard.com/. By accessing this website
-							we assume you accept these terms and conditions. Do not continue
-							to use SearchingYard if you do not agree to take all of the terms
-							and conditions stated on this page. The following terminology
-							applies to these Terms and Conditions, Privacy Statement and
-							Disclaimer Notice and all Agreements: "Client", "You" and "Your"
-							refers to you, the person log on this website and compliant to the
-							Company’s terms and conditions. "The Company", "Ourselves", "We",
-							"Our" and "Us", refers to our Company. "Party", "Parties", or
-							"Us", refers to both the Client and ourselves. All terms refer to
-							the offer, acceptance and consideration of payment necessary to
-							undertake the process of our assistance to the Client in the most
-							appropriate manner for the express purpose of meeting the Client’s
-							needs in respect of provision of the Company’s stated services, in
-							accordance with and subject to, prevailing law of Netherlands. Any
-							use of the above terminology or other words in the singular,
-							plural, capitalization and/or he/she or they, are taken as
-							interchangeable and therefore as referring to same. Cookies We
-							employ the use of cookies. By accessing SearchingYard, you agreed
-							to use cookies in agreement with the SearchingYard's Privacy
-							Policy. Most interactive websites use cookies to let us retrieve
-							the user’s details for each visit. Cookies are used by our website
-							to enable the functionality of certain areas to make it easier for
-							people visiting our website. Some of our affiliate/advertising
-							partners may also use cookies.
-						</p>
-					</div>
-				</TenderLayout>
-			</div>
-		</section>
-	);
+                <>
+                  <tr className="border-b-2">
+                    <td
+                      align="center"
+                      className="w-[10%] text-sm py-2 border-r-2"
+                    >
+                      1
+                    </td>
+                    <td align="center" className="w-[40%] text-sm border-r-2">
+                      {/* {item?.title} */}Android App & Admin Panel Development
+                    </td>
+                    <td align="center" className="w-[30%] text-sm border-r-2">
+                      <div className="flex gap-2 items-center justify-center">
+                        <p className="text-xs">1</p>
+                      </div>
+                    </td>
+                    <td align="center" className="w-[30%] text-sm border-r-2">
+                      <div className="flex gap-2 items-center justify-center">
+                        <p className="text-xs">1,20,000</p>
+                      </div>
+                    </td>
+                    <td align="center" className="w-[20%] text-sm">
+                      <div className="flex gap-1 py-2 justify-center">
+                        <Tooltip title="Edit Document">
+                          <IconButton
+                            size="small"
+                            onClick={() => {
+                              setAdditionDetails(true);
+                            }}
+                          >
+                            <Edit />
+                          </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Delete Document">
+                          <IconButton size="small">
+                            <Delete
+                            // onClick={() => handleDelete(item)}
+                            />
+                          </IconButton>
+                        </Tooltip>
+                      </div>
+                    </td>
+                  </tr>
+                </>
+              </tbody>
+            </table>
+          </div>
+        </TenderLayout>
+      </div>
+      <div className="mt-14">
+        <TenderLayout title="Terms & Condition">
+          <div className="">
+            <div className="flex justify-end ">
+              <Tooltip title="Edit">
+                <IconButton
+                  size="small"
+                  onClick={() => {
+                    setTermsAndConditionDetails(true);
+                  }}
+                >
+                  <Edit />
+                </IconButton>
+              </Tooltip>
+            </div>
+            <p className="text-gray-500 text-justify text-sm">
+              Welcome to SearchingYard! These terms and conditions outline the
+              rules and regulations for the use of SearchingYard's Website,
+              located at https://searchingyard.com/. By accessing this website
+              we assume you accept these terms and conditions. Do not continue
+              to use SearchingYard if you do not agree to take all of the terms
+              and conditions stated on this page. The following terminology
+              applies to these Terms and Conditions, Privacy Statement and
+              Disclaimer Notice and all Agreements: "Client", "You" and "Your"
+              refers to you, the person log on this website and compliant to the
+              Company’s terms and conditions. "The Company", "Ourselves", "We",
+              "Our" and "Us", refers to our Company. "Party", "Parties", or
+              "Us", refers to both the Client and ourselves. All terms refer to
+              the offer, acceptance and consideration of payment necessary to
+              undertake the process of our assistance to the Client in the most
+              appropriate manner for the express purpose of meeting the Client’s
+              needs in respect of provision of the Company’s stated services, in
+              accordance with and subject to, prevailing law of Netherlands. Any
+              use of the above terminology or other words in the singular,
+              plural, capitalization and/or he/she or they, are taken as
+              interchangeable and therefore as referring to same. Cookies We
+              employ the use of cookies. By accessing SearchingYard, you agreed
+              to use cookies in agreement with the SearchingYard's Privacy
+              Policy. Most interactive websites use cookies to let us retrieve
+              the user’s details for each visit. Cookies are used by our website
+              to enable the functionality of certain areas to make it easier for
+              people visiting our website. Some of our affiliate/advertising
+              partners may also use cookies.
+            </p>
+          </div>
+        </TenderLayout>
+      </div>
+    </section>
+  );
 };
 
 export default QuotationData;
--- src/pages/admin/quotation/quotation-details.tsx
+++ src/pages/admin/quotation/quotation-details.tsx
@@ -1,34 +1,40 @@
-import { MeetingData } from "components/admin/meetings";
 import { QuotationData } from "components/admin/quotation";
 import { AdminBreadcrumbs } from "components/core";
+import { useFetch } from "hooks";
 import PanelLayout from "layouts/panel";
+import { useRouter } from "next/router";
+import { Quotation } from "types";
 
 const QuotationDetails = () => {
-	return (
-		<PanelLayout title="Quotation Details - Admin Panel">
-			<section className="px-8 mx-auto p-4">
-				<div className="pb-4">
-					<AdminBreadcrumbs links={links} />
-				</div>
-				<QuotationData />
-			</section>
-		</PanelLayout>
-	);
+  const router = useRouter();
+  const {
+    data: quotationData,
+    mutate,
+    isLoading,
+  } = useFetch<Quotation>(
+    `quotations/${router?.query?.id}`
+  );
+  return (
+    <PanelLayout title="Quotation Details - Admin Panel">
+      <section className="px-8 mx-auto p-4">
+        <div className="pb-4">
+          <AdminBreadcrumbs links={links} />
+        </div>
+        <QuotationData quotationData={quotationData} mutate={mutate}
+          isLoading={isLoading} />
+      </section>
+    </PanelLayout>
+  );
 };
 
 export default QuotationDetails;
 
 const links = [
-	{ id: 1, page: "Quotation", link: "/admin/quotation" },
-	{
-		id: 2,
-		page: "Quotation Details",
-		link: "/admin/meetings/quotation-details",
-	},
+  { id: 1, page: "Quotation", link: "/admin/quotation" },
+  {
+    id: 2,
+    page: "Quotation Details",
+    link: "/admin/meetings/quotation-details",
+  },
 ];
 
-// const attendance = [
-//   { title: "PRESENT", date: "2023-04-03" },
-//   { title: "ABSENT", date: "2023-04-04" },
-//   { title: "PRESENT", date: "2023-04-05" },
-// ];
