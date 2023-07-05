b89641d44e62f01411277f854264f377aa20cca8
KumarGudu <kg150201@gmail.com>
2023-07-05 15:07:32 +0530

    bug fix

--- src/components/admin/quotation/QuotationData.tsx
+++ src/components/admin/quotation/QuotationData.tsx
@@ -194,33 +194,9 @@ const QuotationData = ({ quotationData, mutate, isLoading }: Props) => {
               </Tooltip>
             </div>
             <p className="text-gray-500 text-justify text-sm">
-              Welcome to SearchingYard! These terms and conditions outline the
-              rules and regulations for the use of SearchingYard's Website,
-              located at https://searchingyard.com/. By accessing this website
-              we assume you accept these terms and conditions. Do not continue
-              to use SearchingYard if you do not agree to take all of the terms
-              and conditions stated on this page. The following terminology
-              applies to these Terms and Conditions, Privacy Statement and
-              Disclaimer Notice and all Agreements: "Client", "You" and "Your"
-              refers to you, the person log on this website and compliant to the
-              Company’s terms and conditions. "The Company", "Ourselves", "We",
-              "Our" and "Us", refers to our Company. "Party", "Parties", or
-              "Us", refers to both the Client and ourselves. All terms refer to
-              the offer, acceptance and consideration of payment necessary to
-              undertake the process of our assistance to the Client in the most
-              appropriate manner for the express purpose of meeting the Client’s
-              needs in respect of provision of the Company’s stated services, in
-              accordance with and subject to, prevailing law of Netherlands. Any
-              use of the above terminology or other words in the singular,
-              plural, capitalization and/or he/she or they, are taken as
-              interchangeable and therefore as referring to same. Cookies We
-              employ the use of cookies. By accessing SearchingYard, you agreed
-              to use cookies in agreement with the SearchingYard's Privacy
-              Policy. Most interactive websites use cookies to let us retrieve
-              the user’s details for each visit. Cookies are used by our website
-              to enable the functionality of certain areas to make it easier for
-              people visiting our website. Some of our affiliate/advertising
-              partners may also use cookies.
+              {quotationData?.termsAndConditions ?
+                <div dangerouslySetInnerHTML={{ __html: `${quotationData?.termsAndConditions}` }}></div>
+                : "No Terms And Conditions"}
             </p>
           </div>
         </TenderLayout>
