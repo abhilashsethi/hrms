7a34bb29ca320edff615465df48b7e2f8ea5cd3a
KumarGudu <kg150201@gmail.com>
2023-07-12 12:03:41 +0530

    bug fix

--- src/hooks/useAPI.ts
+++ src/hooks/useAPI.ts
@@ -11,8 +11,8 @@ export const getAccessToken = () => {
 };
 
 // export const BASE_URL: "https://hrms.yardiot.com/api/v1" = `https://hrms.yardiot.com/api/v1`;
-// export const BASE_URL: "https://sy-hrms-api.onrender.com/api/v1" = `https://sy-hrms-api.onrender.com/api/v1`;
-export const BASE_URL: "http://192.168.29.25:8080/api/v1" = `http://192.168.29.25:8080/api/v1`;
+export const BASE_URL: "https://sy-hrms-api.onrender.com/api/v1" = `https://sy-hrms-api.onrender.com/api/v1`;
+// export const BASE_URL: "http://192.168.29.25:8080/api/v1" = `http://192.168.29.25:8080/api/v1`;
 
 type useFetchOptions = {
 	BASE_URL: typeof BASE_URL | "/api";
