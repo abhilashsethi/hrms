eaa9be26fe5cb06be96cc1617bfb5e2d7e2b22de
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-07-17 18:52:17 +0530

    sales chart and card integration onGoing

--- src/components/admin/dashboardComponent/SalesDashboardCharts.tsx
+++ src/components/admin/dashboardComponent/SalesDashboardCharts.tsx
@@ -1,55 +1,69 @@
 import { GuestBarChart, GuestDonutChart } from "components/analytics";
+import { useAuth, useFetch } from "hooks";
 interface Props {
-  data?: any;
+	data?: any;
 }
 const SalesDashboardCharts = ({ data }: Props) => {
-  const getMonthName = (monthNumber: any) => {
-    const date = new Date();
-    date.setMonth(monthNumber - 1);
-    return date.toLocaleString("default", { month: "long" });
-  };
+	const { user } = useAuth();
+	const getMonthName = (monthNumber: any) => {
+		const date = new Date();
+		date.setMonth(monthNumber - 1);
+		return date.toLocaleString("default", { month: "long" });
+	};
+	const { data: dashboardData } = useFetch<any>(
+		`dashboards/sales/dashboard?userId=${user?.id}`
+	);
 
-  return (
-    <div className="w-full">
-      <div className="grid lg:grid-cols-2 content-between gap-6">
-        <div className="px-2 py-4 w-full bg-white flex flex-col justify-center gap-2 !border-gray-500 rounded-xl !shadow-xl">
-          <p className="font-bold text-lg text-center">
-            This Year Attendance Overview
-          </p>
-          <GuestBarChart
-            labels={[
-              "Jan",
-              "Feb",
-              "Mar",
-              "Apr",
-              "May",
-              "Jun",
-              "Jul",
-              "Aug",
-              "Sep",
-              "Oct",
-              "Nov",
-              "Dec",
-            ]}
-            data={[15, 10, 11, 10, 9, 12, 12, 26, 12, 10, 18, 19]}
-            type="bar"
-            text=""
-          />
-        </div>
+	return (
+		<div className="w-full">
+			<div className="grid lg:grid-cols-2 content-between gap-6">
+				<div className="px-2 py-4 w-full bg-white flex flex-col justify-center gap-2 !border-gray-500 rounded-xl !shadow-xl">
+					<p className="font-bold text-lg text-center">
+						This Year Attendance Overview
+					</p>
+					<GuestBarChart
+						labels={
+							dashboardData?.currentYearAttendance?.length
+								? dashboardData?.currentYearAttendance?.map(
+										(item: any) => item?.month
+								  )
+								: []
+						}
+						data={
+							dashboardData?.currentYearAttendance?.length
+								? dashboardData?.currentYearAttendance?.map(
+										(item: any) => item?.count
+								  )
+								: []
+						}
+						type="bar"
+						text=""
+					/>
+				</div>
 
-        <div className="w-full px-2 py-4 flex flex-col bg-white justify-center !border-gray-500 rounded-xl !shadow-xl">
-          <p className="text-lg font-bold text-center">Meetings Overview</p>
-          <GuestDonutChart
-            labels={["PENDING", "COMPLETED"]}
-            series={[80, 20]}
-            text=""
-            type="donut"
-            colors={["#005d32", "#BD33B5", "#E60023"]}
-          />
-        </div>
-      </div>
-    </div>
-  );
+				<div className="w-full px-2 py-4 flex flex-col bg-white justify-center !border-gray-500 rounded-xl !shadow-xl">
+					<p className="text-lg font-bold text-center">Meetings Overview</p>
+					<GuestDonutChart
+						labels={
+							dashboardData?.meetingCounts?.length
+								? dashboardData?.meetingCounts?.map((item: any) => item?.status)
+								: []
+						}
+						series={
+							dashboardData?.meetingCounts?.length
+								? dashboardData?.meetingCounts?.map(
+										(item: any) => item?._count?.status
+								  )
+								: []
+						}
+						text=""
+						type="donut"
+						colors={["#cddc39", "#448aff"]}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default SalesDashboardCharts;
