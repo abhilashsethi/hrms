9a117ccc2b4941d34ea59ce4d4a9e0dacbe1de19
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-07-18 11:45:45 +0530

    env file created

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,33 +1,34 @@
 import { ClientToServerEvents, ServerToClientEvents } from "types";
 import { create } from "zustand";
 import { Socket, io } from "socket.io-client";
+import { Public_SOCKET_URL } from "config/env.config";
 
 // const SOCKET_URL = `wss://hrms.yardiot.com`;
-const SOCKET_URL = `wss://sy-hrms-api.onrender.com`;
+const SOCKET_URL = Public_SOCKET_URL;
 // const SOCKET_URL = `ws://localhost:8080`;
 
 type socketState = {
-  socketRef?: Socket<ServerToClientEvents, ClientToServerEvents>;
-  setSocketRef: (arg: Socket) => void;
-  connect: () => void;
-  disconnect: () => void;
+	socketRef?: Socket<ServerToClientEvents, ClientToServerEvents>;
+	setSocketRef: (arg: Socket) => void;
+	connect: () => void;
+	disconnect: () => void;
 };
 const useSocket = create<socketState>((set, get) => ({
-  setSocketRef: (socket) => {
-    set({
-      socketRef: socket,
-    });
-  },
-  connect: () => {
-    let socket = io(SOCKET_URL);
-    set({
-      socketRef: socket,
-    });
-  },
-  disconnect: () => {
-    let socket = get().socketRef;
-    socket?.disconnect();
-  },
+	setSocketRef: (socket) => {
+		set({
+			socketRef: socket,
+		});
+	},
+	connect: () => {
+		let socket = io(SOCKET_URL);
+		set({
+			socketRef: socket,
+		});
+	},
+	disconnect: () => {
+		let socket = get().socketRef;
+		socket?.disconnect();
+	},
 }));
 
 export default useSocket;
