56d978b5ff523f64db085cce5a02d22857c200f3
KumarGudu <kg150201@gmail.com>
2023-08-18 16:32:20 +0530

    chart bug fix

--- src/components/admin/attendance/AttendanceCharts.tsx
+++ src/components/admin/attendance/AttendanceCharts.tsx
@@ -15,7 +15,7 @@ const AttendanceCharts = ({
   return (
     <>
       <div className="grid grid-cols-12 py-2 content-between gap-6 !mb-6">
-        <div className="col-span-12 bg-white w-full  gap-5 md:col-span-12 lg:col-span-12 !border-grey-500 rounded-xl !shadow-xl">
+        <div className="col-span-12 bg-white pt-2 w-full  gap-5 md:col-span-12 lg:col-span-7 !border-grey-500 rounded-xl !shadow-xl">
           <div className="font-semibold flex justify-center py-4 pl-2 ">
             <p>Last Week Employee's Report </p>
           </div>
