19d1d4897a3a433bfe1ad391416673edff961e4e
KumarGudu <kg150201@gmail.com>
2023-08-22 11:28:05 +0530

    create mail bug fix

--- src/components/chat/ChatHead.tsx
+++ src/components/chat/ChatHead.tsx
@@ -184,6 +184,7 @@ const ChatHead = ({ isNew }: { isNew?: boolean }) => {
       }
     } catch (error) {}
   };
+  console.log({ currentChatProfileDetails });
   return (
     <>
       <ChatProfileDrawer
