7d3c1f22db44ddea58907f43f13f96ed20814369
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-08-24 13:36:02 +0530

    minor changes

--- src/pages/admin/meetings/meeting-details.tsx
+++ src/pages/admin/meetings/meeting-details.tsx
@@ -4,23 +4,23 @@ import { useAuth } from "hooks";
 import PanelLayout from "layouts/panel";
 
 const MeetingDetails = () => {
-  const { user } = useAuth();
-  const links = [
-    user?.role?.name === "CEO"
-      ? { id: 1, page: "Meetings", link: "/admin/meetings" }
-      : { id: 1, page: "My Meetings", link: "/admin/meetings/my-meetings" },
-  ];
+	const { user } = useAuth();
+	const links = [
+		user?.role?.name === "CEO"
+			? { id: 1, page: "Meetings", link: "/admin/meetings" }
+			: { id: 1, page: "My Meetings", link: "/admin/meetings/my-meetings" },
+	];
 
-  return (
-    <PanelLayout title="User Profile - Admin Panel">
-      <section className="px-8 mx-auto p-4">
-        <div className="pb-4">
-          <AdminBreadcrumbs links={links} />
-        </div>
-        <MeetingData />
-      </section>
-    </PanelLayout>
-  );
+	return (
+		<PanelLayout title="Meeting Details - Admin Panel">
+			<section className="px-8 mx-auto p-4">
+				<div className="pb-4">
+					<AdminBreadcrumbs links={links} />
+				</div>
+				<MeetingData />
+			</section>
+		</PanelLayout>
+	);
 };
 
 export default MeetingDetails;
