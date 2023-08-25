243f5449cd5ac1cdfced9ae049d4cdfc2441750f
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-08-25 12:31:28 +0530

    role module bug fixed

--- src/components/admin/roles/AllRollColumn.tsx
+++ src/components/admin/roles/AllRollColumn.tsx
@@ -9,148 +9,160 @@ import Swal from "sweetalert2";
 import { Role } from "types";
 import { MuiTblOptions, clock, getDataWithSL } from "utils";
 interface Props {
-  data?: Role[];
-  mutate?: any;
+	data?: Role[];
+	mutate?: any;
 }
 const AllRollColumn = ({ data, mutate }: Props) => {
-  const [isInfo, setIsInfo] = useState<{ dialogue?: boolean; role?: any }>({
-    dialogue: false,
-    role: null,
-  });
-  const [assetHistory, setAssetHistory] = useState(false);
-  const [roleId, setRileId] = useState("");
-  const { change } = useChange();
-  const [loading, setLoading] = useState(false);
-  return (
-    <section className="mt-8">
-      <RoleWisePageAccess
-        open={assetHistory}
-        onClose={() => setAssetHistory(false)}
-        roleId={roleId}
-      />
-      <RoleInformation
-        open={isInfo?.dialogue}
-        onClose={() => setIsInfo({ dialogue: false })}
-        roleId={isInfo?.role?.id}
-      />
-      <MaterialTable
-        title={<HeadStyle name="All Roles" icon={<PeopleRounded />} />}
-        isLoading={!data}
-        data={data ? getDataWithSL<any>(data) : []}
-        options={{ ...MuiTblOptions(), selection: false, paging: false }}
-        columns={[
-          {
-            title: "#",
-            field: "sl",
-            editable: "never",
-            width: "2%",
-          },
-          {
-            title: "Role",
-            tooltip: "Role",
-            field: "name",
-          },
-          {
-            title: "Total Members",
-            tooltip: "Total Members",
-            // field: "data._count?.users",
-            render: (data) => {
-              return <div className="">{data?._count?.users}</div>;
-            },
-          },
-          {
-            title: "Details",
-            field: "name",
-            export: false,
-            render: (data) => {
-              return (
-                <>
-                  <div className="flex gap-2">
-                    <Tooltip title="Details">
-                      <div className="text-sm bg-gradient-to-r from-blue-500 to-blue-400 h-8 w-8 rounded-md flex justify-center items-center cursor-pointer">
-                        <IconButton
-                          onClick={() => setIsInfo({ dialogue: true, role: data })}
-                        >
-                          <Info className="!text-white" />
-                        </IconButton>
-                      </div>
-                    </Tooltip>
-                    <Tooltip title="Access">
-                      <div className="text-sm bg-gradient-to-r from-green-500 to-green-400 h-8 w-8 rounded-md flex justify-center items-center cursor-pointer">
-                        <IconButton
-                          onClick={() => {
-                            setAssetHistory(true);
-                            setRileId(data?.id);
-                          }}
-                        >
-                          <DoorSliding className="!text-white" />
-                        </IconButton>
-                      </div>
-                    </Tooltip>
-                  </div>
-                </>
-              );
-            },
-            editable: "never",
-          },
-          {
-            title: "Last Updated",
-            field: "updatedAt",
-            render: (data) => clock(data.updatedAt).fromNow(),
-            editable: "never",
-          },
-          {
-            title: "Created",
-            field: "createdAt",
-            render: (data) => new Date(data.createdAt).toDateString(),
-            editable: "never",
-          },
-        ]}
-        editable={{
-          onRowDelete: async (oldData) => {
-            setLoading(true);
-            Swal.fire("", "Please Wait...", "info");
-            try {
-              const res = await change(`roles/${oldData.id}`, {
-                method: "DELETE",
-              });
-              setLoading(false);
-              if (res?.status !== 200) {
-                Swal.fire(
-                  "Error",
-                  res?.results?.msg || "Something went wrong!",
-                  "error"
-                );
-                setLoading(false);
-                return;
-              }
-              mutate();
-              Swal.fire(`Success`, `Deleted Successfully!`, `success`);
-              return;
-            } catch (error) {
-              console.log(error);
-              setLoading(false);
-            } finally {
-              setLoading(false);
-            }
-          },
-          onRowUpdate: async (newData) => {
-            const res = await change(`roles/${newData?.id}`, {
-              method: "PATCH",
-              body: { name: newData?.name },
-            });
-            mutate();
-            if (res?.status !== 200) {
-              Swal.fire(`Error`, "Something went wrong!", "error");
-              return;
-            }
-            Swal.fire(`Success`, "Updated Successfully!", "success");
-            return;
-          },
-        }}
-      />
-    </section>
-  );
+	const [isInfo, setIsInfo] = useState<{ dialogue?: boolean; role?: any }>({
+		dialogue: false,
+		role: null,
+	});
+	const [assetHistory, setAssetHistory] = useState(false);
+	const [roleId, setRileId] = useState("");
+	const { change } = useChange();
+	const [loading, setLoading] = useState(false);
+	return (
+		<section className="mt-8">
+			<RoleWisePageAccess
+				open={assetHistory}
+				onClose={() => setAssetHistory(false)}
+				roleId={roleId}
+			/>
+			<RoleInformation
+				open={isInfo?.dialogue}
+				onClose={() => setIsInfo({ dialogue: false })}
+				roleId={isInfo?.role?.id}
+			/>
+			<MaterialTable
+				title={<HeadStyle name="All Roles" icon={<PeopleRounded />} />}
+				isLoading={!data}
+				data={
+					data
+						? data?.map((_, i) => ({
+								..._,
+								sl: i + 1,
+								totalMembers: _?._count?.users,
+								updatedAt: clock(_.updatedAt).fromNow(),
+								createdAt: new Date(_.createdAt).toDateString(),
+						  }))
+						: []
+				}
+				options={{ ...MuiTblOptions(), selection: false, paging: false }}
+				columns={[
+					{
+						title: "#",
+						field: "sl",
+						editable: "never",
+						width: "2%",
+					},
+					{
+						title: "Role",
+						tooltip: "Role",
+						field: "name",
+					},
+					{
+						title: "Total Members",
+						tooltip: "Total Members",
+						field: "totalMembers",
+						render: (data) => {
+							return <div className="">{data?._count?.users}</div>;
+						},
+					},
+					{
+						title: "Details",
+						field: "name",
+						export: false,
+						render: (data) => {
+							return (
+								<>
+									<div className="flex gap-2">
+										<Tooltip title="Details">
+											<div className="text-sm bg-gradient-to-r from-blue-500 to-blue-400 h-8 w-8 rounded-md flex justify-center items-center cursor-pointer">
+												<IconButton
+													onClick={() =>
+														setIsInfo({ dialogue: true, role: data })
+													}
+												>
+													<Info className="!text-white" />
+												</IconButton>
+											</div>
+										</Tooltip>
+										<Tooltip title="Access">
+											<div className="text-sm bg-gradient-to-r from-green-500 to-green-400 h-8 w-8 rounded-md flex justify-center items-center cursor-pointer">
+												<IconButton
+													onClick={() => {
+														setAssetHistory(true);
+														setRileId(data?.id);
+													}}
+												>
+													<DoorSliding className="!text-white" />
+												</IconButton>
+											</div>
+										</Tooltip>
+									</div>
+								</>
+							);
+						},
+						editable: "never",
+					},
+					{
+						title: "Last Updated",
+						field: "updatedAt",
+						// render: (data) => clock(data.updatedAt).fromNow(),
+						editable: "never",
+					},
+					{
+						title: "Created",
+						field: "createdAt",
+						// render: (data) => new Date(data.createdAt).toDateString(),
+						editable: "never",
+					},
+				]}
+				editable={{
+					onRowDelete: async (oldData) => {
+						setLoading(true);
+						Swal.fire("", "Please Wait...", "info");
+						try {
+							const res = await change(`roles/${oldData.id}`, {
+								method: "DELETE",
+							});
+							setLoading(false);
+							if (res?.status !== 200) {
+								Swal.fire(
+									"Error",
+									res?.results?.msg || "Something went wrong!",
+									"error"
+								);
+								setLoading(false);
+								return;
+							}
+							mutate();
+							Swal.fire(`Success`, `Deleted Successfully!`, `success`);
+							return;
+						} catch (error) {
+							console.log(error);
+							setLoading(false);
+						} finally {
+							setLoading(false);
+						}
+					},
+					onRowUpdate: async (newData) => {
+						const res = await change(`roles/${newData?.id}`, {
+							method: "PATCH",
+							body: { name: newData?.name },
+						});
+						mutate();
+						if (res?.status !== 200) {
+							Swal.fire(`Error`, "Something went wrong!", "error");
+							return;
+						}
+						Swal.fire(`Success`, "Updated Successfully!", "success");
+						return;
+					},
+				}}
+			/>
+		</section>
+	);
 };
 
 export default AllRollColumn;
--- src/pages/admin/roles/all-roles.tsx
+++ src/pages/admin/roles/all-roles.tsx
@@ -1,192 +1,191 @@
 import {
-  Add,
-  Close,
-  FilterListRounded,
-  GridViewRounded,
-  TableRowsRounded,
+	Add,
+	Close,
+	FilterListRounded,
+	GridViewRounded,
+	TableRowsRounded,
 } from "@mui/icons-material";
 import {
-  Button,
-  IconButton,
-  MenuItem,
-  Pagination,
-  Stack,
-  TextField,
-  Tooltip,
+	Button,
+	IconButton,
+	MenuItem,
+	Pagination,
+	Stack,
+	TextField,
+	Tooltip,
 } from "@mui/material";
+import { AllRollColumn, AllRollGrid } from "components/admin/roles";
 import { AdminBreadcrumbs, Loader, LoaderAnime } from "components/core";
 import { CreateRole } from "components/dialogues";
 import { useFetch } from "hooks";
 import PanelLayout from "layouts/panel";
 import { useState } from "react";
-import { AllRollGrid, AllRollColumn } from "components/admin/roles";
-import { Role } from "types";
 const AllRoles = () => {
-  const [pageNumber, setPageNumber] = useState<number>(1);
-  const [isGrid, setIsGrid] = useState(true);
-  const [userName, setUsername] = useState<string | null>(null);
-  const [isOrderBy, setIsOrderBy] = useState<string | null>(null);
-  const [isCreate, setIsCreate] = useState(false);
-  const {
-    data: roleData,
-    mutate,
-    isLoading,
-    pagination,
-  } = useFetch<any>(
-    `roles?page=${pageNumber}&limit=8${
-      userName ? `&contains=${userName}` : ""
-    }${isOrderBy ? `&orderBy=${isOrderBy}` : ""}`
-  );
-  return (
-    <PanelLayout title="All Roles - Admin Panel">
-      <section className="px-8 py-4">
-        <CreateRole
-          open={isCreate}
-          handleClose={() => setIsCreate(false)}
-          mutate={mutate}
-        />
+	const [pageNumber, setPageNumber] = useState<number>(1);
+	const [isGrid, setIsGrid] = useState(true);
+	const [userName, setUsername] = useState<string | null>(null);
+	const [isOrderBy, setIsOrderBy] = useState<string | null>(null);
+	const [isCreate, setIsCreate] = useState(false);
+	const {
+		data: roleData,
+		mutate,
+		isLoading,
+		pagination,
+	} = useFetch<any>(
+		`roles?page=${pageNumber}&limit=8${
+			userName ? `&contains=${userName}` : ""
+		}${isOrderBy ? `&orderBy=${isOrderBy}` : ""}`
+	);
+	return (
+		<PanelLayout title="All Roles - Admin Panel">
+			<section className="px-8 py-4">
+				<CreateRole
+					open={isCreate}
+					handleClose={() => setIsCreate(false)}
+					mutate={mutate}
+				/>
 
-        <div className="lg:flex justify-between items-center py-4">
-          <AdminBreadcrumbs links={links} />
-          <div className="md:flex gap-4 items-center">
-            <div className="flex gap-1">
-              <IconButton onClick={() => setIsGrid(true)} size="small">
-                <div
-                  className={` p-2 rounded-md grid place-items-center transition-all ease-in-out duration-500 ${
-                    isGrid && `border-2 border-theme`
-                  }`}
-                >
-                  <GridViewRounded className={`${isGrid && `!text-theme`}`} />
-                </div>
-              </IconButton>
-              <IconButton onClick={() => setIsGrid(false)} size="small">
-                <div
-                  className={` p-2 rounded-md grid place-items-center transition-all ease-in-out duration-500 ${
-                    !isGrid && `border-2 border-theme`
-                  }`}
-                >
-                  <TableRowsRounded className={`${!isGrid && `!text-theme`}`} />
-                </div>
-              </IconButton>
-            </div>
-            <Button
-              onClick={() => setIsCreate(true)}
-              variant="contained"
-              className="!bg-theme"
-              startIcon={<Add />}
-            >
-              CREATE ROLE
-            </Button>
-          </div>
-        </div>
-        <div>
-          <div className="md:flex gap-4 justify-between w-full py-2">
-            <div
-              className={`w-10 h-10 flex justify-center items-center rounded-md shadow-lg bg-theme
+				<div className="lg:flex justify-between items-center py-4">
+					<AdminBreadcrumbs links={links} />
+					<div className="md:flex gap-4 items-center">
+						<div className="flex gap-1">
+							<IconButton onClick={() => setIsGrid(true)} size="small">
+								<div
+									className={` p-2 rounded-md grid place-items-center transition-all ease-in-out duration-500 ${
+										isGrid && `border-2 border-theme`
+									}`}
+								>
+									<GridViewRounded className={`${isGrid && `!text-theme`}`} />
+								</div>
+							</IconButton>
+							<IconButton onClick={() => setIsGrid(false)} size="small">
+								<div
+									className={` p-2 rounded-md grid place-items-center transition-all ease-in-out duration-500 ${
+										!isGrid && `border-2 border-theme`
+									}`}
+								>
+									<TableRowsRounded className={`${!isGrid && `!text-theme`}`} />
+								</div>
+							</IconButton>
+						</div>
+						<Button
+							onClick={() => setIsCreate(true)}
+							variant="contained"
+							className="!bg-theme"
+							startIcon={<Add />}
+						>
+							CREATE ROLE
+						</Button>
+					</div>
+				</div>
+				<div>
+					<div className="md:flex gap-4 justify-between w-full py-2">
+						<div
+							className={`w-10 h-10 flex justify-center items-center rounded-md shadow-lg bg-theme
                 `}
-            >
-              <IconButton
-                onClick={() => {
-                  setIsOrderBy(null);
-                  setUsername(null);
-                }}
-              >
-                <Tooltip
-                  title={
-                    isOrderBy != null || userName != null
-                      ? `Remove Filters`
-                      : `Filter`
-                  }
-                >
-                  {isOrderBy != null || userName != null ? (
-                    <Close className={"!text-white"} />
-                  ) : (
-                    <FilterListRounded className={"!text-white"} />
-                  )}
-                </Tooltip>
-              </IconButton>
-            </div>
+						>
+							<IconButton
+								onClick={() => {
+									setIsOrderBy(null);
+									setUsername(null);
+								}}
+							>
+								<Tooltip
+									title={
+										isOrderBy != null || userName != null
+											? `Remove Filters`
+											: `Filter`
+									}
+								>
+									{isOrderBy != null || userName != null ? (
+										<Close className={"!text-white"} />
+									) : (
+										<FilterListRounded className={"!text-white"} />
+									)}
+								</Tooltip>
+							</IconButton>
+						</div>
 
-            <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-              <TextField
-                fullWidth
-                size="small"
-                id="name"
-                value={userName ? userName : ""}
-                onChange={(e) => {
-                  setPageNumber(1), setUsername(e.target.value);
-                }}
-                placeholder="Role Name"
-                name="name"
-              />
-              <TextField
-                fullWidth
-                select
-                label="Ascending/Descending"
-                size="small"
-                value={isOrderBy ? isOrderBy : ""}
-                onChange={(e) => {
-                  setPageNumber(1), setIsOrderBy(e?.target?.value);
-                }}
-              >
-                {short.map((option) => (
-                  <MenuItem key={option.id} value={option.value}>
-                    {option.name}
-                  </MenuItem>
-                ))}
-              </TextField>
-            </div>
-          </div>
-        </div>
-        {isGrid ? (
-          <>
-            {isLoading && <Loader />}
-            <AllRollGrid data={roleData} mutate={mutate} />
-          </>
-        ) : (
-          <>
-            {isLoading && <Loader />}
-            <AllRollColumn data={roleData} mutate={mutate} />
-          </>
-        )}
-        {roleData?.length === 0 ? <LoaderAnime /> : null}
-        {Math.ceil(
-          Number(pagination?.total || 1) / Number(pagination?.limit || 1)
-        ) > 1 ? (
-          <div className="flex justify-center py-8">
-            <Stack spacing={2}>
-              <Pagination
-                count={Math.ceil(
-                  Number(pagination?.total || 1) /
-                    Number(pagination?.limit || 1)
-                )}
-                onChange={(e, v: number) => {
-                  setPageNumber(v);
-                }}
-                page={pageNumber}
-                variant="outlined"
-              />
-            </Stack>
-          </div>
-        ) : null}
-      </section>
-    </PanelLayout>
-  );
+						<div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+							<TextField
+								fullWidth
+								size="small"
+								id="name"
+								value={userName ? userName : ""}
+								onChange={(e) => {
+									setPageNumber(1), setUsername(e.target.value);
+								}}
+								placeholder="Role Name"
+								name="name"
+							/>
+							<TextField
+								fullWidth
+								select
+								label="Ascending/Descending"
+								size="small"
+								value={isOrderBy ? isOrderBy : ""}
+								onChange={(e) => {
+									setPageNumber(1), setIsOrderBy(e?.target?.value);
+								}}
+							>
+								{short.map((option) => (
+									<MenuItem key={option.id} value={option.value}>
+										{option.name}
+									</MenuItem>
+								))}
+							</TextField>
+						</div>
+					</div>
+				</div>
+				{isGrid ? (
+					<>
+						{isLoading && <Loader />}
+						<AllRollGrid data={roleData} mutate={mutate} />
+					</>
+				) : (
+					<>
+						{isLoading && <Loader />}
+						<AllRollColumn data={roleData} mutate={mutate} />
+					</>
+				)}
+				{roleData?.length === 0 ? <LoaderAnime /> : null}
+				{Math.ceil(
+					Number(pagination?.total || 1) / Number(pagination?.limit || 1)
+				) > 1 ? (
+					<div className="flex justify-center py-8">
+						<Stack spacing={2}>
+							<Pagination
+								count={Math.ceil(
+									Number(pagination?.total || 1) /
+										Number(pagination?.limit || 1)
+								)}
+								onChange={(e, v: number) => {
+									setPageNumber(v);
+								}}
+								page={pageNumber}
+								variant="outlined"
+							/>
+						</Stack>
+					</div>
+				) : null}
+			</section>
+		</PanelLayout>
+	);
 };
 
 export default AllRoles;
 
 const links = [
-  { id: 1, page: "Roles", link: "/admin/roles" },
-  {
-    id: 2,
-    page: "All Roles",
-    link: "/admin/roles/all-roles",
-  },
+	{ id: 1, page: "Roles", link: "/admin/roles" },
+	{
+		id: 2,
+		page: "All Roles",
+		link: "/admin/roles/all-roles",
+	},
 ];
 const short = [
-  { id: 1, value: "name:asc", name: "Name Ascending" },
-  { id: 2, value: "name:desc", name: "Name Descending" },
-  { id: 3, value: "createdAt:desc", name: "CreatedAt Ascending" },
-  { id: 4, value: "createdAt:asc", name: "CreatedAt Descending" },
+	{ id: 1, value: "name:asc", name: "Name Ascending" },
+	{ id: 2, value: "name:desc", name: "Name Descending" },
+	{ id: 3, value: "createdAt:desc", name: "CreatedAt Ascending" },
+	{ id: 4, value: "createdAt:asc", name: "CreatedAt Descending" },
 ];
