7e83f98ce88d0d17e0eaade2a6f822fac54db888
KumarGudu <kg150201@gmail.com>
2023-08-30 22:43:50 +0530

    chat

--- src/components/core/ChatSection.tsx
+++ src/components/core/ChatSection.tsx
@@ -5,8 +5,8 @@ const ChatSection = () => {
   return (
     <section>
       <div className="h-[85vh] w-full rounded-md bg-white mt-4 shadow-md flex">
-        <ChatLeftBar />
-        <ChatRightSection />
+        {/* <ChatLeftBar />
+        <ChatRightSection /> */}
       </div>
     </section>
   );
