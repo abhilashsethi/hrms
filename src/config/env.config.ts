c9a1080074697820490920ce3886141d71b23df6
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-08-30 09:49:25 +0530

    env file update

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,22 +1,23 @@
 export const Public_LOGO = process.env.NEXT_PUBLIC_LOGO || "";
 export const Public_FAV_ICON = process.env.NEXT_PUBLIC_FAVICON || "";
-export const Public_BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL || "";
-export const Public_SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "";
+export const API_DOMAIN = process.env.NEXT_PUBLIC_API_DOMAIN;
+export const Public_BASE_URL = `https://${API_DOMAIN}/api/v1`;
+export const Public_SOCKET_URL = `wss://${API_DOMAIN}`;
 export const Public_API_KEY = process.env.NEXT_PUBLIC_API_KEY || "";
 export const Public_AUTH_DOMAIN = process.env.NEXT_PUBLIC_AUTH_DOMAIN || "";
 export const Public_DATA_BASE_URL = process.env.NEXT_PUBLIC_DATA_BASE_URL || "";
 export const Public_PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID || "";
 export const Public_STORAGE_BUCKET =
-	process.env.NEXT_PUBLIC_STORAGE_BUCKET || "";
+  process.env.NEXT_PUBLIC_STORAGE_BUCKET || "";
 export const Public_MESSAGING_SENDER_ID =
-	process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID || "";
+  process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID || "";
 export const Public_API_ID = process.env.NEXT_PUBLIC_API_ID || "";
 export const Public_AWS_REGION = process.env.NEXT_PUBLIC_AWS_REGION || "";
 export const Public_AWS_ACCESS_KEY_ID =
-	process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID || "";
+  process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID || "";
 export const Public_AWS_SECRET_ACCESS_KEY =
-	process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY || "";
+  process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY || "";
 export const Public_AWS_S3_URL = process.env.NEXT_PUBLIC_AWS_S3_URL || "";
 export const Public_AWS_DISTRIBUTION_ID =
-	process.env.NEXT_PUBLIC_AWS_DISTRIBUTION_ID || "";
+  process.env.NEXT_PUBLIC_AWS_DISTRIBUTION_ID || "";
 export const Public_LOGIN_TITLE = process.env.NEXT_PUBLIC_LOGIN_TITLE || "";
