a0826f4e87c54e29139c341e65e6b0139638c86f
KumarGudu <kg150201@gmail.com>
2023-09-01 18:47:34 +0530

    final change

--- src/pages/admin/quotation/all-quotation.tsx
+++ src/pages/admin/quotation/all-quotation.tsx
@@ -55,7 +55,7 @@ const AllQuotation = () => {
             </Link>
           </div>
 
-          <div className="md:flex grid gap-4 justify-between w-full py-2">
+          <div className="md:flex grid gap-4 md:justify-between w-full py-2">
             <div
               className={`w-10 h-10 flex justify-center items-center rounded-md shadow-lg bg-theme
                 `}
