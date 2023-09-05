676b70bdee8bd867bb1354d95a7c7fa715ad0378
KumarGudu <kg150201@gmail.com>
2023-09-05 13:24:30 +0530

    meeting

--- src/components/drawer/ViewNotesDrawer.tsx
+++ src/components/drawer/ViewNotesDrawer.tsx
@@ -1,371 +1,239 @@
 import { makeStyles } from "@material-ui/core";
-import { Add, Close, Delete, EventNote } from "@mui/icons-material";
+import { Add, Close, Delete } from "@mui/icons-material";
 import {
-	Avatar,
-	Button,
-	CircularProgress,
-	Container,
-	Drawer,
-	IconButton,
-	Tooltip,
+  Button,
+  CircularProgress,
+  Container,
+  Drawer,
+  IconButton,
+  Tooltip,
 } from "@mui/material";
 import { Loader, LoaderAnime } from "components/core";
-import { DocPreview } from "components/dialogues";
 import AddMeetingNotes from "components/dialogues/AddMeetingNotes";
-import { useAuth, useChange, useFetch } from "hooks";
+import { useAuth, useChange } from "hooks";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import Swal from "sweetalert2";
-import { User } from "types";
 
 type Props = {
-	open?: boolean | any;
-	onClose: () => void;
-	setViewTickets?: any;
-	ticket?: any;
-	meetingDetails: any;
-	mutate?: any;
+  open?: boolean;
+  onClose: () => void;
+  setViewTickets?: any;
+  ticket?: any;
+  meetingDetails: any;
+  isLoading?: boolean;
+  mutate: () => void;
 };
 
 const useStyles = makeStyles((theme) => ({
-	container: {
-		width: "100vw",
-		[theme.breakpoints.up("sm")]: {
-			maxWidth: "50vw",
-		},
-		[theme.breakpoints.up("md")]: {
-			maxWidth: "80vw",
-		},
-		[theme.breakpoints.up("lg")]: {
-			maxWidth: "30vw",
-		},
-	},
+  container: {
+    width: "100vw",
+    [theme.breakpoints.up("sm")]: {
+      maxWidth: "50vw",
+    },
+    [theme.breakpoints.up("md")]: {
+      maxWidth: "80vw",
+    },
+    [theme.breakpoints.up("lg")]: {
+      maxWidth: "30vw",
+    },
+  },
 }));
 
-const style = {
-	position: "absolute" as "absolute",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	width: 400,
-	// height: 600,
-	bgcolor: "background.paper",
-	// border: "2px solid #000",
-	borderRadius: "10px",
-	boxShadow: 24,
-	p: 4,
-};
+const ViewNotesDrawer = ({
+  open,
+  onClose,
+  meetingDetails,
+  mutate,
+  isLoading,
+}: Props) => {
+  const { user } = useAuth();
+  const [openInfoModal, setOpenInfoModal] = useState(false);
 
-const ViewNotesDrawer = ({ open, onClose, meetingDetails, mutate }: Props) => {
-	console.log(meetingDetails);
-	const { user } = useAuth();
-	const router = useRouter();
-	const [openInfoModal, setOpenInfoModal] = useState(false);
-	const handleInfoOpen = () => {
-		setOpenInfoModal(true);
-	};
-	const handleInfoCloseModal = () => setOpenInfoModal(false);
-	const { data: users, isLoading } = useFetch<User[]>(`users`);
+  const [editDetails, setEditDetails] = useState<boolean>(false);
+  const classes = useStyles();
 
-	const [isPreview, setIsPreview] = useState<{
-		dialogue?: boolean;
-		title?: string;
-	}>({
-		dialogue: false,
-		title: "Preview",
-	});
+  return (
+    <>
+      <AddMeetingNotes
+        open={editDetails}
+        handleClose={() => setEditDetails(false)}
+        details={meetingDetails}
+        mutate={mutate}
+      />
 
-	const [editDetails, setEditDetails] = useState<boolean>(false);
-	const classes = useStyles();
+      <Drawer anchor="right" open={open} onClose={() => onClose && onClose()}>
+        <Container style={{ marginTop: "1rem" }} className={classes.container}>
+          <div className="flex items-center justify-between pb-4">
+            <p className="text-lg font-bold text-theme">View All Notes</p>
+            {user?.role?.name === "CEO" ||
+            user?.role?.name === "PROJECT MANAGER" ? (
+              <div className="flex ">
+                <Button
+                  variant="contained"
+                  className="!bg-blue-500 "
+                  startIcon={<Add />}
+                  size="small"
+                  onClick={() => setEditDetails((prev) => !prev)}
+                >
+                  Add Notes
+                </Button>
+                <IconButton onClick={() => onClose()}>
+                  <Close
+                    fontSize="small"
+                    className="text-red-500 block md:hidden"
+                  />
+                </IconButton>
+              </div>
+            ) : null}
+          </div>
 
-	const { change } = useChange();
-	const handleDelete = (meetingId: string, noteId: any) => {
-		Swal.fire({
-			title: "Are you sure?",
-			text: "You want to delete!",
-			icon: "warning",
-			showCancelButton: true,
-			confirmButtonColor: "#3085d6",
-			cancelButtonColor: "#d33",
-			confirmButtonText: "Yes, delete it!",
-		}).then(async (result) => {
-			try {
-				if (result.isConfirmed) {
-					Swal.fire(`Info`, "It will take some time", "info");
-					const response = await change(`meetings/remove-notes/${meetingId}`, {
-						method: "DELETE",
-						body: {
-							notes: [`${noteId}`],
-						},
-					});
-					if (response?.status !== 200) {
-						Swal.fire("Error", "Something went wrong!", "error");
-					}
-					Swal.fire("Success", "Deleted successfully!", "success");
-					mutate();
-				}
-			} catch (error) {
-				console.log(error);
-			}
-		});
-	};
+          {isLoading && <Loader />}
+          <div className="mt-4 flex flex-col gap-4">
+            <div className="">
+              {meetingDetails?.notes?.length ? (
+                meetingDetails?.notes?.map((item: any, i: any) => {
+                  console.log(item);
+                  return (
+                    <div
+                      key={i}
+                      className="w-full relative rounded-l-xl shadow-xl px-2 py-2 bg-gradient-to-r from-rose-100 to-teal-100 mb-8"
+                    >
+                      <div className="absolute -top-4"></div>
+                      <div className="mt-7">
+                        <div className="md:flex w-full grid md:justify-between items-center gap-5">
+                          <img
+                            className="h-20 w-20 "
+                            src={"/writing.png"}
+                            alt=""
+                          />
+                          <div className="">
+                            <p className="font-semibold">
+                              Notes :{" "}
+                              <span className="text-sm text-gray-600">
+                                {item?.text}
+                              </span>
+                              <span className="font-semibold text-sm text-gray-500">
+                                {/* {item?.name} */}
+                              </span>
+                            </p>
+                            <p className="font-semibold">
+                              Added By :{" "}
+                              <span className="font-semibold text-sm text-gray-500">
+                                {item?.addedBy?.name}
+                              </span>
+                            </p>
 
-	return (
-		<>
-			<DocPreview
-				open={isPreview?.dialogue}
-				handleClose={() => setIsPreview({ dialogue: false })}
-				title={isPreview?.title}
-			/>
+                            <p className="font-semibold">
+                              Note Link :{" "}
+                              <a
+                                className="text-sm font-medium text-blue-500 underline"
+                                href={`${item?.link}`}
+                              >
+                                Note Link
+                              </a>
+                            </p>
+                          </div>
+                          <Tooltip title="Details">
+                            <div
+                            //   onClick={() =>
+                            //     setIsPreview({
+                            //       dialogue: true,
+                            //       title: "Doc 53426",
+                            //     })
+                            //   }
+                            >
+                              <div className="w-24 rounded-full group flex justify-start items-center hover:scale-105 ease-in-out transition-all duration-400 cursor-pointer !text-blue-600 flex-col gap-2">
+                                {item?.img}
+                              </div>
+                            </div>
+                          </Tooltip>
+                        </div>
 
-			<AddMeetingNotes
-				open={editDetails}
-				handleClose={() => setEditDetails(false)}
-				details={meetingDetails}
-				mutate={mutate}
-			/>
-
-			<Drawer anchor="right" open={open} onClose={() => onClose && onClose()}>
-				<Container
-					style={{ marginTop: "1rem" }}
-					className={classes.container}
-					// style={{
-					// 	width: "30vw",
-					// 	marginTop: "3.5vh",
-					// }}
-				>
-					{/* Document Modal  */}
-					{/* <Modal
-						open={openInfoModal}
-						onClose={handleInfoCloseModal}
-						aria-labelledby="modal-modal-title"
-						aria-describedby="modal-modal-description"
-					>
-						<Card
-							sx={style}
-							className="dashboard-card-shadow w-[30%] border-t-4 border-b-4 border-t-theme border-b-theme !p-6"
-						>
-							Open Modal
-							<iframe
-								src={"/invoice11.pdf"}
-								width="100%"
-								height="500"
-								title="Document Preview"
-							/>
-						</Card>
-					</Modal> */}
-					<div className="flex items-center justify-between pb-4">
-						<p className="text-lg font-bold text-theme">View All Notes</p>
-						{user?.role?.name === "CEO" ||
-						user?.role?.name === "PROJECT MANAGER" ? (
-							<div className="flex ">
-								<Button
-									variant="contained"
-									className="!bg-blue-500 "
-									startIcon={<Add />}
-									size="small"
-									onClick={() => setEditDetails((prev) => !prev)}
-								>
-									Add Notes
-								</Button>
-								<IconButton onClick={() => onClose()}>
-									<Close
-										fontSize="small"
-										className="text-red-500 block md:hidden"
-									/>
-								</IconButton>
-							</div>
-						) : null}
-					</div>
-
-					{isLoading && <Loader />}
-					<div className="mt-4 flex flex-col gap-4">
-						<div className="">
-							{meetingDetails?.notes?.length ? (
-								meetingDetails?.notes?.map((item: any, i: any) => {
-									console.log(item);
-									return (
-										<div
-											key={i}
-											className="w-full relative rounded-l-xl shadow-xl px-2 py-2 bg-gradient-to-r from-rose-100 to-teal-100 mb-8"
-										>
-											<div className="absolute -top-4">
-												<Avatar
-													onClick={() => handleInfoOpen()}
-													variant="rounded"
-													className="!mr-0.5 !ml-0.5 !cursor-pointer !bg-blue-700 !p-0"
-												>
-													<EventNote
-														sx={{ padding: "0px !important" }}
-														fontSize="large"
-													/>
-												</Avatar>
-											</div>
-											<div className="mt-7">
-												<div className="flex w-full justify-between items-center gap-5">
-													<img
-														className="h-20 w-20 "
-														src={"/writing.png"}
-														alt=""
-													/>
-													<div className="">
-														<p className="font-semibold">
-															Notes :{" "}
-															<span className="text-sm text-gray-600">
-																{item?.text}
-															</span>
-															<span className="font-semibold text-sm text-gray-500">
-																{/* {item?.name} */}
-															</span>
-														</p>
-														<p className="font-semibold">
-															Added By :{" "}
-															<span className="font-semibold text-sm text-gray-500">
-																{item?.addedBy?.name}
-															</span>
-														</p>
-
-														<p className="font-semibold">
-															Note Link :{" "}
-															<a
-																className="text-sm font-medium text-blue-500 underline"
-																href={`${item?.link}`}
-															>
-																Note Link
-															</a>
-														</p>
-													</div>
-													<Tooltip title="Details">
-														<div
-															onClick={() =>
-																setIsPreview({
-																	dialogue: true,
-																	title: "Doc 53426",
-																})
-															}
-														>
-															<div className="w-24 rounded-full group flex justify-start items-center hover:scale-105 ease-in-out transition-all duration-400 cursor-pointer !text-blue-600 flex-col gap-2">
-																{item?.img}
-															</div>
-														</div>
-													</Tooltip>
-												</div>
-
-												{user?.role?.name === "CEO" ||
-												user?.role?.name === "PROJECT MANAGER" ? (
-													<div className="flex justify-end">
-														<DeleteButton
-															meetingId={meetingDetails?.id}
-															id={item?.id}
-															mutate={mutate}
-														/>
-													</div>
-												) : null}
-											</div>
-										</div>
-									);
-								})
-							) : (
-								<LoaderAnime />
-							)}
-						</div>
-					</div>
-				</Container>
-			</Drawer>
-		</>
-	);
+                        {user?.role?.name === "CEO" ||
+                        user?.role?.name === "PROJECT MANAGER" ? (
+                          <div className="flex justify-end">
+                            <DeleteButton
+                              meetingId={meetingDetails?.id}
+                              id={item?.id}
+                              mutate={mutate}
+                            />
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>
+                  );
+                })
+              ) : (
+                <LoaderAnime />
+              )}
+            </div>
+          </div>
+        </Container>
+      </Drawer>
+    </>
+  );
 };
 
 export default ViewNotesDrawer;
 
 interface ButtonProps {
-	id?: string | null | undefined;
-	mutate?: any;
-	meetingId?: any;
+  id?: string | null | undefined;
+  mutate?: any;
+  meetingId?: any;
 }
 
 const DeleteButton = ({ id, mutate, meetingId }: ButtonProps) => {
-	const router = useRouter();
-	const [loading, setLoading] = useState(false);
-	const { change } = useChange();
-	const handleDelete = (meetingId: any, id: any) => {
-		Swal.fire({
-			title: "Are you sure?",
-			text: "You want to delete!",
-			icon: "warning",
-			showCancelButton: true,
-			confirmButtonColor: "#3085d6",
-			cancelButtonColor: "#d33",
-			confirmButtonText: "Yes, delete it!",
-		}).then(async (result) => {
-			try {
-				setLoading(true);
-				if (result.isConfirmed) {
-					const response = await change(`meetings/remove-notes/${meetingId}`, {
-						method: "DELETE",
-						body: {
-							notes: [`${id}`],
-						},
-					});
-					setLoading(false);
-					if (response?.status !== 200) {
-						Swal.fire("Error", "Something went wrong!", "error");
-					}
-					mutate();
-					Swal.fire("Success", "Deleted successfully!", "success");
-				}
-			} catch (error) {
-				console.log(error);
-				setLoading(false);
-			} finally {
-				setLoading(false);
-			}
-		});
-	};
-	return (
-		<Button
-			onClick={() => {
-				// setActiveId(item?.id);
-				handleDelete(meetingId, id);
-			}}
-			disabled={loading}
-			variant="contained"
-			className="!bg-red-500 text-xs"
-			startIcon={loading ? <CircularProgress size={20} /> : <Delete />}
-			size="small"
-		>
-			DELETE
-		</Button>
-	);
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { change } = useChange();
+  const handleDelete = (meetingId: any, id: any) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You want to delete!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      try {
+        setLoading(true);
+        if (result.isConfirmed) {
+          const response = await change(`meetings/remove-notes/${meetingId}`, {
+            method: "DELETE",
+            body: {
+              notes: [`${id}`],
+            },
+          });
+          setLoading(false);
+          if (response?.status !== 200) {
+            Swal.fire("Error", "Something went wrong!", "error");
+          }
+          mutate();
+          Swal.fire("Success", "Deleted successfully!", "success");
+        }
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    });
+  };
+  return (
+    <Button
+      onClick={() => {
+        // setActiveId(item?.id);
+        handleDelete(meetingId, id);
+      }}
+      disabled={loading}
+      variant="contained"
+      className="!bg-red-500 text-xs"
+      startIcon={loading ? <CircularProgress size={20} /> : <Delete />}
+      size="small"
+    >
+      DELETE
+    </Button>
+  );
 };
-
-// const { change } = useChange();
-// const handleDelete = (meetingId: string, noteId: any) => {
-// 	Swal.fire({
-// 		title: "Are you sure?",
-// 		text: "You want to delete!",
-// 		icon: "warning",
-// 		showCancelButton: true,
-// 		confirmButtonColor: "#3085d6",
-// 		cancelButtonColor: "#d33",
-// 		confirmButtonText: "Yes, delete it!",
-// 	}).then(async (result) => {
-// 		try {
-// 			if (result.isConfirmed) {
-// 				const response = await change(`meetings/remove-notes/${meetingId}`, {
-// 					method: "DELETE",
-// 					body: {
-// 						notes: [`${noteId}`],
-// 					},
-// 				});
-// 				if (response?.status !== 200) {
-// 					Swal.fire("Error", "Something went wrong!", "error");
-// 				}
-// 				Swal.fire("Success", "Deleted successfully!", "success");
-// 				mutate();
-// 			}
-// 		} catch (error) {
-// 			console.log(error);
-// 		}
-// 	});
-// };
--- src/pages/admin/meetings/index.tsx
+++ src/pages/admin/meetings/index.tsx
@@ -1,12 +1,12 @@
 import {
-	AssignmentTurnedIn,
-	ContactPhone,
-	PendingActions,
+  AssignmentTurnedIn,
+  ContactPhone,
+  PendingActions,
 } from "@mui/icons-material";
 import {
-	MEETINGICON,
-	MEETINGICON2,
-	MEETINGICON3,
+  MEETINGICON,
+  MEETINGICON2,
+  MEETINGICON3,
 } from "assets/dashboard_Icons";
 import { MeetingAnalytics, MeetingDonutChart } from "components/analytics";
 import { DashboardCard } from "components/core";
@@ -14,115 +14,114 @@ import { useFetch } from "hooks";
 import PanelLayout from "layouts/panel";
 
 const Meetings = () => {
-	const { data: meetingData, mutate } = useFetch<any>(
-		`meetings/dashboard/details`
-	);
-	console.log(meetingData);
+  const { data: meetingData, mutate } = useFetch<any>(
+    `meetings/dashboard/details`
+  );
 
-	const cards = [
-		{
-			id: 1,
-			icon: <ContactPhone className="text-theme" />,
-			count: meetingData?.totalMeetings ? meetingData?.totalMeetings : 0,
-			title: "Total Meetings",
-			img: MEETINGICON.src,
-			bg: "from-blue-500 to-blue-300",
-			link: "/admin/meetings/all-meetings",
-		},
-		{
-			id: 2,
-			icon: <PendingActions className="text-theme" />,
-			count: meetingData?.meetingStatusList.length
-				? meetingData?.meetingStatusList
-						?.filter((item: any) => item?.status === "Ongoing")
-						?.map((item: any) => (item?._count ? item?._count : 0))
-				: 0,
-			title: "Ongoing Meetings",
-			bg: "from-yellow-500 to-yellow-300",
-			img: MEETINGICON2.src,
-			link: "/admin/meetings/all-meetings",
-		},
-		{
-			id: 3,
-			icon: <AssignmentTurnedIn className="text-theme" />,
-			count: meetingData?.meetingStatusList.length
-				? meetingData?.meetingStatusList
-						?.filter((item: any) => item?.status === "Closed")
-						?.map((item: any) => (item?._count ? item?._count : 0))
-				: 0,
-			title: "Closed Meetings",
-			bg: "from-emerald-500 to-emerald-300",
-			img: MEETINGICON3.src,
-			link: "/admin/meetings/all-meetings",
-		},
-	];
+  const cards = [
+    {
+      id: 1,
+      icon: <ContactPhone className="text-theme" />,
+      count: meetingData?.totalMeetings ? meetingData?.totalMeetings : 0,
+      title: "Total Meetings",
+      img: MEETINGICON.src,
+      bg: "from-blue-500 to-blue-300",
+      link: "/admin/meetings/all-meetings",
+    },
+    {
+      id: 2,
+      icon: <PendingActions className="text-theme" />,
+      count: meetingData?.meetingStatusList.length
+        ? meetingData?.meetingStatusList
+            ?.filter((item: any) => item?.status === "Ongoing")
+            ?.map((item: any) => (item?._count ? item?._count : 0))
+        : 0,
+      title: "Ongoing Meetings",
+      bg: "from-yellow-500 to-yellow-300",
+      img: MEETINGICON2.src,
+      link: "/admin/meetings/all-meetings",
+    },
+    {
+      id: 3,
+      icon: <AssignmentTurnedIn className="text-theme" />,
+      count: meetingData?.meetingStatusList.length
+        ? meetingData?.meetingStatusList
+            ?.filter((item: any) => item?.status === "Closed")
+            ?.map((item: any) => (item?._count ? item?._count : 0))
+        : 0,
+      title: "Closed Meetings",
+      bg: "from-emerald-500 to-emerald-300",
+      img: MEETINGICON3.src,
+      link: "/admin/meetings/all-meetings",
+    },
+  ];
 
-	return (
-		<PanelLayout title="Meetings - Admin Panel">
-			<div>
-				<div className="flex gap-2 py-4">
-					<DashboardCard data={cards} />
-				</div>
-				<div className="grid grid-cols-12 content-between gap-6  m-5 !mb-6">
-					<div className="col-span-12 pt-9 w-full  gap-5 md:col-span-12 lg:col-span-7 !border-grey-500 rounded-xl !shadow-xl">
-						<p className="font-bold text-lg text-center">Meeting Overview</p>
-						<MeetingAnalytics
-							series={[
-								{
-									name: "Total Meetings",
-									type: "column",
-									data: meetingData?.meetingCountsMonthWise
-										? meetingData?.meetingCountsMonthWise?.map(
-												(item: any) => item?.count
-										  )
-										: [],
-								},
-								{
-									name: "Closed Meetings",
-									type: "area",
-									data: meetingData?.closedMeetingCountsMonthWise
-										? meetingData?.closedMeetingCountsMonthWise?.map(
-												(item: any) => item?.count
-										  )
-										: [],
-								},
-							]}
-							labels={
-								meetingData?.meetingCountsMonthWise
-									? meetingData?.meetingCountsMonthWise?.map(
-											(item: any) => item?.month
-									  )
-									: []
-							}
-							text=""
-							type="line"
-						/>
-					</div>
-					<div className="col-span-12 pt-9 w-full flex flex-col justify-center gap-5 md:col-span-12 lg:col-span-5 !border-gray-500 rounded-xl !shadow-xl">
-						<p className="font-bold text-lg text-center">Meeting Details</p>
-						<MeetingDonutChart
-							text=""
-							type="donut"
-							labels={
-								meetingData?.meetingStatusList?.length
-									? meetingData?.meetingStatusList?.map(
-											(item: any) => item?.status
-									  )
-									: []
-							}
-							series={
-								meetingData?.meetingStatusList?.length
-									? meetingData?.meetingStatusList?.map(
-											(item: any) => item?._count
-									  )
-									: []
-							}
-						/>
-					</div>
-				</div>
-			</div>
-		</PanelLayout>
-	);
+  return (
+    <PanelLayout title="Meetings - Admin Panel">
+      <div>
+        <div className="flex gap-2 px-2 py-4">
+          <DashboardCard data={cards} />
+        </div>
+        <div className="grid grid-cols-12 content-between gap-6 px-2 !mb-6">
+          <div className="col-span-12 pt-9 w-full  gap-5 md:col-span-12 lg:col-span-7 !border-grey-500 rounded-xl !shadow-xl">
+            <p className="font-bold text-lg text-center">Meeting Overview</p>
+            <MeetingAnalytics
+              series={[
+                {
+                  name: "Total Meetings",
+                  type: "column",
+                  data: meetingData?.meetingCountsMonthWise
+                    ? meetingData?.meetingCountsMonthWise?.map(
+                        (item: any) => item?.count
+                      )
+                    : [],
+                },
+                {
+                  name: "Closed Meetings",
+                  type: "area",
+                  data: meetingData?.closedMeetingCountsMonthWise
+                    ? meetingData?.closedMeetingCountsMonthWise?.map(
+                        (item: any) => item?.count
+                      )
+                    : [],
+                },
+              ]}
+              labels={
+                meetingData?.meetingCountsMonthWise
+                  ? meetingData?.meetingCountsMonthWise?.map(
+                      (item: any) => item?.month
+                    )
+                  : []
+              }
+              text=""
+              type="line"
+            />
+          </div>
+          <div className="col-span-12 pt-9 py-4 bg-white w-full flex flex-col justify-center gap-5 md:col-span-12 lg:col-span-5 !border-gray-500 rounded-xl !shadow-xl">
+            <p className="font-bold text-lg text-center">Meeting Details</p>
+            <MeetingDonutChart
+              text=""
+              type="donut"
+              labels={
+                meetingData?.meetingStatusList?.length
+                  ? meetingData?.meetingStatusList?.map(
+                      (item: any) => item?.status
+                    )
+                  : []
+              }
+              series={
+                meetingData?.meetingStatusList?.length
+                  ? meetingData?.meetingStatusList?.map(
+                      (item: any) => item?._count
+                    )
+                  : []
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </PanelLayout>
+  );
 };
 
 export default Meetings;
