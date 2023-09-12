3708ebc78624befba45f613948589a0aa8c86af2
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-09-12 12:06:38 +0530

    security dashboard done

--- src/pages/admin/security/index.tsx
+++ src/pages/admin/security/index.tsx
@@ -1,35 +1,38 @@
 import { DashboardSkeletonLoading } from "components/admin/assets";
 import {
-  QuotationDashboardCard,
-  QuotationDashboardCharts,
+	QuotationDashboardCard,
+	QuotationDashboardCharts,
 } from "components/admin/quotation";
-import { SecurityDashboardCard } from "components/admin/security";
+import {
+	SecurityDashboardCard,
+	SecurityDashboardCharts,
+} from "components/admin/security";
 import { AdminBreadcrumbs } from "components/core";
 import { useFetch } from "hooks";
 import PanelLayout from "layouts/panel";
 import { Quotation } from "types";
 
 const index = () => {
-  const { data: dashboardData, isLoading } = useFetch<Quotation>(
-    `quotations/dashboard/info`
-  );
-  return (
-    <PanelLayout title="All Branches ">
-      <>
-        <section className="lg:px-8 px-4 py-4">
-          <AdminBreadcrumbs links={links} />
-          {isLoading ? (
-            <DashboardSkeletonLoading />
-          ) : (
-            <>
-              <SecurityDashboardCard data={dashboardData} />
-              <QuotationDashboardCharts data={dashboardData} />
-            </>
-          )}
-        </section>
-      </>
-    </PanelLayout>
-  );
+	const { data: dashboardData, isLoading } = useFetch<Quotation>(
+		`quotations/dashboard/info`
+	);
+	return (
+		<PanelLayout title="All Branches ">
+			<>
+				<section className="lg:px-8 px-4 py-4">
+					<AdminBreadcrumbs links={links} />
+					{isLoading ? (
+						<DashboardSkeletonLoading />
+					) : (
+						<>
+							<SecurityDashboardCard data={dashboardData} />
+							<SecurityDashboardCharts data={dashboardData} />
+						</>
+					)}
+				</section>
+			</>
+		</PanelLayout>
+	);
 };
 
 export default index;
