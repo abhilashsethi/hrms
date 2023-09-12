f70cd74002c0bf7e3d01f140d9423892476f9583
KumarGudu <kg150201@gmail.com>
2023-09-12 11:18:53 +0530

    appoitment

--- src/pages/admin/security/all-appointments.tsx
+++ src/pages/admin/security/all-appointments.tsx
@@ -1,8 +1,10 @@
 import { Add, Close, FilterListRounded } from "@mui/icons-material";
 import {
+  Autocomplete,
   Button,
   IconButton,
   Link,
+  MenuItem,
   Pagination,
   Stack,
   TextField,
@@ -10,19 +12,22 @@ import {
 } from "@mui/material";
 import { AppointmentsGrid } from "components/admin/security";
 import { AdminBreadcrumbs, LoaderAnime, SkeletonLoader } from "components/core";
-import { useFetch } from "hooks";
+import { useAuth, useFetch } from "hooks";
 import PanelLayout from "layouts/panel";
+import moment from "moment";
 import { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
-import { APPOINTMENT } from "types";
+import { APPOINTMENT, Branch } from "types";
 
 const AllAppointments = () => {
+  const { user } = useAuth();
   const [pageNumber, setPageNumber] = useState<number>(1);
-  const [clientName, setClientName] = useState<string | null>(null);
+  const [isBranchId, setBranchId] = useState<string | null>(null);
   const [searchTitle, setSearchTitle] = useState<string | null>(null);
-  const [BillStatus, setBillStatus] = useState<string | null>(null);
+  const [isStatus, setStatus] = useState<string | null>(null);
   const [selectDate, setSelectDate] = useState<string | null>(null);
   const [isOrderBy, setIsOrderBy] = useState<string | null>(null);
+  const { data: branchData } = useFetch<Branch[]>(`branches`);
   const {
     data: appointmentData,
     mutate,
@@ -30,11 +35,17 @@ const AllAppointments = () => {
     pagination,
   } = useFetch<APPOINTMENT[]>(
     `appointments?page=${pageNumber}&limit=6${
-      clientName ? `&clientName=${clientName}` : ""
-    }${BillStatus ? `&billType=${BillStatus}` : ""}${
-      searchTitle ? `&searchTitle=${searchTitle}` : ""
-    }${selectDate ? `&dueDate=${selectDate}` : ""}${
-      isOrderBy ? `&orderBy=${isOrderBy}` : ""
+      isStatus ? `&status=${isStatus}` : ""
+    }${searchTitle ? `&searchTitle=${searchTitle}` : ""}${
+      selectDate ? `&startDate=${new Date(selectDate).getTime()}` : ""
+    }${isOrderBy ? `&orderBy=${isOrderBy}` : ""}${
+      isBranchId ? `&branchId=${isBranchId}` : ""
+    }${
+      user?.role?.name === "CEO" ||
+      user?.role?.name === "DIRECTOR" ||
+      user?.role?.name === "COO"
+        ? ``
+        : `&branchId=${isBranchId}`
     }`
   );
   return (
@@ -62,12 +73,28 @@ const AllAppointments = () => {
               <IconButton
                 onClick={() => {
                   setSearchTitle(null);
+                  setStatus(null);
+                  setSelectDate(null);
+                  setIsOrderBy(null);
+                  setBranchId(null);
                 }}
               >
                 <Tooltip
-                  title={searchTitle != null ? `Remove Filters` : `Filter`}
+                  title={
+                    searchTitle != null ||
+                    isStatus != null ||
+                    selectDate != null ||
+                    isOrderBy != null ||
+                    isBranchId != null
+                      ? `Remove Filters`
+                      : `Filter`
+                  }
                 >
-                  {searchTitle != null ? (
+                  {searchTitle != null ||
+                  isStatus != null ||
+                  selectDate != null ||
+                  isOrderBy != null ||
+                  isBranchId != null ? (
                     <Close className={"!text-white"} />
                   ) : (
                     <FilterListRounded className={"!text-white"} />
@@ -87,6 +114,72 @@ const AllAppointments = () => {
                   setPageNumber(1), setSearchTitle(e.target.value);
                 }}
               />
+              <TextField
+                fullWidth
+                select
+                label="Status"
+                id="status"
+                size="small"
+                value={isStatus ? isStatus : null}
+                onChange={(e) => {
+                  setPageNumber(1), setStatus(e?.target?.value);
+                }}
+              >
+                {Status_Type.map((option) => (
+                  <MenuItem key={option.id} value={option.value}>
+                    {option.name}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                fullWidth
+                size="small"
+                id="date"
+                label="Date"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                placeholder="Select Date"
+                name="date"
+                type="date"
+                value={
+                  selectDate ? moment(selectDate).format("YYYY-MM-DD") : ""
+                }
+                onChange={(e) => {
+                  {
+                    setPageNumber(1),
+                      setSelectDate(new Date(e.target.value).toISOString());
+                  }
+                }}
+              />
+              {user?.role?.name === "CEO" ||
+              user?.role?.name === "COO" ||
+              user?.role?.name === "DIRECTOR" ? (
+                <Autocomplete
+                  fullWidth
+                  size="small"
+                  id="branchId"
+                  options={branchData || []}
+                  onChange={(e: React.ChangeEvent<{}>, r: Branch | null) => {
+                    setBranchId(r?.id || "");
+                  }}
+                  value={
+                    isBranchId
+                      ? branchData?.find(
+                          (option: any) => option.id === isBranchId
+                        )
+                      : null
+                  }
+                  getOptionLabel={(option: Branch) => option.name || ""} // Ensure a string is always returned
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      // label="Role"
+                      placeholder="Branch"
+                    />
+                  )}
+                />
+              ) : null}
             </div>
           </div>
           {isLoading ? (
@@ -129,10 +222,17 @@ const AllAppointments = () => {
 
 export default AllAppointments;
 
-const status = [
-  { id: 1, value: "Unpaid" },
-  { id: 2, value: "Advance" },
-  { id: 3, value: "Paid" },
+const Status_Type = [
+  {
+    id: 1,
+    name: "Completed",
+    value: "Completed",
+  },
+  {
+    id: 2,
+    name: "Pending",
+    value: "Pending",
+  },
 ];
 
 const links = [
