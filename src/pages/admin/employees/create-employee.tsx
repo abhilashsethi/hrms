b5e81683745de3f4f8ac6c7f2117ed8f2c6d1781
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-09-25 10:46:05 +0530

    bug fixed in create employee

--- src/pages/admin/employees/create-employee.tsx
+++ src/pages/admin/employees/create-employee.tsx
@@ -53,8 +53,7 @@ const validationSchema = Yup.object().shape({
 			/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
 			"Phone number is not valid"
 		)
-		.min(6)
-		.max(15),
+		.min(10),
 	email: Yup.string()
 		.email("Invalid email address")
 		.required("Email is required!")
