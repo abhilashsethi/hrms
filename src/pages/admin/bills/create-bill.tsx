ba8bd9f2cc270d8cc6acaab9a5d51716301831fd
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-09-28 17:17:59 +0530

    minor changes

--- src/pages/admin/bills/create-bill.tsx
+++ src/pages/admin/bills/create-bill.tsx
@@ -96,8 +96,7 @@ const CreateBills = () => {
 				/^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/,
 				"Invalid GST number format"
 			)
-			.max(15)
-			.required("GST number is required"),
+			.max(15),
 	});
 	const { data: clients } = useFetch<Client[]>(`clients`);
 	const { data: quotation } = useFetch<Quotation[]>(`quotations`);
