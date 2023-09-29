d699aaa80ecdd5c7fe213c6e967dcf08619b0fcf
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-09-29 17:08:01 +0530

    minor changes

--- src/components/admin/support/SupportColumn.tsx
+++ src/components/admin/support/SupportColumn.tsx
@@ -10,233 +10,234 @@ import Swal from "sweetalert2";
 import { Support } from "types";
 import { MuiTblOptions } from "utils";
 interface Props {
-  data?: Support[];
-  mutate: () => void;
+	data?: Support[];
+	mutate: () => void;
 }
 const SupportColumn = ({ data, mutate }: Props) => {
-  const { change } = useChange();
-  const [isReply, setIsReply] = useState<any>({
-    dialogue: false,
-    departmentData: null,
-  });
+	const { change } = useChange();
+	const [isReply, setIsReply] = useState<any>({
+		dialogue: false,
+		departmentData: null,
+	});
 
-  const handleDelete = (id?: string) => {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You want to delete!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then(async (result) => {
-      try {
-        if (result.isConfirmed) {
-          Swal.fire(`Info`, "It will take some time", "info");
-          const response = await change(`supports/${id}`, {
-            method: "DELETE",
-          });
-          if (response?.status !== 200) {
-            Swal.fire("Error", "Something went wrong!", "error");
-          }
-          Swal.fire("Success", "Deleted successfully!", "success");
-          mutate();
-        }
-      } catch (error) {
-        if (error instanceof Error) {
-          Swal.fire(`Error`, error?.message, `error`);
-        } else {
-          Swal.fire(`Error`, "Something Went Wrong", `error`);
-        }
-      }
-    });
-  };
-  const handleSubmit = async (e: any, id?: string) => {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You want to update status?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, update!",
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        const res = await change(`supports/${id}`, {
-          method: "PATCH",
-          body: {
-            isResolved: !e?.target?.checked,
-          },
-        });
-        mutate();
+	const handleDelete = (id?: string) => {
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You want to delete!",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes, delete it!",
+		}).then(async (result) => {
+			try {
+				if (result.isConfirmed) {
+					Swal.fire(`Info`, "It will take some time", "info");
+					const response = await change(`supports/${id}`, {
+						method: "DELETE",
+					});
+					if (response?.status !== 200) {
+						Swal.fire("Error", "Something went wrong!", "error");
+					}
+					Swal.fire("Success", "Deleted successfully!", "success");
+					mutate();
+				}
+			} catch (error) {
+				if (error instanceof Error) {
+					Swal.fire(`Error`, error?.message, `error`);
+				} else {
+					Swal.fire(`Error`, "Something Went Wrong", `error`);
+				}
+			}
+		});
+	};
+	const handleSubmit = async (e: any, id?: string) => {
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You want to update status?",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes, update!",
+		}).then(async (result) => {
+			if (result.isConfirmed) {
+				const res = await change(`supports/${id}`, {
+					method: "PATCH",
+					body: {
+						isResolved: !e?.target?.checked,
+					},
+				});
+				mutate();
 
-        if (res?.status !== 200) {
-          Swal.fire(
-            `Error`,
-            res?.results?.msg || "Something went wrong!",
-            "error"
-          );
-          return;
-        }
-        Swal.fire(`Success`, "Status updated successfully!", "success");
-        return;
-      }
-    });
-  };
+				if (res?.status !== 200) {
+					Swal.fire(
+						`Error`,
+						res?.results?.msg || "Something went wrong!",
+						"error"
+					);
+					return;
+				}
+				Swal.fire(`Success`, "Status updated successfully!", "success");
+				return;
+			}
+		});
+	};
 
-  return (
-    <section className="mt-8">
-      <SendReply
-        open={isReply?.dialogue}
-        handleClose={() => setIsReply({ dialogue: false })}
-      />
-      <MaterialTable
-        title={<HeadStyle name="All Supports" icon={<SupportAgent />} />}
-        isLoading={!data}
-        data={
-          data
-            ? data?.map((item, i: number) => ({
-                ...item,
-                sl: i + 1,
-                name: item?.reqUser
-                  ? item?.reqUser?.name
-                  : item?.reqClient?.name,
-                email: item?.reqUser
-                  ? item?.reqUser?.email
-                  : item?.reqClient?.email,
-              }))
-            : []
-        }
-        options={{
-          ...MuiTblOptions(),
-          exportMenu: [],
-          search: true,
-        }}
-        columns={[
-          {
-            title: "#",
-            field: "sl",
-            editable: "never",
-            width: "2%",
-          },
+	return (
+		<section className="mt-8">
+			<SendReply
+				open={isReply?.dialogue}
+				handleClose={() => setIsReply({ dialogue: false })}
+			/>
+			<MaterialTable
+				title={<HeadStyle name="All Supports" icon={<SupportAgent />} />}
+				isLoading={!data}
+				data={
+					data
+						? data?.map((item, i: number) => ({
+								...item,
+								sl: i + 1,
+								name: item?.reqUser
+									? item?.reqUser?.name
+									: item?.reqClient?.name,
+								email: item?.reqUser
+									? item?.reqUser?.email
+									: item?.reqClient?.email,
+						  }))
+						: []
+				}
+				options={{
+					...MuiTblOptions(),
+					exportMenu: [],
+					search: true,
+				}}
+				columns={[
+					{
+						title: "#",
+						field: "sl",
+						editable: "never",
+						width: "2%",
+					},
 
-          {
-            title: "Name",
-            tooltip: "Name",
-            field: "name",
-          },
+					{
+						title: "Name",
+						tooltip: "Name",
+						field: "name",
+					},
 
-          {
-            title: "Email",
-            tooltip: "Email",
-            field: "email",
-          },
+					{
+						title: "Email",
+						tooltip: "Email",
+						field: "email",
+					},
 
-          {
-            title: "Resolved",
-            tooltip: "Resolved",
-            render: (data) => {
-              return (
-                <div>
-                  <ReverseIOSSwitch
-                    checked={data?.isResolved}
-                    onChange={(e) => handleSubmit(e, data?.id)}
-                  />
-                </div>
-              );
-            },
-          },
-          {
-            title: "Resolved Status",
-            tooltip: "Resolved Status",
-            field: "isResolved",
-            render: ({ isResolved }) => {
-              return <div>{isResolved ? "Resolved" : "Not Resolved"}</div>;
-            },
-          },
+					{
+						title: "Resolved",
+						tooltip: "Resolved",
+						render: (data) => {
+							return (
+								<div>
+									<ReverseIOSSwitch
+										checked={data?.isResolved}
+										onChange={(e) => handleSubmit(e, data?.id)}
+									/>
+								</div>
+							);
+						},
+					},
+					{
+						title: "Resolved Status",
+						hidden: true,
+						tooltip: "Resolved Status",
+						field: "isResolved",
+						render: ({ isResolved }) => {
+							return <div>{isResolved ? "Resolved" : "Not Resolved"}</div>;
+						},
+					},
 
-          {
-            title: "Created",
-            field: "createdAt",
-            render: (data) => moment(data?.createdAt).format("DD/MM/YYYY"),
-            editable: "never",
-          },
-          {
-            title: "Actions",
-            cellStyle: {
-              textAlign: "right",
-            },
-            export: true,
+					{
+						title: "Created",
+						field: "createdAt",
+						render: (data) => moment(data?.createdAt).format("DD/MM/YYYY"),
+						editable: "never",
+					},
+					{
+						title: "Actions",
+						cellStyle: {
+							textAlign: "right",
+						},
+						export: true,
 
-            render: (data) => (
-              <>
-                <div className="flex">
-                  <Tooltip title="Delete">
-                    <Avatar
-                      onClick={() => handleDelete(data?.id)}
-                      variant="rounded"
-                      className="!mr-0.5 !ml-0.5 !cursor-pointer !bg-red-500 !p-0"
-                      sx={{
-                        mr: "0.1vw",
-                        padding: "0px !important",
-                        backgroundColor: "Highlight",
-                        cursor: "pointer",
-                        color: "",
-                      }}
-                    >
-                      <Delete sx={{ padding: "0px !important" }} />
-                    </Avatar>
-                  </Tooltip>
-                </div>
-              </>
-            ),
-          },
-        ]}
-        detailPanel={[
-          {
-            tooltip: "info",
-            icon: () => <Info />,
-            openIcon: () => <Visibility />,
-            render: ({ rowData }) => (
-              <>
-                <div
-                  style={{
-                    padding: "12px",
-                    margin: "auto",
-                    backgroundColor: "#eef5f9",
-                  }}
-                >
-                  <Card
-                    sx={{
-                      minWidth: 450,
-                      // maxWidth: 500,
-                      maxWidth: 800,
-                      transition: "0.3s",
-                      margin: "auto",
-                      borderRadius: "10px",
-                      boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
-                      "&:hover": {
-                        boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)",
-                      },
-                    }}
-                  >
-                    <CardContent>
-                      <Typography gutterBottom align="left">
-                        Message :
-                        <div className="flex gap-2">
-                          <p className="font-semibold">{rowData?.message}</p>
-                        </div>
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </div>
-              </>
-            ),
-          },
-        ]}
-      />
-    </section>
-  );
+						render: (data) => (
+							<>
+								<div className="flex">
+									<Tooltip title="Delete">
+										<Avatar
+											onClick={() => handleDelete(data?.id)}
+											variant="rounded"
+											className="!mr-0.5 !ml-0.5 !cursor-pointer !bg-red-500 !p-0"
+											sx={{
+												mr: "0.1vw",
+												padding: "0px !important",
+												backgroundColor: "Highlight",
+												cursor: "pointer",
+												color: "",
+											}}
+										>
+											<Delete sx={{ padding: "0px !important" }} />
+										</Avatar>
+									</Tooltip>
+								</div>
+							</>
+						),
+					},
+				]}
+				detailPanel={[
+					{
+						tooltip: "info",
+						icon: () => <Info />,
+						openIcon: () => <Visibility />,
+						render: ({ rowData }) => (
+							<>
+								<div
+									style={{
+										padding: "12px",
+										margin: "auto",
+										backgroundColor: "#eef5f9",
+									}}
+								>
+									<Card
+										sx={{
+											minWidth: 450,
+											// maxWidth: 500,
+											maxWidth: 800,
+											transition: "0.3s",
+											margin: "auto",
+											borderRadius: "10px",
+											boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+											"&:hover": {
+												boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)",
+											},
+										}}
+									>
+										<CardContent>
+											<Typography gutterBottom align="left">
+												Message :
+												<div className="flex gap-2">
+													<p className="font-semibold">{rowData?.message}</p>
+												</div>
+											</Typography>
+										</CardContent>
+									</Card>
+								</div>
+							</>
+						),
+					},
+				]}
+			/>
+		</section>
+	);
 };
 
 export default SupportColumn;
--- src/pages/admin/support/create-support.tsx
+++ src/pages/admin/support/create-support.tsx
@@ -8,142 +8,142 @@ import { useState } from "react";
 import Swal from "sweetalert2";
 import * as Yup from "yup";
 const initialValues = {
-  message: "",
+	message: "",
 };
 
 const validationSchema = Yup.object().shape({
-  message: Yup.string().required("Required!"),
+	message: Yup.string().required("Required!"),
 });
 
 const CreateSupport = () => {
-  // const theme = useTheme();
-  const [loading, setLoading] = useState(false);
-  const { change } = useChange();
-  const { user } = useAuth();
+	// const theme = useTheme();
+	const [loading, setLoading] = useState(false);
+	const { change } = useChange();
+	const { user } = useAuth();
 
-  const handleSubmit = async (values: any, { resetForm }: any) => {
-    setLoading(true);
-    const requestBody = {
-      message: values.message,
-      ...(user?.isClient && { isClient: true }), // Add isClient if user?.isClient is true
-    };
-    try {
-      const res = await change(`supports`, {
-        body: requestBody,
-      });
+	const handleSubmit = async (values: any, { resetForm }: any) => {
+		setLoading(true);
+		const requestBody = {
+			message: values.message,
+			...(user?.isClient && { isClient: true }), // Add isClient if user?.isClient is true
+		};
+		try {
+			const res = await change(`supports`, {
+				body: requestBody,
+			});
 
-      setLoading(false);
-      if (res?.status !== 200) {
-        Swal.fire("Error", res?.results?.msg || "Unable to Submit", "error");
-        setLoading(false);
-        return;
-      }
-      Swal.fire(`Success`, `You have successfully Submitted!`, `success`);
-      resetForm();
+			setLoading(false);
+			if (res?.status !== 200) {
+				Swal.fire("Error", res?.results?.msg || "Unable to Submit", "error");
+				setLoading(false);
+				return;
+			}
+			Swal.fire(`Success`, `You have successfully Submitted!`, `success`);
+			resetForm();
 
-      return;
-    } catch (error) {
-      if (error instanceof Error) {
-        Swal.fire(`Error`, error?.message, `error`);
-      } else {
-        Swal.fire(`Error`, "Something Went Wrong", `error`);
-      }
-      setLoading(false);
-    } finally {
-      setLoading(false);
-    }
-  };
+			return;
+		} catch (error) {
+			if (error instanceof Error) {
+				Swal.fire(`Error`, error?.message, `error`);
+			} else {
+				Swal.fire(`Error`, "Something Went Wrong", `error`);
+			}
+			setLoading(false);
+		} finally {
+			setLoading(false);
+		}
+	};
 
-  return (
-    <PanelLayout title="Create Support ">
-      <section className="md:px-8 px-2 md:py-4 py-2">
-        <div className="px-2 md:px-0">
-          <AdminBreadcrumbs links={links} />
-        </div>
-        <section className="w-full px-0 md:py-4 py-2 flex justify-center items-center">
-          <div className="md:p-6 p-2 md:w-3/4 w-full rounded-xl border-b-4 bg-white shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              enableReinitialize={true}
-              onSubmit={handleSubmit}
-            >
-              {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                setFieldValue,
-              }) => (
-                <Form>
-                  <h1 className="text-lg uppercase md:text-xl lg:text-2xl text-slate-600 flex justify-center font-extrabold py-2">
-                    Support
-                  </h1>
-                  <div className="grid lg:grid-cols-1">
-                    <div className="md:px-4 px-2 md:py-2 py-1">
-                      <div className="py-2">
-                        <InputLabel htmlFor="message">
-                          Message <span className="text-red-600">*</span>
-                        </InputLabel>
-                      </div>
-                      <TextField
-                        size="small"
-                        fullWidth
-                        multiline
-                        rows={4}
-                        placeholder="Message"
-                        id="message"
-                        name="message"
-                        value={values.message}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        error={touched.message && !!errors.message}
-                        helperText={touched.message && errors.message}
-                      />
-                    </div>
-                  </div>
-                  <div className="flex justify-center md:py-4 py-2">
-                    <Button
-                      type="submit"
-                      variant="contained"
-                      className="!bg-theme"
-                      disabled={loading}
-                      startIcon={
-                        loading ? (
-                          <CircularProgress color="secondary" size={20} />
-                        ) : (
-                          <Check />
-                        )
-                      }
-                    >
-                      SUBMIT
-                    </Button>
-                  </div>
-                </Form>
-              )}
-            </Formik>
-          </div>
-        </section>
-      </section>
-    </PanelLayout>
-  );
+	return (
+		<PanelLayout title="Create Support ">
+			<section className="md:px-8 px-2 md:py-4 py-2">
+				<div className="px-2 md:px-0">
+					<AdminBreadcrumbs links={links} />
+				</div>
+				<section className="w-full px-0 md:py-4 py-2 flex justify-center items-center">
+					<div className="md:p-6 p-2 md:w-3/4 w-full rounded-xl border-b-4 bg-white shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+						<Formik
+							initialValues={initialValues}
+							validationSchema={validationSchema}
+							enableReinitialize={true}
+							onSubmit={handleSubmit}
+						>
+							{({
+								values,
+								errors,
+								touched,
+								handleChange,
+								handleBlur,
+								setFieldValue,
+							}) => (
+								<Form>
+									<h1 className="text-lg uppercase md:text-xl lg:text-2xl text-slate-600 flex justify-center font-extrabold py-2">
+										Support
+									</h1>
+									<div className="grid lg:grid-cols-1">
+										<div className="md:px-4 px-2 md:py-2 py-1">
+											<div className="py-2">
+												<InputLabel htmlFor="message">
+													Message <span className="text-red-600">*</span>
+												</InputLabel>
+											</div>
+											<TextField
+												size="small"
+												fullWidth
+												multiline
+												rows={4}
+												placeholder="Message"
+												id="message"
+												name="message"
+												value={values.message}
+												onChange={handleChange}
+												onBlur={handleBlur}
+												error={touched.message && !!errors.message}
+												helperText={touched.message && errors.message}
+											/>
+										</div>
+									</div>
+									<div className="flex justify-center md:py-4 py-2">
+										<Button
+											type="submit"
+											variant="contained"
+											className={`${loading ? "!bg-gray-300" : "bg-theme"}`}
+											disabled={loading}
+											startIcon={
+												loading ? (
+													<CircularProgress color="secondary" size={20} />
+												) : (
+													<Check />
+												)
+											}
+										>
+											SUBMIT
+										</Button>
+									</div>
+								</Form>
+							)}
+						</Formik>
+					</div>
+				</section>
+			</section>
+		</PanelLayout>
+	);
 };
 
 export default CreateSupport;
 
 const links = [
-  { id: 1, page: "Support", link: "/admin/support/create-support" },
+	{ id: 1, page: "Support", link: "/admin/support/create-support" },
 ];
 const Status_Type = [
-  {
-    id: 1,
-    name: "Completed",
-    value: "completed",
-  },
-  {
-    id: 2,
-    name: "Pending",
-    value: "pending",
-  },
+	{
+		id: 1,
+		name: "Completed",
+		value: "completed",
+	},
+	{
+		id: 2,
+		name: "Pending",
+		value: "pending",
+	},
 ];
