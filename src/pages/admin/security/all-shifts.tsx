c3519762edde41be94454562390412a6f0688910
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-10-11 11:42:11 +0530

    minor changes

--- src/pages/admin/security/all-shifts.tsx
+++ src/pages/admin/security/all-shifts.tsx
@@ -11,206 +11,214 @@ import { SHIFT } from "types";
 import { MuiTblOptions } from "utils";
 
 const AllShifts = () => {
-  const { user } = useAuth();
-  const [editDetails, setEditDetails] = useState<boolean>(false);
-  const [editShiftData, setEditShiftData] = useState<SHIFT>();
-  const [pageNumber, setPageNumber] = useState<number>(1);
+	const { user } = useAuth();
+	const [editDetails, setEditDetails] = useState<boolean>(false);
+	const [editShiftData, setEditShiftData] = useState<SHIFT>();
+	const [pageNumber, setPageNumber] = useState<number>(1);
 
-  const {
-    data: shiftData,
-    mutate,
-    pagination,
-  } = useFetch<any>(
-    `security/shift?page=${pageNumber}&limit=6${
-      user?.role?.name === "CEO" ? "" : `&branchId=${user?.employeeOfBranchId}`
-    }`
-  );
-  console.log(shiftData);
-  const { change } = useChange();
-  const handleDelete = (id: string) => {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You want to delete!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then(async (result) => {
-      try {
-        if (result.isConfirmed) {
-          Swal.fire(`Info`, "It will take some time", "info");
-          const response = await change(`security/shift/${id}`, {
-            method: "DELETE",
-          });
-          if (response?.status !== 200) {
-            Swal.fire("Error", "Something went wrong!", "error");
-            return;
-          }
+	const {
+		data: shiftData,
+		mutate,
+		pagination,
+	} = useFetch<any>(
+		`security/shift?page=${pageNumber}&limit=6${
+			user?.role?.name === "CEO" ? "" : `&branchId=${user?.employeeOfBranchId}`
+		}`
+	);
+	console.log(shiftData);
+	const { change } = useChange();
+	const handleDelete = (id: string) => {
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You want to delete!",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes, delete it!",
+		}).then(async (result) => {
+			try {
+				if (result.isConfirmed) {
+					Swal.fire(`Info`, "It will take some time", "info");
+					const response = await change(`security/shift/${id}`, {
+						method: "DELETE",
+					});
+					if (response?.status !== 200) {
+						Swal.fire("Error", "Something went wrong!", "error");
+						return;
+					}
 
-          Swal.fire("Success", "Deleted successfully!", "success");
-          mutate();
-        }
-      } catch (error) {
-        if (error instanceof Error) {
-          Swal.fire(`Error`, error?.message, `error`);
-        } else {
-          Swal.fire(`Error`, "Something Went Wrong", `error`);
-        }
-      }
-    });
-  };
+					Swal.fire("Success", "Deleted successfully!", "success");
+					mutate();
+				}
+			} catch (error) {
+				if (error instanceof Error) {
+					Swal.fire(`Error`, error?.message, `error`);
+				} else {
+					Swal.fire(`Error`, "Something Went Wrong", `error`);
+				}
+			}
+		});
+	};
 
-  return (
-    <>
-      <PanelLayout title="All Shifts ">
-        <section className="md:px-8 px-2">
-          <AdminBreadcrumbs links={links} />
-          <EditShift
-            open={editDetails}
-            handleClose={() => setEditDetails(false)}
-            shiftData={editShiftData}
-            mutate={mutate}
-          />
-          <div className="mt-6">
-            <MaterialTable
-              components={{
-                Container: (props) => <Paper {...props} elevation={5} />,
-              }}
-              title={<HeadStyle name="All Shifts" icon={<MeetingRoom />} />}
-              // isLoading={!data}
-              data={
-                !shiftData?.length
-                  ? []
-                  : shiftData?.map((_: any, i: number) => ({
-                      ..._,
-                      sn: i + 1,
-                      branchName: _?.branchData?.name,
-                    }))
-              }
-              options={{
-                ...MuiTblOptions(),
-                paging: false,
-              }}
-              columns={[
-                {
-                  title: "#",
-                  field: "sn",
-                  editable: "never",
-                  width: "2%",
-                },
-                {
-                  title: "Branch",
-                  tooltip: "Branch",
-                  searchable: true,
-                  field: "branchName",
-                },
-                {
-                  title: "Shift",
-                  tooltip: "Shift",
-                  searchable: true,
-                  field: "type",
-                },
-                {
-                  title: "Start Time",
-                  tooltip: "Start Time",
-                  searchable: true,
-                  field: "startTime",
-                },
-                {
-                  title: "End TIme",
-                  tooltip: "End Time",
-                  searchable: true,
-                  field: "endTime",
-                },
+	return (
+		<>
+			<PanelLayout title="All Shifts ">
+				<section className="md:px-8 px-2">
+					<AdminBreadcrumbs links={links} />
+					<EditShift
+						open={editDetails}
+						handleClose={() => setEditDetails(false)}
+						shiftData={editShiftData}
+						mutate={mutate}
+					/>
+					<div className="mt-6">
+						<MaterialTable
+							components={{
+								Container: (props) => <Paper {...props} elevation={5} />,
+							}}
+							title={<HeadStyle name="All Shifts" icon={<MeetingRoom />} />}
+							// isLoading={!data}
+							data={
+								!shiftData?.length
+									? []
+									: shiftData?.map((_: any, i: number) => ({
+											..._,
+											sn: i + 1,
+											branchName: _?.branchData?.name,
+											shiftType:
+												_?.type === "FirstShift"
+													? "First Shift"
+													: _?.type === "SecondShift"
+													? "Second Shift"
+													: _?.type === "NightShift"
+													? "Night Shift"
+													: "",
+									  }))
+							}
+							options={{
+								...MuiTblOptions(),
+								paging: false,
+							}}
+							columns={[
+								{
+									title: "#",
+									field: "sn",
+									editable: "never",
+									width: "2%",
+								},
+								{
+									title: "Branch",
+									tooltip: "Branch",
+									searchable: true,
+									field: "branchName",
+								},
+								{
+									title: "Shift",
+									tooltip: "Shift",
+									searchable: true,
+									field: "shiftType",
+								},
+								{
+									title: "Start Time",
+									tooltip: "Start Time",
+									searchable: true,
+									field: "startTime",
+								},
+								{
+									title: "End TIme",
+									tooltip: "End Time",
+									searchable: true,
+									field: "endTime",
+								},
 
-                user?.role?.name === "CEO"
-                  ? {
-                      title: "Actions",
-                      cellStyle: {
-                        textAlign: "right",
-                      },
-                      export: true,
-                      // width: "18%",
-                      // field: "pick",
-                      render: (item) => (
-                        <>
-                          <div className="flex">
-                            <Tooltip title="Edit">
-                              <Avatar
-                                // onClick={() => setOpenAddCustomerDrawer(row)}
-                                onClick={() => {
-                                  setEditDetails((prev) => !prev),
-                                    setEditShiftData(item);
-                                }}
-                                variant="rounded"
-                                className="!mr-0.5 !ml-0.5 !cursor-pointer !bg-theme !p-0"
-                                sx={{
-                                  mr: ".1vw",
-                                  padding: "0px !important",
-                                  backgroundColor: "Highlight",
-                                  cursor: "pointer",
-                                  color: "",
-                                }}
-                              >
-                                <BorderColor
-                                  sx={{ padding: "0px !important" }}
-                                />
-                              </Avatar>
-                            </Tooltip>
-                            <Tooltip title="Delete">
-                              <Avatar
-                                onClick={() => handleDelete(item?._id?.$oid)}
-                                variant="rounded"
-                                className="!mr-0.5 !ml-0.5 !cursor-pointer !bg-red-700 !p-0"
-                                sx={{
-                                  mr: "0.1vw",
-                                  padding: "0px !important",
-                                  backgroundColor: "Highlight",
-                                  cursor: "pointer",
-                                  color: "",
-                                }}
-                              >
-                                <Delete sx={{ padding: "0px !important" }} />
-                              </Avatar>
-                            </Tooltip>
-                          </div>
-                        </>
-                      ),
-                    }
-                  : {},
-              ]}
-            />
-          </div>
-          <section className="mb-6">
-            {Math.ceil(
-              Number(pagination?.total || 1) / Number(pagination?.limit || 1)
-            ) > 1 ? (
-              <div className="flex justify-center md:py-8 py-4">
-                <Stack spacing={2}>
-                  <Pagination
-                    count={Math.ceil(
-                      Number(pagination?.total || 1) /
-                        Number(pagination?.limit || 1)
-                    )}
-                    onChange={(e, v: number) => {
-                      setPageNumber(v);
-                    }}
-                    page={pageNumber}
-                    variant="outlined"
-                  />
-                </Stack>
-              </div>
-            ) : null}
-          </section>
-        </section>
-      </PanelLayout>
-    </>
-  );
+								user?.role?.name === "CEO"
+									? {
+											title: "Actions",
+											cellStyle: {
+												textAlign: "right",
+											},
+											export: true,
+											// width: "18%",
+											// field: "pick",
+											render: (item) => (
+												<>
+													<div className="flex">
+														<Tooltip title="Edit">
+															<Avatar
+																// onClick={() => setOpenAddCustomerDrawer(row)}
+																onClick={() => {
+																	setEditDetails((prev) => !prev),
+																		setEditShiftData(item);
+																}}
+																variant="rounded"
+																className="!mr-0.5 !ml-0.5 !cursor-pointer !bg-theme !p-0"
+																sx={{
+																	mr: ".1vw",
+																	padding: "0px !important",
+																	backgroundColor: "Highlight",
+																	cursor: "pointer",
+																	color: "",
+																}}
+															>
+																<BorderColor
+																	sx={{ padding: "0px !important" }}
+																/>
+															</Avatar>
+														</Tooltip>
+														<Tooltip title="Delete">
+															<Avatar
+																onClick={() => handleDelete(item?._id?.$oid)}
+																variant="rounded"
+																className="!mr-0.5 !ml-0.5 !cursor-pointer !bg-red-700 !p-0"
+																sx={{
+																	mr: "0.1vw",
+																	padding: "0px !important",
+																	backgroundColor: "Highlight",
+																	cursor: "pointer",
+																	color: "",
+																}}
+															>
+																<Delete sx={{ padding: "0px !important" }} />
+															</Avatar>
+														</Tooltip>
+													</div>
+												</>
+											),
+									  }
+									: {},
+							]}
+						/>
+					</div>
+					<section className="mb-6">
+						{Math.ceil(
+							Number(pagination?.total || 1) / Number(pagination?.limit || 1)
+						) > 1 ? (
+							<div className="flex justify-center md:py-8 py-4">
+								<Stack spacing={2}>
+									<Pagination
+										count={Math.ceil(
+											Number(pagination?.total || 1) /
+												Number(pagination?.limit || 1)
+										)}
+										onChange={(e, v: number) => {
+											setPageNumber(v);
+										}}
+										page={pageNumber}
+										variant="outlined"
+									/>
+								</Stack>
+							</div>
+						) : null}
+					</section>
+				</section>
+			</PanelLayout>
+		</>
+	);
 };
 
 export default AllShifts;
 
 const links = [
-  { id: 2, page: "All Shifts", link: "/admin/security/all-shifts" },
+	{ id: 2, page: "All Shifts", link: "/admin/security/all-shifts" },
 ];
