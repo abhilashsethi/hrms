15a0bc3da0daf88db409b6ff26dc1757e64f04a7
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2023-10-18 11:16:54 +0530

    phone number is required in create guest

--- src/components/admin/attendance/AttendanceCharts.tsx
+++ src/components/admin/attendance/AttendanceCharts.tsx
@@ -2,41 +2,42 @@ import { DailyAttendance, DonutChart } from "components/analytics";
 import { useFetch } from "hooks";
 import { Attendance, AttendanceDashboard } from "types";
 interface Props {
-  absentData?: Attendance[];
-  presentData?: Attendance[];
+	absentData?: Attendance[];
+	presentData?: Attendance[];
 }
 const AttendanceCharts = ({ absentData, presentData }: Props) => {
-  const { data: employeeDetails } = useFetch<AttendanceDashboard>(
-    `users/dashboard/details`
-  );
-  return (
-    <>
-      <div className="grid grid-cols-12 py-2 content-between gap-6 !mb-6">
-        <div className="col-span-12 bg-white pt-2 w-full  gap-5 md:col-span-12 lg:col-span-7 !border-grey-500 rounded-xl !shadow-xl">
-          <div className="font-semibold flex justify-center py-4 pl-2 ">
-            <p>Last Week Employee's Report </p>
-          </div>
-          <DailyAttendance
-            type="area"
-            data={employeeDetails?.lastWeekAttendanceArr}
-            totalUsers={employeeDetails?.totalUsers}
-          />
-        </div>
-        <div className="col-span-12 bg-white pt-9 w-full flex flex-col justify-center gap-5 md:col-span-12 lg:col-span-5 !border-gray-500 rounded-xl !shadow-xl">
-          <p className="text-lg font-bold text-center">Today's Attendance</p>
-          <DonutChart
-            labels={["Present", "Absent"]}
-            series={[
-              presentData?.length ? presentData?.length : `0`,
-              absentData?.length ? absentData?.length : `0`,
-            ]}
-            text=""
-            type="donut"
-          />
-        </div>
-      </div>
-    </>
-  );
+	const { data: employeeDetails } = useFetch<AttendanceDashboard>(
+		`users/dashboard/details`
+	);
+	console.log(employeeDetails);
+	return (
+		<>
+			<div className="grid grid-cols-12 py-2 content-between gap-6 !mb-6">
+				<div className="col-span-12 bg-white pt-2 w-full  gap-5 md:col-span-12 lg:col-span-7 !border-grey-500 rounded-xl !shadow-xl">
+					<div className="font-semibold flex justify-center py-4 pl-2 ">
+						<p>Last Week Employee's Report </p>
+					</div>
+					<DailyAttendance
+						type="area"
+						data={employeeDetails?.lastWeekAttendanceArr}
+						totalUsers={employeeDetails?.totalUsers}
+					/>
+				</div>
+				<div className="col-span-12 bg-white pt-9 w-full flex flex-col justify-center gap-5 md:col-span-12 lg:col-span-5 !border-gray-500 rounded-xl !shadow-xl">
+					<p className="text-lg font-bold text-center">Today's Attendance</p>
+					<DonutChart
+						labels={["Present", "Absent"]}
+						series={[
+							presentData?.length ? presentData?.length : `0`,
+							absentData?.length ? absentData?.length : `0`,
+						]}
+						text=""
+						type="donut"
+					/>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default AttendanceCharts;
--- src/pages/admin/attendances/index.tsx
+++ src/pages/admin/attendances/index.tsx
@@ -8,82 +8,83 @@ import PanelLayout from "layouts/panel";
 import { useMemo } from "react";
 import { Attendance, UserDashboard } from "types";
 const Attendance = () => {
-  const { user } = useAuth();
-  const { data: absentData } = useFetch<Attendance[]>(
-    `attendances/${new Date().toISOString().slice(0, 10)}/absent`
-  );
-  const { data: presentData } = useFetch<Attendance[]>(
-    `attendances/${new Date().toISOString().slice(0, 10)}/present`
-  );
-  const { data: dashboardDetails, isLoading } = useFetch<UserDashboard>(
-    `users/dashboard/details`
-  );
-  const cards = useMemo(
-    () => [
-      {
-        id: 1,
-        icon: (
-          <HowToReg
-            fontSize="large"
-            className="text-theme group-hover:text-white shadow-xl rounded-lg"
-          />
-        ),
-        count: `${presentData?.length ? presentData?.length : `0`}`,
-        title: "Today Present",
-        bg: "from-blue-500 to-blue-300",
-        img: CHECK.src,
-        link: "/admin/attendances/today",
-      },
-      {
-        id: 2,
-        icon: (
-          <PersonOff
-            fontSize="large"
-            className="text-theme group-hover:text-white shadow-xl rounded-lg"
-          />
-        ),
-        count: `${absentData?.length ? absentData?.length : `0`}`,
-        title: "Today Absent",
-        bg: "from-purple-500 to-purple-300",
-        img: CANCEL.src,
-        link: "/admin/attendances/today",
-      },
-      {
-        id: 3,
-        icon: (
-          <Groups
-            fontSize="large"
-            className="text-theme group-hover:text-white shadow-xl rounded-lg"
-          />
-        ),
-        count: `${
-          dashboardDetails?.totalUsers ? dashboardDetails?.totalUsers : `0`
-        }`,
-        title: "Total Employees",
-        bg: "from-yellow-500 to-yellow-300",
-        img: GROUP.src,
-        link: "/admin/employees/all-employees",
-      },
-    ],
-    [dashboardDetails, absentData, presentData]
-  );
-  if (isLoading) {
-    return <Loader />;
-  }
-  return (
-    <PanelLayout title="Attendance Dashboard">
-      <>
-        <WelcomeUser title={`Welcome ${user?.role?.name}`} />
-        <div className="lg:px-8 px-4 pt-4">
-          <AdminBreadcrumbs links={links} />
-        </div>
-        <div className="lg:px-8 px-4 pt-4">
-          <DashboardCard data={cards} />
-          <AttendanceCharts absentData={absentData} presentData={presentData} />
-        </div>
-      </>
-    </PanelLayout>
-  );
+	const { user } = useAuth();
+	const { data: absentData } = useFetch<Attendance[]>(
+		`attendances/${new Date().toISOString().slice(0, 10)}/absent`
+	);
+	const { data: presentData } = useFetch<Attendance[]>(
+		`attendances/${new Date().toISOString().slice(0, 10)}/present`
+	);
+	console.log(presentData);
+	const { data: dashboardDetails, isLoading } = useFetch<UserDashboard>(
+		`users/dashboard/details`
+	);
+	const cards = useMemo(
+		() => [
+			{
+				id: 1,
+				icon: (
+					<HowToReg
+						fontSize="large"
+						className="text-theme group-hover:text-white shadow-xl rounded-lg"
+					/>
+				),
+				count: `${presentData?.length ? presentData?.length : `0`}`,
+				title: "Today Present",
+				bg: "from-blue-500 to-blue-300",
+				img: CHECK.src,
+				link: "/admin/attendances/today",
+			},
+			{
+				id: 2,
+				icon: (
+					<PersonOff
+						fontSize="large"
+						className="text-theme group-hover:text-white shadow-xl rounded-lg"
+					/>
+				),
+				count: `${absentData?.length ? absentData?.length : `0`}`,
+				title: "Today Absent",
+				bg: "from-purple-500 to-purple-300",
+				img: CANCEL.src,
+				link: "/admin/attendances/today",
+			},
+			{
+				id: 3,
+				icon: (
+					<Groups
+						fontSize="large"
+						className="text-theme group-hover:text-white shadow-xl rounded-lg"
+					/>
+				),
+				count: `${
+					dashboardDetails?.totalUsers ? dashboardDetails?.totalUsers : `0`
+				}`,
+				title: "Total Employees",
+				bg: "from-yellow-500 to-yellow-300",
+				img: GROUP.src,
+				link: "/admin/employees/all-employees",
+			},
+		],
+		[dashboardDetails, absentData, presentData]
+	);
+	if (isLoading) {
+		return <Loader />;
+	}
+	return (
+		<PanelLayout title="Attendance Dashboard">
+			<>
+				<WelcomeUser title={`Welcome ${user?.role?.name}`} />
+				<div className="lg:px-8 px-4 pt-4">
+					<AdminBreadcrumbs links={links} />
+				</div>
+				<div className="lg:px-8 px-4 pt-4">
+					<DashboardCard data={cards} />
+					<AttendanceCharts absentData={absentData} presentData={presentData} />
+				</div>
+			</>
+		</PanelLayout>
+	);
 };
 
 export default Attendance;
